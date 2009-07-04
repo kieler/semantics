b9f8c336be68cde3f0c25666d6e1c9839b6d85af
Christian Motika <cmot@informatik.uni-kiel.de>
2009-07-04 16:39:35 +0000

    new file type property tester :-)
    see 
    1)http://dev.eclipse.org/viewsvn/index.cgi/org.eclipse.core.resources/plugin.xml?revision=1.80&view=markup
    2)http://help.eclipse.org/galileo/index.jsp?topic=/org.eclipse.platform.doc.isv/guide/workbench_cmd_expressions.htm
    3)http://help.eclipse.org/stable/index.jsp?topic=/org.eclipse.platform.doc.isv/reference/api/org/eclipse/core/expressions/package-summary.html

--- plugins/de.cau.cs.kieler.simplerailctrl.codegen.c/src/de/cau/cs/kieler/simplerailctrl/codegen/c/handlers/CodeGenerationHandler.java
+++ plugins/de.cau.cs.kieler.simplerailctrl.codegen.c/src/de/cau/cs/kieler/simplerailctrl/codegen/c/handlers/CodeGenerationHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
@@ -13,80 +14,118 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.emf.common.ui.URIEditorInput;
 import org.eclipse.emf.common.util.URI;
-import org.openarchitectureware.workflow.WorkflowRunner;
 import org.eclipse.ui.part.FileEditorInput;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.core.runtime.IAdaptable;
+
+import de.cau.cs.kieler.simplerailctrl.SimplerailctrlPackage;
+
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.WorkflowContextDefaultImpl;
+import org.eclipse.emf.mwe.core.WorkflowFacade;
+import org.eclipse.emf.mwe.utils.Reader;
+import org.eclipse.emf.mwe.utils.Writer;
+import org.eclipse.xtend.typesystem.emf.EmfMetaModel;
+import org.eclipse.xpand2.Generator;
+import org.eclipse.xpand2.output.Outlet;
+import org.eclipse.xpand2.output.Output;
+import org.eclipse.emf.mwe.core.issues.*;
+import org.eclipse.emf.mwe.core.monitor.NullProgressMonitor;
+import org.eclipse.emf.mwe.internal.core.Workflow;
+
 
 public class CodeGenerationHandler extends AbstractHandler {
 	
 	private static String _pluginFolder;
 	
+	
+	IResource extractSelection(ISelection sel) {
+	      if (!(sel instanceof IStructuredSelection))
+	         return null;
+	      IStructuredSelection ss = (IStructuredSelection) sel;
+	      Object element = ss.getFirstElement();
+	      if (element instanceof IResource)
+	         return (IResource) element;
+	      if (!(element instanceof IAdaptable))
+	         return null;
+	      IAdaptable adaptable = (IAdaptable)element;
+	      Object adapter = adaptable.getAdapter(IResource.class);
+	      return (IResource) adapter;
+	}
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil
 				.getActiveWorkbenchWindow(event);
 
-		IWorkbenchPage page = activeWorkbenchWindow.getActivePage();
-		if (page == null) {
-			return null;
-		}
-
-		IEditorPart editor = page.getActiveEditor();
-		if (editor == null) {
-			return null;
-		}
-		
-		IWorkbenchWindow window = activeWorkbenchWindow;
-		IEditorPart Editor = window.getActivePage().getActiveEditor();
-
-        String ModelFile = "";
-		if (Editor.getEditorInput() instanceof URIEditorInput) {
-			URIEditorInput EditorInput = (URIEditorInput)Editor.getEditorInput();
-	        URI EditorURI = EditorInput.getURI();
-	        ModelFile = EditorURI.toString();
-		}
-		else {
-			FileEditorInput EditorInput = (FileEditorInput)Editor.getEditorInput();
-			ModelFile = EditorInput.getURI().toString();
-		}
-		
-        //delete "_diagram"-extension
-		ModelFile = ModelFile.replace("_diagram", "");
-		//ModelFile = ModelFile.replace("reference:", "");
-		//ModelFile = ModelFile.replace("file:/", "");
-
-		String PluginRoot = CodeGenerationHandler.getPluginFolder();
-		String GenFolder = PluginRoot + "src-gen";
-		
-		// code generation here //
-        Map<String,String> properties = new HashMap<String,String>();
-        Map<String, Object> slotContents = new HashMap<String, Object>();
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+	    
+        ISelection selection = window.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+        String inputModel = extractSelection(selection).getFullPath().toString();
         
-        String WorkflowFile = "model/workflowM2T.oaw";
-		properties.put("model", ModelFile);
-		properties.put("metamodel", "SimpleRailCtrl.ecore");
-		properties.put("src-gen", GenFolder); 
-		
-		System.out.println("Code Generation - starting...");
-		
+        
+        System.out.println(inputModel);
+        		
+        //Workflow
+        Workflow workflow = new Workflow();
+        
+        //EMF reader
+        Reader emfReader = new Reader();
+        emfReader.setUri(inputModel);
+        emfReader.setModelSlot("model");
+        
+        //Meta model
+        EmfMetaModel metaModel = new EmfMetaModel(de.cau.cs.kieler.simplerailctrl.SimplerailctrlPackage.eINSTANCE);
+
+        //Outlet
+        Outlet outlet = new Outlet();
+        outlet.setPath("\\src-gen");
+        outlet.setOverwrite(true);
+        
+        //Generator
+        Generator generator = new Generator();
+        //generator.setGenPath("\\src-gen");
+        generator.addMetaModel(metaModel);
+        generator.addOutlet(outlet);
+        
+        generator.setExpand("template::main FOR model");
+        
+        WorkflowContext wfx = new WorkflowContextDefaultImpl();
+        Issues issues = new org.eclipse.emf.mwe.core.issues.IssuesImpl();
+        NullProgressMonitor monitor = new NullProgressMonitor();
+        
+        workflow.addComponent(emfReader);
+        workflow.addComponent(generator);
+
 		boolean success = false;
-		try {  
-//		     ProjectIncludingResourceLoader resourceLoader =   
-//		      new ProjectIncludingResourceLoader(project);  
-//		     ResourceLoaderFactory.setCurrentThreadResourceLoader(resourceLoader);  
-		     WorkflowRunner runner = new WorkflowRunner();  
-		     success = runner.run(WorkflowFile, null,    
-		                                  properties, slotContents);  
-		}	finally {  
-//		     ResourceLoaderFactory.setCurrentThreadResourceLoader(null);  
-		}  
-			   
+		try {
+			workflow.invoke(wfx, monitor, issues);
+			success = true;
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+        
+        System.out.print(generator.getLogMessage());
+        
+        System.out.print(issues.getInfos());
+        System.out.print(issues.getIssues());
+        System.out.print(issues.getWarnings());
+        System.out.print(issues.getErrors().toString());
+        
+        //EMF writer
+        //Writer emfWriter = new Writer();
+        //emfWriter.setUri(modelURI);
+        //emfWriter.setModelSlot("model");        
+        
         if (success) {
         	System.out.println("Code Generation - completed.");
         }
         else {
         	System.out.println("Code Generation - failed.");
         }
-		
-	    return null;
+        
+		return null;
 	}
 
 	
