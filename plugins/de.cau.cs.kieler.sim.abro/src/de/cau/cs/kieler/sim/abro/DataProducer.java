862f80ab2c1c38d5ccff1dce70e0c89d78821d8d
Christian Motika <cmot@informatik.uni-kiel.de>
2009-07-06 12:37:06 +0000

    simple ABRO data producer

---
+++ plugins/de.cau.cs.kieler.sim.abro/src/de/cau/cs/kieler/sim/abro/DataProducer.java
@@ -0,0 +1,39 @@
+package de.cau.cs.kieler.sim.abro;
+
+import de.cau.cs.kieler.sim.kiem.extension.DataProducerInterface;
+
+public class DataProducer extends
+		de.cau.cs.kieler.sim.kiem.extension.DataProducer implements
+		DataProducerInterface {
+
+	@Override
+	public void ExecutionInitialize() {
+		// TODO Auto-generated method stub
+		System.out.println("ABRO initialze");
+	}
+
+	@Override
+	public void ExecutionPause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ExecutionPlay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ExecutionStep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ExecutionStop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
