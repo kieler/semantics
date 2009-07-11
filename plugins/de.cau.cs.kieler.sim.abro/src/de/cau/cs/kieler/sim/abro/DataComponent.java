b7b843311da1f69b690c56a267e490f187e6317b
Christian Motika <cmot@informatik.uni-kiel.de>
2009-07-11 21:50:13 +0000

    KIELER ABRO in Java example (working :-)

--- plugins/de.cau.cs.kieler.sim.abro/src/de/cau/cs/kieler/sim/abro/DataComponent.java
+++ plugins/de.cau.cs.kieler.sim.abro/src/de/cau/cs/kieler/sim/abro/DataComponent.java
@@ -4,20 +4,84 @@ import java.util.Random;
 
 import de.cau.cs.kieler.sim.kiem.extension.IJSONObjectDataComponent;
 import de.cau.cs.kieler.sim.kiem.extension.JSONObjectDataComponent;
+import de.cau.cs.kieler.sim.kiem.json.JSONException;
 import de.cau.cs.kieler.sim.kiem.json.JSONObject;
 
 public class DataComponent extends JSONObjectDataComponent implements
 		IJSONObjectDataComponent {
+	
+	boolean wA;
+	boolean wB;
+	boolean dA;
+	boolean dB;
+	boolean done;
+	
 
 	public DataComponent() {
-		// TODO Auto-generated constructor stub
+		resetABO();
+	}
+	
+	private void resetABO() {
+		wA = true;
+		wB = true;
+		dA = false;
+		dB = false;
+		done = false;
+	}
+	private void transition_wA_dA() {
+		wA = false;
+		dA = true;
+	}
+	private void transition_wB_dB() {
+		wB = false;
+		dB = true;
+	}
+	private void transition_done() {
+		dB = false;
+		dA = false;
+		done = true;
 	}
 
 	@Override
 	public JSONObject step(JSONObject JSONobject) {
-		// TODO Auto-generated method stub
 		try{Thread.sleep(new Random().nextInt(200));}catch(Exception e){}
-		return null;
+		
+		JSONObject returnObj = new JSONObject();
+		
+		try{
+			if (JSONobject.has("R")) {
+				resetABO();
+			}
+			else {
+				if (wA && JSONobject.has("A")) {
+					transition_wA_dA();
+				}
+				if (wB && JSONobject.has("B")) {
+					transition_wB_dB();
+				}
+				if (dA && dB) {
+					transition_done();
+					returnObj.accumulate("O","");
+				}
+			}
+			if (done) {
+				returnObj.accumulate("state","done");
+			}
+			if (dA && !dB) {
+				returnObj.accumulate("state","dA, wB");
+			}
+			if (dB && !dA) {
+				returnObj.accumulate("state","wA, dB");
+			}
+			if (wA && wB) {
+				returnObj.accumulate("state","wA, wB");
+			}
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return returnObj;
 	}
 
 	@Override
@@ -39,10 +103,14 @@ public class DataComponent extends JSONObjectDataComponent implements
 	}
 	
 	public boolean needModelFile() {
-		return true;
+		return false;
 	}
 	
 	public boolean isMaster() {
 		return false;
+	}
+
+	public boolean isDeltaConsumer() {
+		return true;
 	}
 }
