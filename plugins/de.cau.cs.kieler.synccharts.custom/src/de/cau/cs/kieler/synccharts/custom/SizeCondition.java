db50a4c48d8eeb7888a7ee04723865c580315702
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-07-24 07:19:53 +0000

    Added license text.

--- plugins/de.cau.cs.kieler.synccharts.custom/src/de/cau/cs/kieler/synccharts/custom/SizeCondition.java
+++ plugins/de.cau.cs.kieler.synccharts.custom/src/de/cau/cs/kieler/synccharts/custom/SizeCondition.java
@@ -1,3 +1,17 @@
+/******************************************************************************
+ * KIELER - Kiel Integrated Environment for Layout for the Eclipse RCP
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2009 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ * 
+ *****************************************************************************/
 package de.cau.cs.kieler.synccharts.custom;
 
 import org.eclipse.emf.common.util.EList;
