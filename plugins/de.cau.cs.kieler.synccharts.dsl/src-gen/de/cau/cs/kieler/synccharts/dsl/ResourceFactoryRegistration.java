08c34a7ee077e0efd270b942fbacfa53eac2a2a8
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2009-09-09 14:53:02 +0000

    replaced by labelparser

--- plugins/de.cau.cs.kieler.synccharts.dsl/src-gen/de/cau/cs/kieler/synccharts/dsl/ResourceFactoryRegistration.java
+++
@@ -1,9 +0,0 @@
-package de.cau.cs.kieler.synccharts.dsl;
-
-import de.cau.cs.kieler.synccharts.dsl.resource.scdslResourceFactory;
-
-public class ResourceFactoryRegistration {
-	public static void register() {
-	  	scdslResourceFactory.register();
-	}
-}
