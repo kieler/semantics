44c309d10d8bd704b541f76a5ecc84013c548c5e
Christian Motika <cmot@informatik.uni-kiel.de>
2009-09-22 13:01:18 +0000

    preparing execution environment KIEM data component

---
+++ plugins/de.cau.cs.kieler.synccharts.sim.ptolemy/src/de/cau/cs/kieler/synccharts/sim/ptolemy/ExecutePtolemyModel.java
@@ -0,0 +1,311 @@
+/******************************************************************************
+ * KIELER - Kiel Integrated Environment for Layout for the Eclipse RCP
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2009 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ ******************************************************************************/
+package de.cau.cs.kieler.synccharts.sim.ptolemy;
+
+import java.io.File;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.URI;
+
+import de.cau.cs.kieler.sim.kiem.extension.KiemExecutionException;
+
+import ptolemy.actor.CompositeActor;
+import ptolemy.actor.Manager;
+import ptolemy.domains.fsm.modal.ModalController;
+import ptolemy.domains.fsm.modal.ModalModel;
+import ptolemy.kernel.InstantiableNamedObj;
+import ptolemy.kernel.util.NamedObj;
+import ptolemy.kernel.util.StringAttribute;
+import ptolemy.moml.MoMLParser;
+import ptolemy.actor.kiel.*;
+
+/**
+ * The class ExecutePtolemyModel implements the PtolemyExecutor. This is the
+ * component that enables loading and executing (generated) Ptolemy models.
+ * 
+ * @author Christian Motika - cmot AT informatik.uni-kiel.de
+ */
+public class ExecutePtolemyModel implements Runnable {
+	
+	/** The Ptolemy model to execute. */
+	private String PtolemyModel;
+	
+	/** The flag indicating the execution is paused. */
+	private boolean paused;
+	
+	/** The flag indicating the execution should stop. */
+	private boolean stop;
+	
+	/** The steps to make, because {@link #executionStep()} is 
+	 * called asynchronously. */
+	private int makesteps;
+	
+	/** The currently active state of the EMF model as FragmentURI. */
+	private String currentState;
+	
+	/** The Ptolemy manager. */
+	private Manager manager; 
+	
+	/** The host for connecting to the model railway simulation. */
+	private String host;
+	
+	/** The port for connecting to the model railway simulation. */
+	private String port;
+	
+	/** The KiemExecutionException that may have occurred during the 
+	 * asynchronous performance of steps. */
+	private KiemExecutionException executionException; 
+	
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * Instantiates a new ExecutePtolemyModel.
+	 * 
+	 * @param PtolemyModel the Ptolemy model to execute
+	 * @param host the host for the simulation
+	 * @param port the port for the simulation
+	 */
+	public ExecutePtolemyModel(String PtolemyModel, 
+							   String host,
+							   String port) {
+		this.PtolemyModel = PtolemyModel;
+		this.paused = true;
+		this.stop = false;
+		this.makesteps = 0;
+		this.currentState = "";
+		this.host = host;
+		this.port = port;
+		this.executionException = null;
+	}
+	
+	//-------------------------------------------------------------------------
+
+	/**
+	 * Gets the currently active state as URIFragment.
+	 * 
+	 * @return the current state
+	 */
+	public String getCurrentState() {
+		return currentState;
+	}
+	
+	//-------------------------------------------------------------------------
+
+	/**
+	 * Trigger the execution to perform another step. This is done 
+	 * asynchronously meaning that the step is not immediately performed but
+	 * whenever the executing thread is able to perform the next one.
+	 * <BR><BR>
+	 * If the {@link #executionStop()} method is called before this step
+	 * is actually performed it wont be performed!
+	 * <BR><BR>
+	 * If any KiemExecutionException was thrown in the {@link #run()} method
+	 * of the thread then it is thrown in here. Note that this exception
+	 * normally happened in any step performed before the current one but only
+	 * now will be brought to the users or KIEM attention. 
+	 * 
+	 * @throws KiemExecutionException a KiemExecutionException
+	 */
+	public synchronized void executionStep() throws KiemExecutionException {
+		this.paused = false;
+		//increase steps to make
+		this.makesteps++;
+		//throw exception that may have occurred (asynchronously)
+		if (this.executionException != null)
+			throw this.executionException;
+	}
+
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * This immediately stops the execution and will prevent any steps to make
+	 * even if they were triggered before a call to this method.
+	 */
+	public void executionStop() {
+		this.stop = true;
+	}
+	
+	//-------------------------------------------------------------------------
+
+	/**
+	 * This method will cause the execution pause.
+	 */
+	public void executionPause() {
+		this.paused = true;
+	}
+	
+	//-------------------------------------------------------------------------
+
+	/**
+	 * Fills the modalModelList by recursively going thru the models elements.
+	 * 
+	 * @param modalModelList the list of ModalModels to fill
+	 * @param children the children to walk thru
+	 */
+	@SuppressWarnings("unchecked")
+	private void fillModalModelList(List<ModalModel> modalModelList,
+									List<InstantiableNamedObj> children) {
+		// if no further children
+		if (children == null) return;
+
+		// do recursively for children
+		for (int c = 0; c < children.size(); c++){
+			Object child = children.get(c);
+            if(child instanceof CompositeActor){
+            	CompositeActor compositeActor = (CompositeActor)child;
+            	
+            	fillModalModelList(
+            			modalModelList,
+            			compositeActor.entityList());
+            }
+            
+
+            if (child instanceof ModalModel) {
+            	ModalModel modalModel = (ModalModel)child;
+            	modalModelList.add(modalModel);
+            	
+            	fillModalModelList(
+            			modalModelList,
+            			modalModel.entityList());
+            }
+
+            if (child instanceof ModalController) {
+            	ModalController modalController = (ModalController)child;
+            	
+            	fillModalModelList(
+            			modalModelList,
+            			modalController.entityList());
+            }
+        }//end while
+	}
+
+	
+	//-------------------------------------------------------------------------
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@SuppressWarnings("unchecked")
+	public void run() {
+		URI fileURI = URI.createFileURI(new File(PtolemyModel).getAbsolutePath());
+        URI momlFile = fileURI;
+
+        //create new MoML parser
+        //make sure Ptolemy is in dependencies
+        MoMLParser parser = new MoMLParser();
+        
+        List<ModalModel> modalModelList = new LinkedList<ModalModel>();
+        
+        NamedObj ptolemyModel = null;
+        try {
+            //parse
+            ptolemyModel = parser.parse(null, new URL(momlFile.toString()));
+
+            //now execute the model
+            if (ptolemyModel != null && ptolemyModel instanceof CompositeActor) {
+                //check if the parsed model is of correct type
+                CompositeActor modelActor = ((CompositeActor) ptolemyModel);
+                
+                //get the manager that manages execution
+                manager = modelActor.getManager();
+                if (manager == null) {
+                    //there is likely no manager available, 
+                	//hence create a new one
+                    manager = new Manager(modelActor.workspace(), 
+                    						"kieler manager");
+                    modelActor.setManager(manager);
+                }
+                else {
+                	//if there is already a manager (e.g., from previous calls)
+                	//then try to stop it and create a new one
+                	try {
+                    	manager.stop();
+                    	manager.wrapup();
+                	}catch(Exception e){}
+                    manager = new Manager(modelActor.workspace(), 
+                    						"kieler manager");
+                    modelActor.setManager(manager);
+                }
+
+                //go thru the model and add fill the modalModelList
+                fillModalModelList(
+                		modalModelList,
+                		modelActor.entityList());
+                
+                //modify host and port of railway simulation engine actor
+                if (modelActor.entityList() != null) {
+                    for (int c = 0; c < modelActor.entityList().size(); c++) {
+                    	Object entity = modelActor.entityList().get(c);
+                    	
+                    	if (entity instanceof ModelRailwayIO) {
+                    	  ModelRailwayIO modelRailwayIO =
+                    			(ModelRailwayIO) entity;
+                    	  modelRailwayIO.host.setExpression("'"+this.host+"'");
+                    	  modelRailwayIO.port.setExpression(this.port);
+                    	}
+                    	
+                    }//next c
+                }
+
+                // run the model
+                if (manager != null) {
+                    // run forest, run!
+                    manager.initialize();
+                    while(true) {
+                    	
+                    	while(this.paused) {
+                    		Thread.sleep(100);
+                    	}
+                    	if (this.stop) {
+                            manager.wrapup();
+                    		return;
+                    	}
+                    	
+                    	synchronized(this) {
+                          if ((this.makesteps == -1)||(this.makesteps > 0)) {
+                        	  if (makesteps > 0) makesteps--;
+                        		manager.iterate();
+                        		
+                        	  //iterate thru all modal models and concatenate
+                        	  //the fragment URIs with a colon
+                        	  currentState = "";
+                        	  for (int c = 0; c < modalModelList.size(); c++) {
+                        		ModalModel modalModel = modalModelList.get(c);
+                        		//if more than one active state
+                        		if (!currentState.equals(""))
+                        			currentState += ", ";
+                        		currentState += ((StringAttribute)modalModel
+                        				 .getController()
+                        				 .currentState()
+                        				 .getAttribute("elementURIFragment"))
+                        				 .getValueAsString();
+                        		}
+                        		
+                        	}
+                    	}
+                    }
+                    // calling manager.execute() would run the model
+                    // for the amount of iterations set in the director
+                }
+            }
+        } catch (Exception e) {
+        	//raise a KiemExecutionException in case of any error
+        	this.executionException = 
+        		new KiemExecutionException(e.getLocalizedMessage(),true, e);
+        }
+	}
+	
+}
---
+++ plugins/de.cau.cs.kieler.synccharts.sim.ptolemy/src/de/cau/cs/kieler/synccharts/sim/ptolemy/SyncchartsSimDataComponent.java
@@ -0,0 +1,265 @@
+/******************************************************************************
+ * KIELER - Kiel Integrated Environment for Layout for the Eclipse RCP
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2009 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ ******************************************************************************/
+
+package de.cau.cs.kieler.synccharts.sim.ptolemy;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.WorkflowContextDefaultImpl;
+import org.eclipse.emf.mwe.core.issues.Issues;
+import org.eclipse.emf.mwe.core.monitor.NullProgressMonitor;
+import org.eclipse.emf.mwe.internal.core.Workflow;
+import org.eclipse.emf.mwe.utils.Reader;
+import org.eclipse.xtend.XtendComponent;
+import org.eclipse.xtend.typesystem.emf.EmfMetaModel;
+
+import de.cau.cs.kieler.sim.kiem.data.KiemProperty;
+import de.cau.cs.kieler.sim.kiem.data.KiemPropertyException;
+import de.cau.cs.kieler.sim.kiem.data.KiemPropertyTypeWorkspaceFile;
+import de.cau.cs.kieler.sim.kiem.extension.JSONObjectDataComponent;
+import de.cau.cs.kieler.sim.kiem.extension.KiemExecutionException;
+import de.cau.cs.kieler.sim.kiem.extension.KiemInitializationException;
+import de.cau.cs.kieler.sim.kiem.json.JSONObject;
+import de.cau.cs.kieler.simplerailctrl.sim.ptolemy.oaw.MomlWriter;
+
+/**
+ * The class SimpleRailCtrl DataComponent implements a KIELER Execution Manager
+ * DataComponent.
+ * <BR>
+ * Within its {@link #initialize()} method it performs the model2model 
+ * Xtend transformation to create a semantically equivalent but executable
+ * Ptolemy model out of the SimpleRailCtrl EMF model instance. It also loads
+ * the Ptolemy model within a PtolemyExecutor and adapts the port and host
+ * for connecting to the model railway simulation engine.
+ * <BR>
+ * Within its {@link #step(JSONObject)} method it then triggers a step of
+ * the PtolemyExecutor. Because this is done asynchronously the triggering of
+ * a consecutive step may lead to an KiemExecutionError be thrown that
+ * was initially the consequence of the last (async) call to the step method
+ * of the PtolemyExecutor.
+ * 
+ * @author Christian Motika - cmot AT informatik.uni-kiel.de
+ */
+@SuppressWarnings("restriction")
+public class SyncchartsSimDataComponent extends JSONObjectDataComponent { 
+  
+	/** The Ptolemy Executor */
+	private ExecutePtolemyModel PTOEXE;
+	
+	/** The thread of the Ptolemy Executor */
+	private Thread PTOEXE_Thread;
+	
+	/** The KIELER model. */
+	private String kielerModel;
+	
+	/** The Ptolemy model. */
+	private String ptolemyModel;
+	
+	/** The current workspace folder. */
+	private String workspaceFolder;
+
+	//-------------------------------------------------------------------------
+	
+	/**
+	 * Instantiates a new SimpleRailCtrl DataComponent for the KIELER Execution
+	 * Manager.
+	 */
+	public SyncchartsSimDataComponent() {
+	}
+
+    //-------------------------------------------------------------------------
+	 
+	 /**
+     * Transform KIELER SimpleRailCtrl model into a semantically equivalent 
+     * Ptolemy model.
+     * <BR>
+     * This transformation uses the Xtend transformation language.
+     * 
+     * @param inputModel the input EMF model instance
+     * @param outputModel the output Ptolemy model
+     * 
+     * @return true, if m2m transformation was successful
+     */
+    public boolean Model2ModelTransformation(String inputModel,
+			 								 String outputModel) {
+		 	try {
+		        //Workflow
+		        Workflow workflow = new Workflow();
+		        
+		        //EMF reader
+		        Reader emfReader = new Reader();
+		        emfReader.setUri(inputModel);
+		        emfReader.setModelSlot("emfmodel");
+
+		        //MOML writer
+		        MomlWriter momlWriter = new MomlWriter();
+		        momlWriter.setUri(outputModel);
+		        momlWriter.setModelSlot("momlmodel");
+		        
+		        //Meta models
+		        EmfMetaModel metaModel1 = new EmfMetaModel(de.cau.cs.kieler.simplerailctrl.SimplerailctrlPackage.eINSTANCE);
+		        EmfMetaModel metaModel2 = new EmfMetaModel(Moml.MomlPackage.eINSTANCE);
+				
+		        //XtendComponent
+		        XtendComponent xtendComponent = new XtendComponent();
+		        xtendComponent.addMetaModel(metaModel1);
+		        xtendComponent.addMetaModel(metaModel2);
+		        xtendComponent.setInvoke("simplerailctrl2moml::transform(emfmodel)");
+		        xtendComponent.setOutputSlot("momlmodel");
+		        
+		        //workflow
+		        WorkflowContext wfx = new WorkflowContextDefaultImpl();
+		        Issues issues = new org.eclipse.emf.mwe.core.issues.IssuesImpl();
+		        NullProgressMonitor monitor = new NullProgressMonitor();
+		        
+		        workflow.addComponent(emfReader);
+		        workflow.addComponent(xtendComponent);
+		        workflow.addComponent(momlWriter);
+		        workflow.invoke(wfx, monitor, issues);
+		        
+		        System.out.print(xtendComponent.getLogMessage());
+		        System.out.print(issues.getInfos());
+		        System.out.print(issues.getIssues());
+		        System.out.print(issues.getWarnings());
+		        System.out.print(issues.getErrors().toString());
+		 	}
+		 	catch(Exception e){
+		 		return false;
+		 	} 
+	        
+		    return true;
+	}
+	 
+    //-------------------------------------------------------------------------	 
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.IJSONObjectDataComponent#step(de.cau.cs.kieler.sim.kiem.json.JSONObject)
+	 */
+	public JSONObject step(JSONObject JSONobject) throws
+												KiemExecutionException {
+		//perform an asynchronous step in PtolemyExecutor
+		//note that this may produce a KiemExecutionException which has it 
+		//source in the previous call.
+		PTOEXE.executionStep();
+		String currentState = PTOEXE.getCurrentState();
+		//the stateName is the second KIEM property
+		String stateName = this.getProperties()[1].getValue();
+		JSONObject returnObj = new JSONObject();
+		try {returnObj.accumulate(stateName,currentState);}catch(Exception e){}
+		return returnObj;
+	}
+
+    //-------------------------------------------------------------------------	 
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.IDataComponent#initialize()
+	 */
+	public void initialize() throws KiemInitializationException {
+		workspaceFolder = Platform.getLocation().toString();
+		
+		//the SimpleRailCtrl EMF model instance is the first KIEM property
+		kielerModel = this.getProperties()[0].getFilePath();
+		ptolemyModel = this.getProperties()[0].getDirectory() + "generated.moml";
+		
+		String ptolemyModelFile = workspaceFolder + ptolemyModel;
+				
+		System.out.println("Now creating Ptolemy Model ..." + ptolemyModel);
+
+		if (this.Model2ModelTransformation(kielerModel, ptolemyModel)) {
+			System.out.println("Now loading Ptolemy Model..." + ptolemyModelFile);
+			//the simulation host is the third KIEM property
+			String host = this.getProperties()[2].getValue();
+			//the simulation port is the fourth KIEM property 
+			String port = this.getProperties()[3].getValue();
+	        //load the Ptolemy Model
+	        PTOEXE = new ExecutePtolemyModel(ptolemyModelFile,host,port);
+	        PTOEXE_Thread = new Thread(PTOEXE);
+			System.out.println("Now executing Ptolemy Model...");
+	        //start the thread - it is paused by default (steps==0)
+	        PTOEXE_Thread.start();
+		}//end if
+		else 
+			new KiemInitializationException
+					("Ptolemy Model could not be generated", true, null);
+	}
+
+    //-------------------------------------------------------------------------	 
+	
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.IDataComponent#isObserver()
+	 */
+	public boolean isObserver() {
+		return true;
+	}
+
+    //-------------------------------------------------------------------------	 
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.IDataComponent#isProducer()
+	 */
+	public boolean isProducer() {
+		return true;
+	}
+
+    //-------------------------------------------------------------------------	 
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.IDataComponent#wrapup()
+	 */
+	public void wrapup() {
+		//stop the model and unlink the PtolemyExecutor thread
+		PTOEXE.executionStop();
+		PTOEXE = null;
+	}
+	 
+    //-------------------------------------------------------------------------	 
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.DataComponent#initializeProperties()
+	 */
+	@Override
+	public KiemProperty[] provideProperties() {
+		KiemProperty[] properties = new KiemProperty[4];
+		properties[0] = new KiemProperty(
+				"KIELER Model File",
+				new KiemPropertyTypeWorkspaceFile(),
+				"\\example\\default.simplerailctrl");
+		properties[1] = new KiemProperty(
+				"State Name",
+				"RAIL state");
+		properties[2] = new KiemProperty(
+				"Host",
+				"localhost");
+//				"epoch");
+		properties[3] = new KiemProperty(
+				"Port",
+				2000);
+//				5000);
+		return properties;
+	}
+
+    //-------------------------------------------------------------------------	 
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.sim.kiem.extension.DataComponent#testProperties(de.cau.cs.kieler.sim.kiem.data.KiemProperty[])
+	 */
+	@Override
+	public void checkProperties(KiemProperty[] properties) throws KiemPropertyException {
+		//check if any EMF model instance form the current workspace is selected
+		if (properties[0].getValue().trim().length() == 0) {
+			throw new KiemPropertyException("A SimpleRailCtrl-Model File must be selected!");
+		}
+	}
+	
+}
