008b6648a723365debc8264f7647321d020d2186
Miro Spoenemann <msp@informatik.uni-kiel.de>
2009-12-23 10:53:25 +0000

    workaround for #409

--- plugins/de.cau.cs.kieler.synccharts.diagram/src/de/cau/cs/kieler/synccharts/diagram/edit/parts/SyncchartsEditPartFactory.java
+++ plugins/de.cau.cs.kieler.synccharts.diagram/src/de/cau/cs/kieler/synccharts/diagram/edit/parts/SyncchartsEditPartFactory.java
@@ -12,6 +12,7 @@ import org.eclipse.gmf.runtime.draw2d.ui.figures.WrappingLabel;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Text;
 
 import de.cau.cs.kieler.synccharts.diagram.part.SyncchartsVisualIDRegistry;
@@ -183,9 +184,13 @@ public class SyncchartsEditPartFactory implements EditPartFactory {
             if (getWrapLabel().isTextWrapOn() && getWrapLabel().getText().length() > 0) {
                 rect.setSize(new Dimension(text.computeSize(rect.width, SWT.DEFAULT)));
             } else {
-                int avr = FigureUtilities.getFontMetrics(text.getFont()).getAverageCharWidth();
-                rect.setSize(new Dimension(text.computeSize(SWT.DEFAULT, SWT.DEFAULT))
-                        .expand(avr * 2, 0));
+
+                Font font = text.getFont();
+                if (!font.isDisposed()) {
+                    int avr = FigureUtilities.getFontMetrics(font).getAverageCharWidth();
+                    rect.setSize(new Dimension(text.computeSize(SWT.DEFAULT, SWT.DEFAULT)).expand(
+                            avr * 2, 0));
+                }
             }
             if (!rect.equals(new Rectangle(text.getBounds()))) {
                 text.setBounds(rect.x, rect.y, rect.width, rect.height);
@@ -224,8 +229,13 @@ public class SyncchartsEditPartFactory implements EditPartFactory {
             Text text = (Text) celleditor.getControl();
             Rectangle rect = getLabel().getTextBounds().getCopy();
             getLabel().translateToAbsolute(rect);
-            int avr = FigureUtilities.getFontMetrics(text.getFont()).getAverageCharWidth();
-            rect.setSize(new Dimension(text.computeSize(SWT.DEFAULT, SWT.DEFAULT)).expand(avr * 2, 0));
+
+            Font font = text.getFont();
+            if (!font.isDisposed()) {
+                int avr = FigureUtilities.getFontMetrics(font).getAverageCharWidth();
+                rect.setSize(new Dimension(text.computeSize(SWT.DEFAULT, SWT.DEFAULT))
+                        .expand(avr * 2, 0));
+            }
             if (!rect.equals(new Rectangle(text.getBounds()))) {
                 text.setBounds(rect.x, rect.y, rect.width, rect.height);
             }
