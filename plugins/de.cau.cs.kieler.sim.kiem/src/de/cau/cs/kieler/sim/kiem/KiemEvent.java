0e28fc6781e0c454b689ed11aebec1bd8c0a1806
Christian Motika <cmot@informatik.uni-kiel.de>
2010-01-20 08:15:58 +0000

    fixed a bug that prevents KiemEvents to work properly, changed internal typ to be int[] now #546   	 Event Identifiers are not recognized properly

--- plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/KiemEvent.java
+++ plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/KiemEvent.java
@@ -15,6 +15,7 @@
 package de.cau.cs.kieler.sim.kiem;
 
 import java.lang.reflect.Field;
+import java.math.BigInteger;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -31,87 +32,87 @@ import java.util.List;
  */
 public class KiemEvent {
 
-    private double event;
+    private int[] event;
     private Object info;
 
     /**
      * The Constant NONE event represents the default NULL event and means this component does not
      * want to be notified for any event.
      */
-    public static final double NONE = 0;
+    public static final int NONE = 0;
 
     /** The Constant STEP event is triggered before a step is about to occur. */
-    public static final double STEP_INFO = 1;
+    public static final int STEP_INFO = 1;
 
     /** The Constant CMD_STEP event is triggered when the user hits the step button. */
-    public static final double CMD_STEP = 2;
+    public static final int CMD_STEP = 2;
 
     /** The Constant CMD_STEP event is triggered when the user hits the step button. */
-    public static final double CMD_STEP_BACK = 4;
+    public static final int CMD_STEP_BACK = 3;
 
     /** The Constant CMD_STEP event is triggered when the user hits the step button. */
-    public static final double CMD_STEP_MACRO = 8;
+    public static final int CMD_STEP_MACRO = 4;
 
     /** The Constant CMD_RUN event is triggered when the user hits the run button. */
-    public static final double CMD_RUN = 16;
+    public static final int CMD_RUN = 5;
 
     /** The Constant CMD_PAUSE event is triggered when the user hits the pause button. */
-    public static final double CMD_PAUSE = 32;
+    public static final int CMD_PAUSE = 6;
 
     /** The Constant CMD_STOP event is triggered when user hits the stop button. */
-    public static final double CMD_STOP = 64;
+    public static final int CMD_STOP = 7;
 
     /** The Constant ADDED event is triggered when the user adds a component (instance). */
-    public static final double ADDED = 128;
+    public static final int ADDED = 8;
 
     /** The Constant DELETED event is triggered when the user deletes a component (instance). */
-    public static final double DELETED = 256;
+    public static final int DELETED = 9;
 
     /** The Constant SCHEDULED event is triggered when the user changes the scheduling order. */
-    public static final double SCHEDULED = 512;
+    public static final int SCHEDULED = 10;
 
     /** The Constant ACTIVATED event is triggered when the user activates the component. */
-    public static final double ACTIVATED = 1024;
+    public static final int ACTIVATED = 11;
 
     /** The Constant DEACTIVATED event is triggered when the user deactivates the component. */
-    public static final double DEACTIVATED = 2048;
+    public static final int DEACTIVATED = 12;
 
     /**
      * The Constant LOAD event is triggered when the user loads a scheduling. The info in this case
      * is the location of the file as an IPath object.
      */
-    public static final double LOAD = 4096;
+    public static final int LOAD = 13;
 
     /**
      * The Constant SAVE event is triggered when the user saves a scheduling. The info in this case
      * is the location of the file as an IPath object.
      */
-    public static final double SAVE = 8192;
+    public static final int SAVE = 14;
 
     /**
      * The Constant KIEMPROPERTY_CHANGE event is triggered when the user changes/edited a property
      * value in table.
      */
-    public static final double KIEMPROPERTY_CHANGE = 16384;
+    public static final int KIEMPROPERTY_CHANGE = 15;
 
     /** The Constant ERROR_STOP event is triggered when an error leads to the execution stop. */
-    public static final double ERROR_STOP = 32768;
+    public static final int ERROR_STOP = 16;
 
     /** The Constant ERROR_STOP event is triggered when an error leads to the execution pause. */
-    public static final double ERROR_PAUSE = 65536;
+    public static final int ERROR_PAUSE = 17;
 
     // -------------------------------------------------------------------------
 
     /**
      * This KiemEvent constructor sets the event(s) of interest during construction.
      * 
-     * @param eventParam
-     *            the event of interest
      * @param infoParam
      *            the (optional) info parameter
+     * @param eventCodesParam
+     *            the event codes param
      */
-    public KiemEvent(final double eventParam, final Object infoParam) {
-        this.event = eventParam;
+    public KiemEvent(final int[] eventCodesParam, final Object infoParam) {
+        this.event = eventCodesParam.clone();
         this.info = infoParam;
     }
 
@@ -120,11 +121,41 @@ public class KiemEvent {
     /**
      * This KiemEvent constructor sets the event(s) of interest during construction.
      * 
-     * @param eventParam
-     *            the event of interest
+     * @param eventCodesParam
+     *            the event codes param
      */
-    public KiemEvent(final double eventParam) {
-        this.event = eventParam;
+    public KiemEvent(final int[] eventCodesParam) {
+        this.event = eventCodesParam.clone();
+        this.info = null;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * This KiemEvent constructor sets the event(s) of interest during construction.
+     * 
+     * @param infoParam
+     *            the (optional) info parameter
+     * @param eventCodeParam
+     *            the event code param
+     */
+    public KiemEvent(final int eventCodeParam, final Object infoParam) {
+        this.event = new int[1];
+        this.event[0] = eventCodeParam;
+        this.info = infoParam;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * This KiemEvent constructor sets the event(s) of interest during construction.
+     * 
+     * @param eventCodeParam
+     *            the event code param
+     */
+    public KiemEvent(final int eventCodeParam) {
+        this.event = new int[1];
+        this.event[0] = eventCodeParam;
         this.info = null;
     }
 
@@ -134,7 +165,8 @@ public class KiemEvent {
      * This KiemEvent constructor sets the no event of interest during construction.
      */
     public KiemEvent() {
-        this.event = KiemEvent.NONE;
+        this.event = new int[1];
+        this.event[0] = KiemEvent.NONE;
         this.info = null;
     }
 
@@ -145,7 +177,7 @@ public class KiemEvent {
      * 
      * @return the events
      */
-    public double getEvents() {
+    public int[] getEventCodes() {
         return this.event;
     }
 
@@ -154,13 +186,18 @@ public class KiemEvent {
     /**
      * Checks whether an event is contained.
      * 
-     * @param eventParam
-     *            the event of interest to check
+     * @param eventCodeParam
+     *            the event code param
      * 
      * @return true, if is event
      */
-    public boolean isEvent(final double eventParam) {
-        return ((this.event % eventParam) == 0);
+    public boolean isEvent(final int eventCodeParam) {
+        for (int currentEvent : this.event) {
+            if (currentEvent == eventCodeParam) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // -------------------------------------------------------------------------
@@ -168,11 +205,16 @@ public class KiemEvent {
     /**
      * Sets the event.
      * 
-     * @param eventParam
-     *            the new event
+     * @param eventCodeParam
+     *            the event code param
      */
-    public void setEvent(final double eventParam) {
-        this.event = eventParam;
+    public void addEvent(final int eventCodeParam) {
+        int[] newEvent = new int[this.event.length + 1];
+        for (int c = 0; c < this.event.length; c++) {
+            newEvent[c] = this.event[c];
+        }
+        newEvent[this.event.length] = eventCodeParam;
+        this.event = newEvent;
     }
 
     // -------------------------------------------------------------------------
@@ -180,11 +222,11 @@ public class KiemEvent {
     /**
      * Adds the event.
      * 
-     * @param eventParam
-     *            the event param
+     * @param eventCodesParam
+     *            the event codes param
      */
-    public void addEvent(final double eventParam) {
-        this.event += eventParam;
+    public void setEvents(final int[] eventCodesParam) {
+        this.event = eventCodesParam.clone();
     }
 
     // -------------------------------------------------------------------------
@@ -192,8 +234,9 @@ public class KiemEvent {
     /**
      * Resets the event to NONE.
      */
-    public void resetEvent() {
-        this.event = KiemEvent.NONE;
+    public void resetEvents() {
+        this.event = new int[1];
+        this.event[0] = KiemEvent.NONE;
         this.info = null;
     }
 
@@ -223,13 +266,42 @@ public class KiemEvent {
     // -------------------------------------------------------------------------
 
     /**
+     * Gets the event name for an integer event code or null.
+     * 
+     * @param eventCode
+     *            the event code
+     * 
+     * @return the event name
+     */
+    public static String getEventName(int eventCode) {
+        Field[] flds = KiemEvent.class.getDeclaredFields();
+        for (Field f : flds) {
+            if (f.getType().getName().equals("int")) {
+                try {
+                    int currentEvent = f.getInt(f);
+                    if (currentEvent == eventCode) {
+                        return f.getName();
+                    }
+                } catch (IllegalArgumentException e) {
+                    // e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    // e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
      * Gets all events as a list.
      * 
      * @return all events as a list
      */
-    public List<Double> getEventsAsList() {
-        LinkedList<Double> returnList = new LinkedList<Double>();
-        
+    public List<Integer> getEventCodesAsList() {
+        LinkedList<Integer> returnList = new LinkedList<Integer>();
+
         int publicStaticFinalModifier = 0;
         try {
             publicStaticFinalModifier = KiemEvent.class.getDeclaredField("NONE").getModifiers();
@@ -238,18 +310,21 @@ public class KiemEvent {
         } catch (NoSuchFieldException e0) {
             e0.printStackTrace();
         }
-        
+
         Field[] flds = KiemEvent.class.getDeclaredFields();
         for (Field f : flds) {
-            //if (f.isEnumConstant()) {
-            if (f.getModifiers() == publicStaticFinalModifier) {
+            if ((f.getType().getName().equals("int"))
+                    && (f.getModifiers() == publicStaticFinalModifier)) {
                 try {
-                    System.out.println(f.getName());
-                    returnList.add(f.getDouble(f));
+                    int currentEvent = f.getInt(f);
+                    if (currentEvent > 0 && this.isEvent(currentEvent)) {
+                        System.out.println(f.getName());
+                        returnList.add(currentEvent);
+                    }
                 } catch (IllegalArgumentException e) {
-                    e.printStackTrace();
+                    // e.printStackTrace();
                 } catch (IllegalAccessException e) {
-                    e.printStackTrace();
+                    // e.printStackTrace();
                 }
             }
         }
--- plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/internal/EventManager.java
+++ plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/internal/EventManager.java
@@ -54,7 +54,7 @@ public class EventManager {
     public void add(final DataComponentWrapper dataComponentWrapper) {
         AbstractDataComponent dataComponent = dataComponentWrapper.getDataComponent();
         KiemEvent events = dataComponent.provideEventOfInterest();
-        for (double event : events.getEventsAsList()) {
+        for (double event : events.getEventCodesAsList()) {
             if (!lists.containsKey(event)) {
                 // create new event list
                 LinkedList<DataComponentWrapper> eventList = new LinkedList<DataComponentWrapper>();
@@ -80,7 +80,7 @@ public class EventManager {
     public void remove(final DataComponentWrapper dataComponentWrapper) {
         AbstractDataComponent dataComponent = dataComponentWrapper.getDataComponent();
         KiemEvent events = dataComponent.provideEventOfInterest();
-        for (double event : events.getEventsAsList()) {
+        for (double event : events.getEventCodesAsList()) {
             if (!lists.containsKey(event)) {
                 // this should not happen because a component should
                 // be registered first
@@ -109,7 +109,7 @@ public class EventManager {
      * @param event
      *            the event as an KiemEvent (integer) constant
      */
-    public void notify(final double event) {
+    public void notify(final int event) {
         notify(new KiemEvent(event));
     }
 
@@ -125,7 +125,7 @@ public class EventManager {
         LinkedList<DataComponentWrapper> doneList = new LinkedList<DataComponentWrapper>();
 
         // for all notification events
-        for (double event : events.getEventsAsList()) {
+        for (double event : events.getEventCodesAsList()) {
             if (!lists.containsKey(event)) {
                 // if no DataComponent registered for this event, no one needs to
                 // be notified --> simply return!
