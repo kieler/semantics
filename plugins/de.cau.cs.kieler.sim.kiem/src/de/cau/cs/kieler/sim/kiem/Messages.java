c45939bfa6fcd9f0b1d9d22c06e72441e1cf366c
Christian Motika <cmot@informatik.uni-kiel.de>
2010-01-26 08:23:03 +0000

    moved generated tag

--- plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/Messages.java
+++ plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/Messages.java
@@ -20,8 +20,9 @@ import org.eclipse.osgi.util.NLS;
  * The Class Messages is used to externalize strings in the messages.properties file.
  * 
  * @author Christian Motika - cmot AT informatik.uni-kiel.de
- * @generated no checkstyle
+ * 
  */
+//@generated no checkstyle
 public class Messages extends NLS {
 
     /** The Constant BUNDLE_NAME. */
