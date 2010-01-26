1d4aa408bc8a6c5a57e55ffbd097558f50bddaca
Claus Traulsen <ctr@informatik.uni-kiel.de>
2010-01-26 11:18:59 +0000

    removed shortcut, handle immediate transitions

--- plugins/de.cau.cs.kieler.synccharts.codegen.quartz/src/de/cau/cs/kieler/synccharts/codegen/quartz/Helper.java
+++ plugins/de.cau.cs.kieler.synccharts.codegen.quartz/src/de/cau/cs/kieler/synccharts/codegen/quartz/Helper.java
@@ -1,19 +1,59 @@
 package de.cau.cs.kieler.synccharts.codegen.quartz;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
+
 import de.cau.cs.kieler.synccharts.State;
+import de.cau.cs.kieler.synccharts.Transition;
 
 public class Helper {
-    public static List<State> sortStates(List<State> states) {
+    public static List<State> sortStates(final List<State> states) {
         LinkedList<State> res = new LinkedList<State>();
-        HashSet<State> todo = new HashSet<State>(states);
+        HashMap<State, Integer> todo = new HashMap<State, Integer>();
+        // Initialize List
+        
+        for(State s: states){
+            if(!todo.containsKey(s)){
+                todo.put(s, 0);
+            }
+            for(Transition t: s.getOutgoingTransitions()){
+                if(t.isIsImmediate()){
+                    Integer value = todo.get(t.getTargetState());
+                    if(value == null){
+                        value=0;
+                    }
+                    value++;
+                    if(todo.containsKey(t.getTargetState())){
+                        todo.put(t.getTargetState(), value);
+                    }
+                }
+            }
+        }
+        
         while (!todo.isEmpty()) {
-            State s = todo.iterator().next();
-            res.addFirst(s);
+            // search for state without unhandled, ingoing immediate transitions
+            State s=null;
+            for(Entry<State, Integer> e: todo.entrySet()){
+                if(e.getValue()==0){
+                    s=e.getKey();
+                    break;
+                }
+            }
+            if(s==null){
+                // Cycle in immediate transitions, should be checked before
+                break;
+            }
+            res.add(s);
             todo.remove(s);
-            // TODO add successors by BFS
+            for(Transition t: s.getOutgoingTransitions()){
+                if(t.isIsImmediate()){
+                Integer value = todo.get(t.getTargetState());               
+                value--;                
+                todo.put(t.getTargetState(), value);
+                }
+            }
         }
         return res;
     }
