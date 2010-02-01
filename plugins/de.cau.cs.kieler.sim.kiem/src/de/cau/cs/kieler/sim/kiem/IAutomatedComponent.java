e7d038c79d3d22b497de8372295df7827119e750
Soeren Hansen <soh@informatik.uni-kiel.de>
2010-02-01 16:38:15 +0000

    changed boolean to int values to better estimate runs a priori

--- plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/IAutomatedComponent.java
+++ plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/IAutomatedComponent.java
@@ -18,13 +18,13 @@ import java.util.List;
 import de.cau.cs.kieler.sim.kiem.properties.KiemProperty;
 
 /**
- * Interface for components that want to interact
- * with the automated execution plugin.
+ * Interface for components that want to interact with the automated execution
+ * plugin.
  * 
  * @author soh
  */
 public interface IAutomatedComponent {
-    
+
     /** Identifier for the model file. */
     String MODEL_FILE = "Model file";
     /** Identifier for the iteration. */
@@ -33,35 +33,52 @@ public interface IAutomatedComponent {
     String STATUS = "Status";
 
     /**
-     * Give the component information.
-     * This at least contains the following properties:
-     * {@link #MODEL_FILE} gotten from IPath.toOSString()
+     * Give the component information. This at least contains the following
+     * properties: {@link #MODEL_FILE} gotten from IPath.toOSString()
      * {@link #ITERATION} as an integer starting with 0 of course
      * 
      * 
-     * @param properties some info
+     * @param properties
+     *            some info
      */
     void setParameters(List<KiemProperty> properties);
-    
-    
+
     /**
-     * Ask the component if it wants to do another run.
-     * This causes the entire execution to be executed again
-     * with an incremented index.
+     * Ask the component if it wants to do another run. This causes the entire
+     * execution to be executed again with an incremented index.
      * 
      * @return true if the component wants another run.
+     * @deprecated use wantsMoreRuns() instead
      */
+    @Deprecated
     boolean wantsAnotherRun();
-    
+
     /**
-     * Ask the component if it wants to do another step.
-     * As soon as all components answer with false the execution
-     * is stopped, the producers are asked to supply data
-     * and the components will be asked if they want
+     * Ask the component if it wants to do another step. As soon as all
+     * components answer with false the execution is stopped, the producers are
+     * asked to supply data and the components will be asked if they want
      * another run.
      * 
      * @return true if another step should be performed
+     * @deprecated use wantsMoreSteps() instead
      */
+    @Deprecated
     boolean wantsNextStep();
+
+    /**
+     * Ask the component if it wants to do more runs. This causes the entire
+     * execution to be executed again as many times as the components specified.
+     * 
+     * @return the number of additional runs
+     */
+    int wantsMoreRuns();
+
+    /**
+     * Ask the component if it wants to do more steps. This causes the entire
+     * execution to perform at least as many steps as the components specified.
+     * 
+     * @return the number of additional steps
+     */
+    int wantsMoreSteps();
 
 }
