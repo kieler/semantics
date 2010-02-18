a44f68461ef8e1afdbb3876c3b7203e799626e03
Claas Anders Rathje <car@informatik.uni-kiel.de>
2010-02-18 17:06:55 +0000

    Try to unit test commands for undo-verification. Mainly committed to synchronize with uru as discussion base.

--- plugins/de.cau.cs.kieler.synccharts.interfacedeclparser.bridge/src/de/cau/cs/kieler/synccharts/interfacedeclparser/bridge/test/InterfaceDeclParserTests.java
+++ plugins/de.cau.cs.kieler.synccharts.interfacedeclparser.bridge/src/de/cau/cs/kieler/synccharts/interfacedeclparser/bridge/test/InterfaceDeclParserTests.java
@@ -17,8 +17,14 @@ package de.cau.cs.kieler.synccharts.interfacedeclparser.bridge.test;
 import java.io.IOException;
 import java.util.List;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.common.ui.services.parser.IParser;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ITextAwareEditPart;
+import org.eclipse.ui.internal.commands.WorkbenchCommandSupport;
 import org.eclipse.xtext.parser.antlr.IAntlrParser;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,6 +37,7 @@ import de.cau.cs.kieler.synccharts.Region;
 import de.cau.cs.kieler.synccharts.Signal;
 import de.cau.cs.kieler.synccharts.State;
 import de.cau.cs.kieler.synccharts.SyncchartsFactory;
+import de.cau.cs.kieler.synccharts.SyncchartsPackage;
 import de.cau.cs.kieler.synccharts.Variable;
 import de.cau.cs.kieler.synccharts.interfacedeclparser.InterfaceDeclStandaloneSetup;
 import de.cau.cs.kieler.synccharts.interfacedeclparser.bridge.InterfaceDeclParseCommand;
@@ -50,6 +57,7 @@ public class InterfaceDeclParserTests {
     private State rootState;
     private Injector injector;
     private IAntlrParser parser;
+    InterfaceDeclProcessorWrapper idpw;
 
     private static final int A = 65;
     private static final int Z = 90;
@@ -83,6 +91,7 @@ public class InterfaceDeclParserTests {
         injector = new InterfaceDeclStandaloneSetup()
                 .createInjectorAndDoEMFRegistration();
         parser = injector.getInstance(IAntlrParser.class);
+        idpw = new InterfaceDeclProcessorWrapper();
     }
 
     @Test
@@ -116,7 +125,6 @@ public class InterfaceDeclParserTests {
         }
     }
 
-    
     @Test
     public void testMoreSignals() throws Exception {
         String prefix = "magicTest";
@@ -143,7 +151,7 @@ public class InterfaceDeclParserTests {
                 throw new KielerException("Missing var " + var.getName()
                         + " in serialized String.");
         }
-        //System.out.println(rootState.getInterfaceDeclaration());
+        // System.out.println(rootState.getInterfaceDeclaration());
         // test them all again
         for (int i = A; i <= Z; i++) { // ASCII letters A to Z
             char[] letters = Character.toChars(i);
@@ -156,6 +164,59 @@ public class InterfaceDeclParserTests {
                 throw new KielerException("Missing var " + prefix + "var"
                         + letter + " in serialized String.");
         }
+    }
+
+    // TODO: this is not the way
+    @Test
+    public void undoTestParse() throws Exception {
+        rootState.setInterfaceDeclaration("//comment");
+        Command parseCommand = idpw.getParseCommand(rootState);
+        parseCommand.execute();
+        // System.out.println(rootState.getSignals().size() + " Sigs");
+        rootState.setInterfaceDeclaration("//comment\nsignal somesig;");
+        Command parseCommand2 = idpw.getParseCommand(rootState);
+        parseCommand2.execute();
+        // System.out.println(rootState.getSignals().size() + " Sigs");
+        parseCommand2.undo();
+        // System.out.println(rootState.getSignals().size() + " Sigs");
+    }
+
+    @Test
+    public void undoTestSerialize() throws Exception {
+        // rootState.eGet(SyncchartsPackage.STATE__INTERFACE_DECLARATION);
+
+        /*
+         * IParser p = ((ITextAwareEditPart) editPart).getParser(); // only edit
+         * if empty, other behaviors like append would also be possible if
+         * (p.getEditString(editPart, 0).length() == 0) { ICommand c =
+         * p.getParseCommand(editPart, insertIfEmpty, 0); try { c.execute(null,
+         * null); } catch (ExecutionException e) { // if it fails we do not want
+         * to nag the user } }
+         */
+
+        // serialize empty state
+        Command c1 = idpw.getCanonialSerializeCommand(rootState);
+        c1.execute();
+        System.out.println("#" + rootState.getInterfaceDeclaration() + "#"
+                + rootState.getSignals().size());
+        Signal sig1 = generateRandomSignal("sig1");
+        rootState.getSignals().add(sig1);
+        Command c2 = idpw.getCanonialSerializeCommand(rootState);
+        c2.execute();
+        System.out.println("#" + rootState.getInterfaceDeclaration() + "#"
+                + rootState.getSignals().size());
+        c2.undo();
+        System.out.println("#" + rootState.getInterfaceDeclaration() + "#"
+                + rootState.getSignals().size());
+        Signal sig2 = generateRandomSignal("sig2");
+        rootState.getSignals().add(sig2);
+        c2 = idpw.getCanonialSerializeCommand(rootState);
+        c2.execute();
+        System.out.println("#" + rootState.getInterfaceDeclaration() + "#"
+                + rootState.getSignals().size());
+        c2.undo();
+        System.out.println("#" + rootState.getInterfaceDeclaration() + "#"
+                + rootState.getSignals().size());
     }
 
     private Signal generateRandomSignal(String name) {
@@ -215,14 +276,12 @@ public class InterfaceDeclParserTests {
     private void parse(final String stringToParse) throws KielerException,
             IOException {
         rootState.setInterfaceDeclaration(stringToParse);
-        InterfaceDeclParseCommand parseCommand = new InterfaceDeclParseCommand(
-                rootState, injector);
+        Command parseCommand = idpw.getParseCommand(rootState);
         parseCommand.execute();
     }
 
     String serialize(final State state) {
-        InterfaceDeclProcessorWrapper foo = new InterfaceDeclProcessorWrapper();
-        Command c = foo.getCanonialSerializeCommand(state);
+        Command c = idpw.getCanonialSerializeCommand(state);
         c.execute();
         return state.getInterfaceDeclaration();
     }
