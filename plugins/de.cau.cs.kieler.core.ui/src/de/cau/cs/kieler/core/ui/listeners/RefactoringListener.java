0b507bbd9e289b3826329c2e7bdba7bba27a281b
Soeren Hansen <soh@informatik.uni-kiel.de>
2010-03-09 09:15:19 +0000

    fixed listener to include diagram files that have names different from the model file

--- plugins/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/listeners/RefactoringListener.java
+++ plugins/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/listeners/RefactoringListener.java
@@ -13,7 +13,19 @@
  */
 package de.cau.cs.kieler.core.ui.listeners;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -36,6 +48,28 @@ import org.eclipse.ltk.core.refactoring.resource.RenameResourceDescriptor;
  * @kieler.rating 2010-02-18 proposed yellow soh
  */
 public class RefactoringListener implements IRefactoringHistoryListener {
+
+    /** File extension for diagram files. */
+    private static final String DIAGRAM_EXTENSION = "kids";
+
+    /** File extension for model files. */
+    private static final String MODEL_EXTENSION = "kixs";
+
+    /**
+     * Contains the different refactoring operations.
+     * 
+     * @author soh
+     */
+    private enum OP {
+        /** Indicates a delete action. */
+        DELETE,
+
+        /** Indicates a move. */
+        MOVE,
+
+        /** Indicates a renaming. */
+        RENAME;
+    }
 
     /**
      * {@inheritDoc}
@@ -78,31 +112,117 @@ public class RefactoringListener implements IRefactoringHistoryListener {
         IPath[] locations = deleteDesc.getResourcePaths();
 
         for (IPath path : locations) {
-            File partner = getPartner(path);
-            if (partner != null) {
-                partner.delete();
-            }
+            // File partner = getPartner(path);
+            // if (partner != null) {
+            // partner.delete();
+            // }
+            executeOperation(path, OP.DELETE, null);
         }
     }
 
     /**
-     * Calculates the partner to a synccharts model file.
+     * Execute a refactoring operation.
      * 
      * @param path
-     *            the .kixs file
-     * @return the .kids file
+     *            the old path of the file that was refactored.
+     * @param op
+     *            the operation
+     * @param newName
+     *            optionally the new name of the file
+     * @return the list of files that link to the refactored file
      */
-    private File getPartner(final IPath path) {
-        File result = null;
-        if (path.getFileExtension().equals("kixs")) {
-            IPath partnerPath = path.removeFileExtension().addFileExtension(
-                    "kids");
-            String location = Platform.getLocation().toOSString()
-                    + partnerPath.toOSString();
-            IPath partnerLocation = Path.fromOSString(location);
-            result = partnerLocation.toFile();
+    private List<File> executeOperation(final IPath path, final OP op,
+            final String newName) {
+        List<File> result = new LinkedList<File>();
+        if (path.getFileExtension().equals(MODEL_EXTENSION)) {
+            findRec(result, Platform.getLocation().toFile(), path, op, newName);
+
         }
         return result;
+    }
+
+    /**
+     * Internal method to recursively search the whole workspace.
+     * 
+     * @param result
+     *            the list of affected files.
+     * @param root
+     *            the root file
+     * @param model
+     *            the model file
+     * @param op
+     *            the operation
+     * @param newName
+     *            optionally the new name of the file
+     */
+    private void findRec(final List<File> result, final File root,
+            final IPath model, final OP op, final String newName) {
+        if (root.isDirectory()) {
+            // recursively look through all files in the directory
+            for (File file : root.listFiles()) {
+                findRec(result, file, model, op, newName);
+            }
+        } else if (root.getPath().endsWith(DIAGRAM_EXTENSION)) {
+            // found relevant file
+            try {
+                InputStream is = new FileInputStream(root);
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+
+                // list where the new content of the file is build
+                List<String> list = new LinkedList<String>();
+
+                // line that is read at the moment
+                String s = br.readLine();
+                boolean done = false;
+
+                // search the file for references to the model file
+                while (s != null && !done) {
+                    if (s.contains(model.toFile().getName())) {
+                        switch (op) {
+                        case DELETE:
+                            root.delete();
+                            done = true;
+                            break;
+                        case RENAME:
+                            // replace the old name with the new one
+                            s = s.replaceAll(model.toFile().getName(), newName);
+                            break;
+                        case MOVE:
+                            done = true;
+                            break;
+                        }
+                    }
+                    list.add(s);
+                    if (!done) {
+                        s = br.readLine();
+                    }
+                }
+
+                // close streams
+                br.close();
+                isr.close();
+                is.close();
+
+                if (op.equals(OP.RENAME)) {
+                    // write information back into the file
+                    OutputStream os = new FileOutputStream(root);
+                    OutputStreamWriter osw = new OutputStreamWriter(os);
+                    BufferedWriter bw = new BufferedWriter(osw);
+
+                    for (String string : list) {
+                        bw.write(string);
+                        bw.flush();
+                    }
+                }
+
+            } catch (FileNotFoundException e0) {
+                e0.printStackTrace();
+            } catch (IOException e0) {
+                e0.printStackTrace();
+            }
+
+        }
     }
 
     /**
@@ -117,15 +237,18 @@ public class RefactoringListener implements IRefactoringHistoryListener {
         IPath dest = moveDesc.getDestinationPath();
 
         for (IPath path : src) {
-            File partner = getPartner(path);
-            if (partner != null) {
-                IPath srcName = path.removeFileExtension();
-                String name = srcName.segment(srcName.segmentCount() - 1);
-                name = dest.toOSString() + IPath.SEPARATOR + name + ".kids";
-                IPath newPath = Path.fromOSString(Platform.getLocation()
-                        .toOSString()
-                        + name);
-                partner.renameTo(newPath.toFile());
+            List<File> list = executeOperation(path, OP.MOVE, null);
+            for (File partner : list) {
+                if (partner != null) {
+                    IPath srcName = path.removeFileExtension();
+                    String name = srcName.segment(srcName.segmentCount() - 1);
+                    name = dest.toOSString() + IPath.SEPARATOR + name + "."
+                            + DIAGRAM_EXTENSION;
+                    IPath newPath = Path.fromOSString(Platform.getLocation()
+                            .toOSString()
+                            + name);
+                    partner.renameTo(newPath.toFile());
+                }
             }
         }
     }
@@ -140,17 +263,18 @@ public class RefactoringListener implements IRefactoringHistoryListener {
     private void renameFile(final RenameResourceDescriptor renameDesc) {
         IPath location = renameDesc.getResourcePath();
         String newName = renameDesc.getNewName();
-        File partner = getPartner(location);
-        if (partner != null) {
-            String newPath = location.removeFileExtension().removeLastSegments(
-                    1).addTrailingSeparator().toOSString()
-                    + newName;
-            IPath newLocation = Path.fromOSString(Platform.getLocation()
-                    .toOSString()
-                    + newPath);
-            IPath partnerPath = newLocation.removeFileExtension()
-                    .addFileExtension("kids");
-            partner.renameTo(partnerPath.toFile());
-        }
+        executeOperation(location, OP.RENAME, newName);
+        // File partner = getPartner(location);
+        // if (partner != null) {
+        // String newPath = location.removeFileExtension().removeLastSegments(
+        // 1).addTrailingSeparator().toOSString()
+        // + newName;
+        // IPath newLocation = Path.fromOSString(Platform.getLocation()
+        // .toOSString()
+        // + newPath);
+        // IPath partnerPath = newLocation.removeFileExtension()
+        // .addFileExtension("kids");
+        // partner.renameTo(partnerPath.toFile());
+        // }
     }
 }
