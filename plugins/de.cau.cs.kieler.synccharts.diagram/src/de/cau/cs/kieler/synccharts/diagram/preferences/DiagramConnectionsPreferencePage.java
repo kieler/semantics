e4cd10fb63404a3514c047f5a34bfa7d181137c6
Christian Schneider <chsch@informatik.uni-kiel.de>
2010-03-10 14:46:07 +0000

    trunk: spline mode default setup repaired

--- plugins/de.cau.cs.kieler.synccharts.diagram/src/de/cau/cs/kieler/synccharts/diagram/preferences/DiagramConnectionsPreferencePage.java
+++ plugins/de.cau.cs.kieler.synccharts.diagram/src/de/cau/cs/kieler/synccharts/diagram/preferences/DiagramConnectionsPreferencePage.java
@@ -3,7 +3,6 @@ package de.cau.cs.kieler.synccharts.diagram.preferences;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.preferences.ConnectionsPreferencePage;
 
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.draw2d.Connection;
 import org.eclipse.gef.EditPart;
@@ -25,102 +24,96 @@ import de.cau.cs.kieler.synccharts.diagram.part.SyncchartsDiagramEditorPlugin;
  */
 public class DiagramConnectionsPreferencePage extends ConnectionsPreferencePage {
 
-    /**
-     * @generated
-     */
-    public DiagramConnectionsPreferencePage() {
-        setPreferenceStore(SyncchartsDiagramEditorPlugin.getInstance().getPreferenceStore());
-    }
+	/**
+	 * @generated
+	 */
+	public DiagramConnectionsPreferencePage() {
+		setPreferenceStore(SyncchartsDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
 
-    /**
-     * @generated
-     */
-    private String SPLINE_LABEL = "Spline Mode:";
-    /**
-     * @generated
-     */
-    private ComboFieldEditor splineModeFieldEditor = null;
+	/**
+	 * @generated
+	 */
+	private String SPLINE_LABEL = "Spline Mode:";
+	/**
+	 * @generated
+	 */
+	private ComboFieldEditor splineModeFieldEditor = null;
 
-    /**
-     * @generated
-     */
-    protected void addFieldEditors(Composite composite) {
-        super.addFieldEditors(composite);
+	/**
+	 * @generated
+	 */
+	protected void addFieldEditors(Composite composite) {
+		super.addFieldEditors(composite);
 
-        // spline mode
-        splineModeFieldEditor = new ComboFieldEditor(SplineConnection.PREF_SPLINE_MODE,
-                SPLINE_LABEL, composite, ComboFieldEditor.INT_TYPE, true, 0, 0, true);
-        splineModeFieldEditor.autoStorage = true;
-        addField(splineModeFieldEditor);
-        Combo splineModeCombo = splineModeFieldEditor.getComboControl();
-        splineModeCombo.add("Off");
-        splineModeCombo.add("On (cubic sections)");
-        splineModeCombo.add("On (cubic sections approximation)");
-    }
+		// spline mode
+		splineModeFieldEditor = new ComboFieldEditor(
+				SplineConnection.PREF_SPLINE_MODE, SPLINE_LABEL, composite,
+				ComboFieldEditor.INT_TYPE, true, 0, 0, true);
+		splineModeFieldEditor.autoStorage = true;
+		addField(splineModeFieldEditor);
+		Combo splineModeCombo = splineModeFieldEditor.getComboControl();
+		splineModeCombo.add("Off");
+		splineModeCombo.add("On (cubic sections)");
+		splineModeCombo.add("On (cubic sections approximation)");
+	}
 
-    /**
-     * @generated
-     */
-    public static void initDefaults(IPreferenceStore preferenceStore) {
-        ConnectionsPreferencePage.initDefaults(preferenceStore);
-        preferenceStore.setDefault(SplineConnection.PREF_SPLINE_MODE, SplineConnection.SPLINE_OFF);
-    }
+	/**
+	 * @generated
+	 */
+	@Override
+	public boolean performOk() {
+		boolean ok = super.performOk();
+		IWorkbench wb = PlatformUI.getWorkbench();
+		if (wb != null) {
+			IWorkbenchWindow wbw = wb.getActiveWorkbenchWindow();
+			if (wbw != null) {
+				IWorkbenchPage wbp = wbw.getActivePage();
+				if (wbp != null) {
+					IEditorReference[] ers = wbp.getEditorReferences();
+					for (IEditorReference er : ers) {
+						IEditorPart editor = er.getEditor(true);
+						if (editor instanceof SyncchartsDiagramEditor) {
+							SyncchartsDiagramEditor sde = (SyncchartsDiagramEditor) editor;
+							applySplineMode(sde.getDiagramEditPart());
+						}
+					}
+				}
+			}
+		}
+		return ok;
+	}
 
-    /**
-     * @generated
-     */
-    @Override
-    public boolean performOk() {
-        boolean ok = super.performOk();
-        IWorkbench wb = PlatformUI.getWorkbench();
-        if (wb != null) {
-            IWorkbenchWindow wbw = wb.getActiveWorkbenchWindow();
-            if (wbw != null) {
-                IWorkbenchPage wbp = wbw.getActivePage();
-                if (wbp != null) {
-                    IEditorReference[] ers = wbp.getEditorReferences();
-                    for (IEditorReference er : ers) {
-                        IEditorPart editor = er.getEditor(true);
-                        if (editor instanceof SyncchartsDiagramEditor) {
-                            SyncchartsDiagramEditor sde = (SyncchartsDiagramEditor) editor;
-                            applySplineMode(sde.getDiagramEditPart());
-                        }
-                    }
-                }
-            }
-        }
-        return ok;
-    }
-
-    /**
-     * @generated
-     */
-    protected void applySplineMode(EditPart part) {
-        if (part instanceof TransitionEditPart) {
-            Connection c = ((TransitionEditPart) part).getConnectionFigure();
-            if (c instanceof SplineConnection) {
-                ((SplineConnection) c).setSplineMode(getPreferenceStore().getInt(
-                        SplineConnection.PREF_SPLINE_MODE));
-            }
-        }
-        if (part instanceof ShapeNodeEditPart) {
-            ShapeNodeEditPart state = (ShapeNodeEditPart) part;
-            for (Object so : state.getSourceConnections()) {
-                if (so instanceof EditPart) {
-                    applySplineMode((EditPart) so);
-                }
-            }
-            for (Object to : state.getSourceConnections()) {
-                if (to instanceof EditPart) {
-                    applySplineMode((EditPart) to);
-                }
-            }
-        }
-        for (Object child : part.getChildren()) {
-            if (child instanceof EditPart) {
-                applySplineMode((EditPart) child);
-            }
-        }
-    }
+	/**
+	 * @generated
+	 */
+	protected void applySplineMode(EditPart part) {
+		if (part instanceof TransitionEditPart) {
+			Connection c = ((TransitionEditPart) part).getConnectionFigure();
+			if (c instanceof SplineConnection) {
+				((SplineConnection) c).setSplineMode(getPreferenceStore()
+						.getInt(SplineConnection.PREF_SPLINE_MODE));
+			}
+		}
+		if (part instanceof ShapeNodeEditPart) {
+			ShapeNodeEditPart state = (ShapeNodeEditPart) part;
+			for (Object so : state.getSourceConnections()) {
+				if (so instanceof EditPart) {
+					applySplineMode((EditPart) so);
+				}
+			}
+			for (Object to : state.getSourceConnections()) {
+				if (to instanceof EditPart) {
+					applySplineMode((EditPart) to);
+				}
+			}
+		}
+		for (Object child : part.getChildren()) {
+			if (child instanceof EditPart) {
+				applySplineMode((EditPart) child);
+			}
+		}
+	}
 
 }
