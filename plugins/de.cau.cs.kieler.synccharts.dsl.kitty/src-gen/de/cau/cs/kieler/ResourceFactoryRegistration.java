b70d846a516eb80b894be27e186c27e7ae0c988c
Ozgun Bayramoglu <oba@informatik.uni-kiel.de>
2010-03-18 13:13:07 +0000

    removed kitty

--- plugins/de.cau.cs.kieler.synccharts.dsl.kitty/src-gen/de/cau/cs/kieler/ResourceFactoryRegistration.java
+++
@@ -1,9 +0,0 @@
-package de.cau.cs.kieler;
-
-import de.cau.cs.kieler.resource.kittyResourceFactory;
-
-public class ResourceFactoryRegistration {
-	public static void register() {
-	  	kittyResourceFactory.register();
-	}
-}
