82dfb8db94fb03a85b23fa36c4ccef73ef434898
Soeren Hansen <soh@informatik.uni-kiel.de>
2010-06-01 07:24:37 +0000

    restricted toggling of problem markers to diagram editors

---
+++ plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/util/KielerModelPropertyTester.java
@@ -0,0 +1,39 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ *
+ * Copyright 2009 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ *
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.core.model.util;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+
+/**
+ * This tester tests whether the active editor is a diagram editor and thus
+ * problem markers can be toggled.
+ * 
+ * @author soh
+ */
+public class KielerModelPropertyTester extends PropertyTester {
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean test(final Object receiver, final String property,
+            final Object[] args, final Object expectedValue) {
+        if (property.equals("isDiagramEditor")
+                && receiver instanceof DiagramEditor) {
+            return true;
+        }
+        return false;
+    }
+
+}
