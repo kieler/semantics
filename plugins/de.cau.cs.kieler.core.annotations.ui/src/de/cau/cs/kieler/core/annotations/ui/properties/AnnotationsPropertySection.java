a75181c2b45be54cf3dfd586e7c5224b9c801a84
Matthias Schmeling <schm@informatik.uni-kiel.de>
2010-06-02 15:43:00 +0000

    Experimented with property tabs.

---
+++ plugins/de.cau.cs.kieler.core.annotations.ui/src/de/cau/cs/kieler/core/annotations/ui/properties/AnnotationsPropertySection.java
@@ -0,0 +1,7 @@
+package de.cau.cs.kieler.core.annotations.ui.properties;
+
+import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
+
+public class AnnotationsPropertySection extends AbstractPropertySection {
+
+}
