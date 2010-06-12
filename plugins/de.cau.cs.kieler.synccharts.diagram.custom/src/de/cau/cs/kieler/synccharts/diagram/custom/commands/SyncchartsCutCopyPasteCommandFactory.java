5ff9fd2581c35a22b3f06d071d2a9cd54d6b73b7
Christian Motika <cmot@informatik.uni-kiel.de>
2010-06-12 10:39:56 +0000

    initializeTransformationCommand was not defined

--- plugins/de.cau.cs.kieler.synccharts.diagram.custom/src/de/cau/cs/kieler/synccharts/diagram/custom/commands/SyncchartsCutCopyPasteCommandFactory.java
+++ plugins/de.cau.cs.kieler.synccharts.diagram.custom/src/de/cau/cs/kieler/synccharts/diagram/custom/commands/SyncchartsCutCopyPasteCommandFactory.java
@@ -13,11 +13,17 @@
  */
 package de.cau.cs.kieler.synccharts.diagram.custom.commands;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.ui.IEditorPart;
 import org.osgi.framework.Bundle;
 
+import de.cau.cs.kieler.core.model.transformation.ITransformationFramework;
 import de.cau.cs.kieler.kiml.ui.layout.DiagramLayoutManager;
+import de.cau.cs.kieler.ksbase.ui.handler.TransformationCommand;
 import de.cau.cs.kieler.ksbase.ui.utils.AbstractCutCopyPasteCommandFactory;
 import de.cau.cs.kieler.synccharts.Region;
 import de.cau.cs.kieler.synccharts.State;
@@ -50,7 +56,6 @@ public class SyncchartsCutCopyPasteCommandFactory extends
         return SyncchartsDiagramCustomPlugin.instance.getBundle();
     }
 
-    @Override
     protected Class<?>[] getTypes() {
         return TYPES;
     }
@@ -91,5 +96,13 @@ public class SyncchartsCutCopyPasteCommandFactory extends
     @Override
     protected String getModel() {
         return MODEL;
+    }
+
+    @Override
+    protected void initializeTransformationCommand(TransformationCommand result,
+            DiagramEditor editor, List<EObject> selection, String label, String filePathParam,
+            String modelParam, ITransformationFramework framework) {
+        // TODO Auto-generated method stub
+        
     }
 }
