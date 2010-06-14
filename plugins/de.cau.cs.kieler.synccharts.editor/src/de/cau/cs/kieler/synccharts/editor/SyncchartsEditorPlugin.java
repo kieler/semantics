87167ffcf92f3f0617a18c479755043b38613653
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2010-06-14 12:59:18 +0000

    merged synccharts branch with trunk

--- plugins/de.cau.cs.kieler.synccharts.editor/src/de/cau/cs/kieler/synccharts/editor/SyncchartsEditorPlugin.java
+++ plugins/de.cau.cs.kieler.synccharts.editor/src/de/cau/cs/kieler/synccharts/editor/SyncchartsEditorPlugin.java
@@ -1,10 +1,19 @@
 /**
- * <copyright>
- * </copyright>
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ * 
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2009 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
  *
  * $Id$
  */
-package de.cau.cs.kieler.synccharts.presentation;
+package de.cau.cs.kieler.synccharts.editor;
 
 import org.eclipse.emf.common.EMFPlugin;
 
@@ -19,77 +28,77 @@ import org.eclipse.emf.common.util.ResourceLocator;
  * @generated
  */
 public final class SyncchartsEditorPlugin extends EMFPlugin {
-    /**
+	/**
 	 * Keep track of the singleton.
 	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-    public static final SyncchartsEditorPlugin INSTANCE = new SyncchartsEditorPlugin();
-    
-    /**
+	public static final SyncchartsEditorPlugin INSTANCE = new SyncchartsEditorPlugin();
+	
+	/**
 	 * Keep track of the singleton.
 	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-    private static Implementation plugin;
+	private static Implementation plugin;
 
-    /**
+	/**
 	 * Create the instance.
 	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-    public SyncchartsEditorPlugin() {
+	public SyncchartsEditorPlugin() {
 		super
 			(new ResourceLocator [] {
 			});
 	}
 
-    /**
+	/**
 	 * Returns the singleton instance of the Eclipse plugin.
 	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+	 * <!-- end-user-doc -->
 	 * @return the singleton instance.
 	 * @generated
 	 */
-    @Override
-    public ResourceLocator getPluginResourceLocator() {
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
 		return plugin;
 	}
-    
-    /**
+	
+	/**
 	 * Returns the singleton instance of the Eclipse plugin.
 	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+	 * <!-- end-user-doc -->
 	 * @return the singleton instance.
 	 * @generated
 	 */
-    public static Implementation getPlugin() {
+	public static Implementation getPlugin() {
 		return plugin;
 	}
-    
-    /**
+	
+	/**
 	 * The actual implementation of the Eclipse <b>Plugin</b>.
 	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-    public static class Implementation extends EclipseUIPlugin {
-        /**
+	public static class Implementation extends EclipseUIPlugin {
+		/**
 		 * Creates an instance.
 		 * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
+		 * <!-- end-user-doc -->
 		 * @generated
 		 */
-        public Implementation() {
+		public Implementation() {
 			super();
 	
 			// Remember the static instance.
 			//
 			plugin = this;
 		}
-    }
+	}
 
 }
