6c1d4f1a246f19ec190831368cc093280f090f55
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2010-06-16 13:46:52 +0000

    added initial expressions projects

---
+++ plugins/de.cau.cs.kieler.core.expressions.edit/src/de/cau/cs/kieler/core/expressions/provider/VariableItemProvider.java
@@ -0,0 +1,248 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.cau.cs.kieler.core.expressions.provider;
+
+
+import de.cau.cs.kieler.core.expressions.ExpressionsFactory;
+import de.cau.cs.kieler.core.expressions.ExpressionsPackage;
+import de.cau.cs.kieler.core.expressions.Variable;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+/**
+ * This is the item provider adapter for a {@link de.cau.cs.kieler.core.expressions.Variable} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class VariableItemProvider
+    extends ValuedObjectItemProvider
+    implements
+        IEditingDomainItemProvider,
+        IStructuredItemContentProvider,
+        ITreeItemContentProvider,
+        IItemLabelProvider,
+        IItemPropertySource {
+    /**
+     * This constructs an instance from a factory and a notifier.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public VariableItemProvider(AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
+
+    /**
+     * This returns the property descriptors for the adapted class.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+        if (itemPropertyDescriptors == null) {
+            super.getPropertyDescriptors(object);
+
+            addConstPropertyDescriptor(object);
+        }
+        return itemPropertyDescriptors;
+    }
+
+    /**
+     * This adds a property descriptor for the Const feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected void addConstPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add
+            (createItemPropertyDescriptor
+                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                 getResourceLocator(),
+                 getString("_UI_Variable_const_feature"),
+                 getString("_UI_PropertyDescriptor_description", "_UI_Variable_const_feature", "_UI_Variable_type"),
+                 ExpressionsPackage.Literals.VARIABLE__CONST,
+                 true,
+                 false,
+                 false,
+                 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
+                 null,
+                 null));
+    }
+
+    /**
+     * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+     * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+     * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+        if (childrenFeatures == null) {
+            super.getChildrenFeatures(object);
+            childrenFeatures.add(ExpressionsPackage.Literals.VARIABLE__VALUE);
+        }
+        return childrenFeatures;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EStructuralFeature getChildFeature(Object object, Object child) {
+        // Check the type of the specified child object and return the proper feature to use for
+        // adding (see {@link AddCommand}) it as a child.
+
+        return super.getChildFeature(object, child);
+    }
+
+    /**
+     * This returns Variable.gif.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public Object getImage(Object object) {
+        return overlayImage(object, getResourceLocator().getImage("full/obj16/Variable"));
+    }
+
+    /**
+     * This returns the label text for the adapted class.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public String getText(Object object) {
+        String label = ((Variable)object).getName();
+        return label == null || label.length() == 0 ?
+            getString("_UI_Variable_type") :
+            getString("_UI_Variable_type") + " " + label;
+    }
+
+    /**
+     * This handles model notifications by calling {@link #updateChildren} to update any cached
+     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public void notifyChanged(Notification notification) {
+        updateChildren(notification);
+
+        switch (notification.getFeatureID(Variable.class)) {
+            case ExpressionsPackage.VARIABLE__CONST:
+                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+                return;
+            case ExpressionsPackage.VARIABLE__VALUE:
+                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+                return;
+        }
+        super.notifyChanged(notification);
+    }
+
+    /**
+     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+     * that can be created under this object.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+        super.collectNewChildDescriptors(newChildDescriptors, object);
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createExpression()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createComplexExpression()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createTextualCode()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createValuedObject()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createSignal()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createSignalReference()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createVariable()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createVariableReference()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createValue()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createIntValue()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createFloatValue()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createBooleanValue()));
+
+        newChildDescriptors.add
+            (createChildParameter
+                (ExpressionsPackage.Literals.VARIABLE__VALUE,
+                 ExpressionsFactory.eINSTANCE.createOperatorExpression()));
+    }
+
+}
---
+++ plugins/de.cau.cs.kieler.core.expressions/src/de/cau/cs/kieler/core/expressions/impl/SignalImpl.java
@@ -0,0 +1,410 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.cau.cs.kieler.core.expressions.impl;
+
+import de.cau.cs.kieler.core.expressions.CombineOperator;
+import de.cau.cs.kieler.core.expressions.ExpressionsPackage;
+import de.cau.cs.kieler.core.expressions.Signal;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Signal</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link de.cau.cs.kieler.core.expressions.impl.SignalImpl#getValue <em>Value</em>}</li>
+ *   <li>{@link de.cau.cs.kieler.core.expressions.impl.SignalImpl#isIsInput <em>Is Input</em>}</li>
+ *   <li>{@link de.cau.cs.kieler.core.expressions.impl.SignalImpl#isIsOutput <em>Is Output</em>}</li>
+ *   <li>{@link de.cau.cs.kieler.core.expressions.impl.SignalImpl#getCombineOperator <em>Combine Operator</em>}</li>
+ *   <li>{@link de.cau.cs.kieler.core.expressions.impl.SignalImpl#getHostCombineOperator <em>Host Combine Operator</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class SignalImpl extends ValuedObjectImpl implements Signal {
+    /**
+     * The cached value of the '{@link #getValue() <em>Value</em>}' containment reference.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getValue()
+     * @generated
+     * @ordered
+     */
+    protected EObject value;
+
+    /**
+     * The default value of the '{@link #isIsInput() <em>Is Input</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #isIsInput()
+     * @generated
+     * @ordered
+     */
+    protected static final boolean IS_INPUT_EDEFAULT = false;
+
+    /**
+     * The cached value of the '{@link #isIsInput() <em>Is Input</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #isIsInput()
+     * @generated
+     * @ordered
+     */
+    protected boolean isInput = IS_INPUT_EDEFAULT;
+
+    /**
+     * The default value of the '{@link #isIsOutput() <em>Is Output</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #isIsOutput()
+     * @generated
+     * @ordered
+     */
+    protected static final boolean IS_OUTPUT_EDEFAULT = false;
+
+    /**
+     * The cached value of the '{@link #isIsOutput() <em>Is Output</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #isIsOutput()
+     * @generated
+     * @ordered
+     */
+    protected boolean isOutput = IS_OUTPUT_EDEFAULT;
+
+    /**
+     * The default value of the '{@link #getCombineOperator() <em>Combine Operator</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getCombineOperator()
+     * @generated
+     * @ordered
+     */
+    protected static final CombineOperator COMBINE_OPERATOR_EDEFAULT = CombineOperator.NONE;
+
+    /**
+     * The cached value of the '{@link #getCombineOperator() <em>Combine Operator</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getCombineOperator()
+     * @generated
+     * @ordered
+     */
+    protected CombineOperator combineOperator = COMBINE_OPERATOR_EDEFAULT;
+
+    /**
+     * The default value of the '{@link #getHostCombineOperator() <em>Host Combine Operator</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getHostCombineOperator()
+     * @generated
+     * @ordered
+     */
+    protected static final String HOST_COMBINE_OPERATOR_EDEFAULT = null;
+
+    /**
+     * The cached value of the '{@link #getHostCombineOperator() <em>Host Combine Operator</em>}' attribute.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see #getHostCombineOperator()
+     * @generated
+     * @ordered
+     */
+    protected String hostCombineOperator = HOST_COMBINE_OPERATOR_EDEFAULT;
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected SignalImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return ExpressionsPackage.Literals.SIGNAL;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public EObject getValue() {
+        return value;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public NotificationChain basicSetValue(EObject newValue, NotificationChain msgs) {
+        EObject oldValue = value;
+        value = newValue;
+        if (eNotificationRequired()) {
+            ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, ExpressionsPackage.SIGNAL__VALUE, oldValue, newValue);
+            if (msgs == null) msgs = notification; else msgs.add(notification);
+        }
+        return msgs;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setValue(EObject newValue) {
+        if (newValue != value) {
+            NotificationChain msgs = null;
+            if (value != null)
+                msgs = ((InternalEObject)value).eInverseRemove(this, EOPPOSITE_FEATURE_BASE - ExpressionsPackage.SIGNAL__VALUE, null, msgs);
+            if (newValue != null)
+                msgs = ((InternalEObject)newValue).eInverseAdd(this, EOPPOSITE_FEATURE_BASE - ExpressionsPackage.SIGNAL__VALUE, null, msgs);
+            msgs = basicSetValue(newValue, msgs);
+            if (msgs != null) msgs.dispatch();
+        }
+        else if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, ExpressionsPackage.SIGNAL__VALUE, newValue, newValue));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public boolean isIsInput() {
+        return isInput;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setIsInput(boolean newIsInput) {
+        boolean oldIsInput = isInput;
+        isInput = newIsInput;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, ExpressionsPackage.SIGNAL__IS_INPUT, oldIsInput, isInput));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public boolean isIsOutput() {
+        return isOutput;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setIsOutput(boolean newIsOutput) {
+        boolean oldIsOutput = isOutput;
+        isOutput = newIsOutput;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, ExpressionsPackage.SIGNAL__IS_OUTPUT, oldIsOutput, isOutput));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public CombineOperator getCombineOperator() {
+        return combineOperator;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setCombineOperator(CombineOperator newCombineOperator) {
+        CombineOperator oldCombineOperator = combineOperator;
+        combineOperator = newCombineOperator == null ? COMBINE_OPERATOR_EDEFAULT : newCombineOperator;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, ExpressionsPackage.SIGNAL__COMBINE_OPERATOR, oldCombineOperator, combineOperator));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public String getHostCombineOperator() {
+        return hostCombineOperator;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    public void setHostCombineOperator(String newHostCombineOperator) {
+        String oldHostCombineOperator = hostCombineOperator;
+        hostCombineOperator = newHostCombineOperator;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, ExpressionsPackage.SIGNAL__HOST_COMBINE_OPERATOR, oldHostCombineOperator, hostCombineOperator));
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+        switch (featureID) {
+            case ExpressionsPackage.SIGNAL__VALUE:
+                return basicSetValue(null, msgs);
+        }
+        return super.eInverseRemove(otherEnd, featureID, msgs);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public Object eGet(int featureID, boolean resolve, boolean coreType) {
+        switch (featureID) {
+            case ExpressionsPackage.SIGNAL__VALUE:
+                return getValue();
+            case ExpressionsPackage.SIGNAL__IS_INPUT:
+                return isIsInput();
+            case ExpressionsPackage.SIGNAL__IS_OUTPUT:
+                return isIsOutput();
+            case ExpressionsPackage.SIGNAL__COMBINE_OPERATOR:
+                return getCombineOperator();
+            case ExpressionsPackage.SIGNAL__HOST_COMBINE_OPERATOR:
+                return getHostCombineOperator();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public void eSet(int featureID, Object newValue) {
+        switch (featureID) {
+            case ExpressionsPackage.SIGNAL__VALUE:
+                setValue((EObject)newValue);
+                return;
+            case ExpressionsPackage.SIGNAL__IS_INPUT:
+                setIsInput((Boolean)newValue);
+                return;
+            case ExpressionsPackage.SIGNAL__IS_OUTPUT:
+                setIsOutput((Boolean)newValue);
+                return;
+            case ExpressionsPackage.SIGNAL__COMBINE_OPERATOR:
+                setCombineOperator((CombineOperator)newValue);
+                return;
+            case ExpressionsPackage.SIGNAL__HOST_COMBINE_OPERATOR:
+                setHostCombineOperator((String)newValue);
+                return;
+        }
+        super.eSet(featureID, newValue);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public void eUnset(int featureID) {
+        switch (featureID) {
+            case ExpressionsPackage.SIGNAL__VALUE:
+                setValue((EObject)null);
+                return;
+            case ExpressionsPackage.SIGNAL__IS_INPUT:
+                setIsInput(IS_INPUT_EDEFAULT);
+                return;
+            case ExpressionsPackage.SIGNAL__IS_OUTPUT:
+                setIsOutput(IS_OUTPUT_EDEFAULT);
+                return;
+            case ExpressionsPackage.SIGNAL__COMBINE_OPERATOR:
+                setCombineOperator(COMBINE_OPERATOR_EDEFAULT);
+                return;
+            case ExpressionsPackage.SIGNAL__HOST_COMBINE_OPERATOR:
+                setHostCombineOperator(HOST_COMBINE_OPERATOR_EDEFAULT);
+                return;
+        }
+        super.eUnset(featureID);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public boolean eIsSet(int featureID) {
+        switch (featureID) {
+            case ExpressionsPackage.SIGNAL__VALUE:
+                return value != null;
+            case ExpressionsPackage.SIGNAL__IS_INPUT:
+                return isInput != IS_INPUT_EDEFAULT;
+            case ExpressionsPackage.SIGNAL__IS_OUTPUT:
+                return isOutput != IS_OUTPUT_EDEFAULT;
+            case ExpressionsPackage.SIGNAL__COMBINE_OPERATOR:
+                return combineOperator != COMBINE_OPERATOR_EDEFAULT;
+            case ExpressionsPackage.SIGNAL__HOST_COMBINE_OPERATOR:
+                return HOST_COMBINE_OPERATOR_EDEFAULT == null ? hostCombineOperator != null : !HOST_COMBINE_OPERATOR_EDEFAULT.equals(hostCombineOperator);
+        }
+        return super.eIsSet(featureID);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    public String toString() {
+        if (eIsProxy()) return super.toString();
+
+        StringBuffer result = new StringBuffer(super.toString());
+        result.append(" (isInput: ");
+        result.append(isInput);
+        result.append(", isOutput: ");
+        result.append(isOutput);
+        result.append(", combineOperator: ");
+        result.append(combineOperator);
+        result.append(", hostCombineOperator: ");
+        result.append(hostCombineOperator);
+        result.append(')');
+        return result.toString();
+    }
+
+} //SignalImpl
