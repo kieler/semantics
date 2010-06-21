3899d3b976d59956744443fd64be6bb687541fcf
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2010-06-21 14:31:37 +0000

    copied custom code to kieler.synccharts.textual from labelparser

--- plugins/de.cau.cs.kieler.synccharts.textual.actions/src/de/cau/cs/kieler/synccharts/textual/actions/ActionsRuntimeModule.java
+++ plugins/de.cau.cs.kieler.synccharts.textual.actions/src/de/cau/cs/kieler/synccharts/textual/actions/ActionsRuntimeModule.java
@@ -3,9 +3,29 @@
  */
 package de.cau.cs.kieler.synccharts.textual.actions;
 
+import org.eclipse.xtext.conversion.IValueConverterService;
+import org.eclipse.xtext.parsetree.reconstr.ITransientValueService;
+
+import de.cau.cs.kieler.synccharts.textual.actions.formatting.ActionsTransientValueService;
+import de.cau.cs.kieler.synccharts.textual.actions.formatting.ActionsValueConverter;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class ActionsRuntimeModule extends de.cau.cs.kieler.synccharts.textual.actions.AbstractActionsRuntimeModule {
-
+    /* (non-Javadoc)
+     * @see org.eclipse.xtext.service.DefaultRuntimeModule#bindITransientValueService()
+     */
+    @Override
+    public Class<? extends ITransientValueService> bindITransientValueService() {
+        return ActionsTransientValueService.class; 
+    }
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.xtext.service.DefaultRuntimeModule#bindIValueConverterService()
+     */
+    @Override
+    public Class<? extends IValueConverterService> bindIValueConverterService() {
+        return ActionsValueConverter.class;
+    }
 }
