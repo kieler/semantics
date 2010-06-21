1b114243f108bf470cf8e90be53cce3009c78f80
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2010-06-21 17:31:02 +0000

    replaced all synccharts textual projects with this integrated project (kits, actionlabel, interfacedecl)

--- plugins/de.cau.cs.kieler.synccharts.textual.ui/src-gen/de/cau/cs/kieler/synccharts/textual/ui/wizard/KitsProjectInfo.java
+++
@@ -1,7 +0,0 @@
-package de.cau.cs.kieler.synccharts.textual.ui.wizard;
-
-import org.eclipse.xtext.ui.core.wizard.DefaultProjectInfo;
-
-public class KitsProjectInfo extends DefaultProjectInfo {
-	
-}
