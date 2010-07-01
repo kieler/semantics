5ee19357818c4c09ef97365332c314f6a6ccf5e9
John Julian Carstens <jjc@informatik.uni-kiel.de>
2010-07-01 16:41:57 +0000

    *now fetches URI and data from KIEM

--- plugins/de.cau.cs.kieler.kvid/src/de/cau/cs/kieler/kvid/dataprovider/KIEMDataProvider.java
+++ plugins/de.cau.cs.kieler.kvid/src/de/cau/cs/kieler/kvid/dataprovider/KIEMDataProvider.java
@@ -1,5 +1,11 @@
 package de.cau.cs.kieler.kvid.dataprovider;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import de.cau.cs.kieler.sim.kiem.IJSONObjectDataComponent;
@@ -8,9 +14,22 @@ import de.cau.cs.kieler.sim.kiem.KiemExecutionException;
 import de.cau.cs.kieler.sim.kiem.KiemInitializationException;
 
 public class KIEMDataProvider extends JSONObjectDataComponent implements IJSONObjectDataComponent {
+    
+    private HashMap<String, Object> dataByURI = new HashMap<String, Object>();
 
     public JSONObject step(JSONObject jSONObject) throws KiemExecutionException {
-        jSONObject.toString();
+        Iterator allKeys = jSONObject.keys();
+        while (allKeys.hasNext()) {
+            Object o = allKeys.next();
+            try {
+                dataByURI.put(o.toString(), jSONObject.get(o.toString()));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        for (String string : dataByURI.keySet()) {
+            System.out.println("KViD: " + string + " " + dataByURI.get(string));            
+        }
         return null;
     }
 
