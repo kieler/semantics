e0319062a5047d049b2c652e386ae7170e365237
Christian Motika <cmot@informatik.uni-kiel.de>
2010-07-12 05:15:05 +0000

    added http://www.eclipse.org/uml2/3.0.0/UML meta model

---
+++ plugins/de.cau.cs.kieler.uml2.sim/src/de/cau/cs/kieler/uml2/sim/xtend/JavaEscape.java
@@ -0,0 +1,15 @@
+package de.cau.cs.kieler.uml2.sim.xtend;
+
+public class JavaEscape {
+
+    //FIXME: only works for simple transition labels
+    static String getTrigger(String TransitionLabel) {
+        return TransitionLabel.substring(0,TransitionLabel.indexOf("/"));
+    }
+    
+    //FIXME: only works for simple transition labels
+    static String getEffect(String TransitionLabel) {
+        return TransitionLabel.substring(TransitionLabel.indexOf("/"));
+    }
+    
+}
