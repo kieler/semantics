11c1fcefca9bb99b394432d9d0be1f6f49c2dc21
John Julian Carstens <jjc@informatik.uni-kiel.de>
2010-07-28 13:42:45 +0000

    groundwork for properties view

---
+++ plugins/de.cau.cs.kieler.kvid/src/de/cau/cs/kieler/kvid/views/KViDView.java
@@ -0,0 +1,20 @@
+package de.cau.cs.kieler.kvid.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class KViDView extends ViewPart {
+
+    @Override
+    public void createPartControl(Composite parent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
