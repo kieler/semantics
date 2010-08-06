efd7795f4336c2981fa2a8c2a12353ae5796840a
Christian Schneider <chsch@informatik.uni-kiel.de>
2010-08-06 09:42:21 +0000

    missing dependency added to expressions.edit

--- plugins/de.cau.cs.kieler.core.expressions.edit/src/de/cau/cs/kieler/core/expressions/provider/ValuedObjectItemProvider.java
+++ plugins/de.cau.cs.kieler.core.expressions.edit/src/de/cau/cs/kieler/core/expressions/provider/ValuedObjectItemProvider.java
@@ -34,7 +34,6 @@ import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
-import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
 /**
