c7e936b8e698d44b7b746912f82b3601e69159f8
Christian Motika <cmot@informatik.uni-kiel.de>
2010-08-09 08:10:14 +0000

    transform Modell to Maude

--- plugins/de.cau.cs.kieler.uml2.sim/src/de/cau/cs/kieler/uml2/sim/JavaEscape.java
+++ plugins/de.cau.cs.kieler.uml2.sim/src/de/cau/cs/kieler/uml2/sim/JavaEscape.java
@@ -1,17 +1,41 @@
 package de.cau.cs.kieler.uml2.sim;
+import org.eclipse.uml2.uml.Pseudostate;
+
 
 public class JavaEscape {
 
     //FIXME: only works for simple transition labels
     public static String getTrigger(String TransitionLabel) {
-        return "a";
-        //return TransitionLabel.substring(0,TransitionLabel.indexOf("/"));
+    	if (TransitionLabel.contains("/")) 
+        return TransitionLabel.substring(0,TransitionLabel.indexOf("/"));
+    	else return null ;
+
     }
     
     //FIXME: only works for simple transition labels
-    public static String getEffect(String TransitionLabel) {
-        return "b";
-//        return TransitionLabel.substring(TransitionLabel.indexOf("/"));
+    public static String getAction(String TransitionLabel) {
+    	if (TransitionLabel.contains("/")) 
+        if (TransitionLabel.substring(TransitionLabel.indexOf("/")+1) != "")
+        	return TransitionLabel.substring(TransitionLabel.indexOf("/")+1) ;
+        else return "skip" ;
+    	else return "skip" ;
     }
-    
+    public static String transID(String TransitionLabel) {
+    	if (TransitionLabel == null) return null;
+    	else return TransitionLabel.substring(TransitionLabel.indexOf("@")+1, TransitionLabel.indexOf("(name")-1) ;
+    }
+
+    public Boolean contains(String str) {
+    	return (this.contains(str)); 
+    }
+
+    public Boolean isInitial(org.eclipse.uml2.uml.Pseudostate pstate) {
+    	return ((pstate.getKind()).getValue() == org.eclipse.uml2.uml.PseudostateKind.INITIAL); 
+    }
+    public Boolean isDH(org.eclipse.uml2.uml.Pseudostate pstate) {
+    	return ((pstate.getKind()).getValue() == org.eclipse.uml2.uml.PseudostateKind.DEEP_HISTORY); 
+    }
+    public Boolean isSH(org.eclipse.uml2.uml.Pseudostate pstate) {
+    	return ((pstate.getKind()).getValue() == org.eclipse.uml2.uml.PseudostateKind.SHALLOW_HISTORY); 
+    }
 }
