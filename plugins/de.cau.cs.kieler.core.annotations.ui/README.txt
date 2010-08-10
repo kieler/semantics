4d647b6a0a1c04b75fb88e2e19c2d46bd3a5033f
Christian Schneider <chsch@informatik.uni-kiel.de>
2010-08-10 13:10:10 +0000

    xtext stuff removed from branch

--- plugins/de.cau.cs.kieler.core.annotations.ui/README.txt
+++
@@ -1,4 +0,0 @@
-| de.cau.cs.kieler.core.annotations.ui
-\________________________________________
-
-User interface additions to access the annotations of annotatable objects.
--- plugins/de.cau.cs.kieler.core.annotations.ui/src/de/cau/cs/kieler/core/annotations/ui/properties/AnnotationsPropertySection.java
+++
@@ -1,7 +0,0 @@
-package de.cau.cs.kieler.core.annotations.ui.properties;
-
-import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
-
-public class AnnotationsPropertySection extends AbstractPropertySection {
-
-}
