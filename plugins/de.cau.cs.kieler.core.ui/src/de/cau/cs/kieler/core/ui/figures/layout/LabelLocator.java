e04fa75152cbacb900adc3870628c5c308886684
Miro Spoenemann <msp@informatik.uni-kiel.de>
2010-09-20 10:08:08 +0000

    improved label locating for port labels

--- plugins/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/figures/layout/LabelLocator.java
+++ plugins/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/figures/layout/LabelLocator.java
@@ -22,6 +22,7 @@ import org.eclipse.gmf.runtime.diagram.ui.figures.BorderItemLocator;
 
 /**
  * A border item locator for entity and port labels.
+ * TODO add support for user's position constraints
  * 
  * @author msp
  */
@@ -38,43 +39,17 @@ public class LabelLocator extends BorderItemLocator {
     public LabelLocator(final IFigure parent, final int side) {
         super(parent, side);
     }
-
+    
+    /** whether the locator should handle the parents as ports. */
+    private boolean portBehavior;
+    
     /**
-     * {@inheritDoc}
+     * Sets whether the locator should handle the parents as ports.
+     * 
+     * @param theportBehavior the port behavior status
      */
-    @Override
-    public Rectangle getValidLocation(final Rectangle proposedLocation, final IFigure borderItem) {
-        Rectangle location = new Rectangle(proposedLocation);
-        if (proposedLocation.x == 0 && proposedLocation.y == 0) {
-            Rectangle parentBorder = getParentBorder();
-            Dimension offset = getBorderItemOffset();
-            Dimension size = borderItem.getSize();
-            switch (getPreferredSideOfParent()) {
-            case PositionConstants.EAST:
-                location.x = parentBorder.x + parentBorder.width - offset.width;
-                location.y = parentBorder.y + (parentBorder.height - size.height) / 2;
-                break;
-            case PositionConstants.WEST:
-                location.x = parentBorder.x - size.width + offset.width;
-                location.y = parentBorder.y + (parentBorder.height - size.height) / 2;
-                break;
-            case PositionConstants.NORTH:
-                location.x = parentBorder.x + (parentBorder.width - size.width) / 2;
-                location.y = parentBorder.y - size.height + offset.height;
-                break;
-            default:
-                location.x = parentBorder.x + (parentBorder.width - size.width) / 2;
-                location.y = parentBorder.y + parentBorder.height - offset.height;
-            }
-        } else {
-            int side = findClosestSideOfParent(proposedLocation, getParentBorder());
-            Point newTopLeft = locateOnBorder(location.getTopLeft(), side, 0, borderItem);
-            location.setLocation(newTopLeft);
-        }
-        if (location.getSize().isEmpty()) {
-            location.setSize(borderItem.getPreferredSize());
-        }
-        return location;
+    public void setPortBehavior(final boolean theportBehavior) {
+        this.portBehavior = theportBehavior;
     }
 
     /**
@@ -82,8 +57,100 @@ public class LabelLocator extends BorderItemLocator {
      */
     @Override
     public void relocate(final IFigure borderItem) {
-        Rectangle validLocation = getValidLocation(getConstraint(), borderItem);
+        Rectangle validLocation = getValidLocation(null, borderItem);
         borderItem.setBounds(validLocation);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Rectangle getValidLocation(final Rectangle proposedLocation, final IFigure borderItem) {
+        Rectangle location = proposedLocation == null ? new Rectangle()
+                : new Rectangle(proposedLocation);
+        if (location.getSize().isEmpty()) {
+            location.setSize(borderItem.getPreferredSize());
+        }
+        if (location.x == 0 && location.y == 0) {
+            locate(location);
+        } else {
+            int side = findClosestSideOfParent(proposedLocation, getParentBorder());
+            Point newTopLeft = locateOnBorder(location.getTopLeft(), side, 0, borderItem);
+            location.setLocation(newTopLeft);
+        }
+        return location;
+    }
+    
+    /**
+     * Find a suitable location for the given border item.
+     * 
+     * @param location the rectangle where the new location is written to
+     * @param borderItem a border item
+     */
+    private void locate(final Rectangle location) {
+        Rectangle parentBorder = getParentBorder();
+        Dimension offset = getBorderItemOffset();
+        if (portBehavior) {
+            Rectangle nodeBorder = getParentFigure().getParent().getParent().getParent().getBounds();
+            switch (calcPortSide(nodeBorder, parentBorder)) {
+            case PositionConstants.EAST:
+                location.x = parentBorder.x - offset.width;
+                location.y = parentBorder.y + parentBorder.height - offset.height;
+                break;
+            case PositionConstants.WEST:
+                location.x = parentBorder.x + parentBorder.width - location.width + offset.width;
+                location.y = parentBorder.y + parentBorder.height - offset.height;
+                break;
+            case PositionConstants.NORTH:
+                location.x = parentBorder.x + (parentBorder.width - location.width) / 2;
+                location.y = parentBorder.y - location.height + offset.height;
+                break;
+            default:
+                location.x = parentBorder.x + (parentBorder.width - location.width) / 2;
+                location.y = parentBorder.y + parentBorder.height - offset.height;
+            }
+        } else {
+            switch (getPreferredSideOfParent()) {
+            case PositionConstants.EAST:
+                location.x = parentBorder.x + parentBorder.width - offset.width;
+                location.y = parentBorder.y + (parentBorder.height - location.height) / 2;
+                break;
+            case PositionConstants.WEST:
+                location.x = parentBorder.x - location.width + offset.width;
+                location.y = parentBorder.y + (parentBorder.height - location.height) / 2;
+                break;
+            case PositionConstants.NORTH:
+                location.x = parentBorder.x + (parentBorder.width - location.width) / 2;
+                location.y = parentBorder.y - location.height + offset.height;
+                break;
+            default:
+                location.x = parentBorder.x + (parentBorder.width - location.width) / 2;
+                location.y = parentBorder.y + parentBorder.height - offset.height;
+            }
+        }
+    }
+    
+    /**
+     * Calculates a side for the given port.
+     * 
+     * @param nodeBounds bounds of the parent node
+     * @param portBounds bounds of the port
+     * @return the port side
+     */
+    private static int calcPortSide(final Rectangle nodeBounds, final Rectangle portBounds) {
+        int portx = portBounds.x + portBounds.width / 2;
+        int porty = portBounds.y + portBounds.height / 2;
+        if (portx <= nodeBounds.x) {
+            return PositionConstants.WEST;
+        } else if (portx >= nodeBounds.x + nodeBounds.width) {
+            return PositionConstants.EAST;
+        } else if (porty <= nodeBounds.y) {
+            return PositionConstants.NORTH;
+        } else if (porty >= nodeBounds.y + nodeBounds.height) {
+            return PositionConstants.SOUTH;
+        } else {
+            return PositionConstants.NSEW;
+        }
     }
 
 }
