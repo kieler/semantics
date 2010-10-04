fd83918901c9c2ea18f47e06702799c9df0b5bfa
Soeren Hansen <soh@informatik.uni-kiel.de>
2010-10-04 11:28:45 +0000

    Removed references to old viewmanagement

--- plugins/de.cau.cs.kieler.synccharts.diagram.custom/src/de/cau/cs/kieler/synccharts/diagram/custom/HighlightingManager.java
+++ plugins/de.cau.cs.kieler.synccharts.diagram.custom/src/de/cau/cs/kieler/synccharts/diagram/custom/HighlightingManager.java
@@ -13,14 +13,12 @@
  */
 package de.cau.cs.kieler.synccharts.diagram.custom;
 
-import java.util.HashMap;
-import java.util.Iterator;
-
 import org.eclipse.gef.EditPart;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.ui.IWorkbenchPart;
 
-import de.cau.cs.kieler.viewmanagement.effects.ShapeHighlightEffect;
+//FIXME: mmu: redirect this to new kivi effects
+//import de.cau.cs.kieler.viewmanagement.effects.ShapeHighlightEffect;
 
 /**
  * Manager for highlighting of edit parts.
@@ -31,10 +29,12 @@ import de.cau.cs.kieler.viewmanagement.effects.ShapeHighlightEffect;
 public final class HighlightingManager {
 
     /** Map containing all highlighting effects and highlighted edit parts. */
-    private final HashMap<IWorkbenchPart, HashMap<EditPart, ShapeHighlightEffect>> map;
+    // private final HashMap<IWorkbenchPart, HashMap<EditPart,
+    // ShapeHighlightEffect>> map;
 
     /**
-     * True if the manager should only be able to receive events through the alternate methods.
+     * True if the manager should only be able to receive events through the
+     * alternate methods.
      */
     private boolean isLocked = false;
 
@@ -43,145 +43,170 @@ public final class HighlightingManager {
 
     /** singleton pattern. */
     private HighlightingManager() {
-        map = new HashMap<IWorkbenchPart, HashMap<EditPart, ShapeHighlightEffect>>();
+        // map = new HashMap<IWorkbenchPart, HashMap<EditPart,
+        // ShapeHighlightEffect>>();
     }
 
     /**
      * Lock the Highlight manager so that only the locked methods can be used.
      */
     public static void lock() {
-        instance.isLocked = true;
+        // instance.isLocked = true;
     }
 
     /**
      * Unlock the Highlighting manager.
      */
     public static void unlock() {
-        instance.isLocked = false;
+        // instance.isLocked = false;
     }
 
     /**
-     * Highlight an edit part with the given colors. This will override any highlighting effect
-     * currently in place on the particular edit part.
+     * Highlight an edit part with the given colors. This will override any
+     * highlighting effect currently in place on the particular edit part.
      * 
      * The highlighting can be reset by calling the reset methods.
      * 
-     * @param editor the editor
-     * @param editPart the edit part
-     * @param foreground the foreground color
-     * @param background the background color
+     * @param editor
+     *            the editor
+     * @param editPart
+     *            the edit part
+     * @param foreground
+     *            the foreground color
+     * @param background
+     *            the background color
      */
-    public static void lockedHighlight(final IWorkbenchPart editor, final EditPart editPart,
-            final Color foreground, final Color background) {
-        HashMap<EditPart, ShapeHighlightEffect> parts = instance.map.get(editor);
-        if (parts == null) {
-            parts = new HashMap<EditPart, ShapeHighlightEffect>();
-            instance.map.put(editor, parts);
-        }
-        ShapeHighlightEffect oldEffect = parts.remove(editPart);
-        if (oldEffect != null) {
-            oldEffect.undo();
-        }
-        parts.put(editPart, highlightPart(editPart, foreground, background));
+    public static void lockedHighlight(final IWorkbenchPart editor,
+            final EditPart editPart, final Color foreground,
+            final Color background) {
+        // HashMap<EditPart, ShapeHighlightEffect> parts = instance.map
+        // .get(editor);
+        // if (parts == null) {
+        // parts = new HashMap<EditPart, ShapeHighlightEffect>();
+        // instance.map.put(editor, parts);
+        // }
+        // ShapeHighlightEffect oldEffect = parts.remove(editPart);
+        // if (oldEffect != null) {
+        // oldEffect.undo();
+        // }
+        // parts.put(editPart, highlightPart(editPart, foreground, background));
     }
 
     /**
-     * Highlight an edit part with the given colors. This will override any highlighting effect
-     * currently in place on the particular edit part.
+     * Highlight an edit part with the given colors. This will override any
+     * highlighting effect currently in place on the particular edit part.
      * 
      * The highlighting can be reset by calling the reset methods.
      * 
-     * @param editor the editor
-     * @param editPart the edit part
-     * @param foreground the foreground color
-     * @param background the background color
+     * @param editor
+     *            the editor
+     * @param editPart
+     *            the edit part
+     * @param foreground
+     *            the foreground color
+     * @param background
+     *            the background color
      */
-    public static void highlight(final IWorkbenchPart editor, final EditPart editPart,
-            final Color foreground, final Color background) {
-        if (!instance.isLocked) {
-            lockedHighlight(editor, editPart, foreground, background);
-        }
+    public static void highlight(final IWorkbenchPart editor,
+            final EditPart editPart, final Color foreground,
+            final Color background) {
+        // if (!instance.isLocked) {
+        // lockedHighlight(editor, editPart, foreground, background);
+        // }
     }
 
     /**
      * Reset all highlighting effects on the given editor.
      * 
-     * @param editor the editor
+     * @param editor
+     *            the editor
      */
     public static void lockedReset(final IWorkbenchPart editor) {
-        HashMap<EditPart, ShapeHighlightEffect> parts = instance.map.get(editor);
-
-        if (parts != null) {
-
-            Iterator<EditPart> iter = parts.keySet().iterator();
-            while (iter.hasNext()) {
-                EditPart part = iter.next();
-                try {
-                    parts.get(part).undo();
-                } catch (NullPointerException e0) {
-                    // obscure exception when edit part is removed
-                }
-            }
-            instance.map.remove(editor);
-        }
+        // HashMap<EditPart, ShapeHighlightEffect> parts = instance.map
+        // .get(editor);
+        //
+        // if (parts != null) {
+        //
+        // Iterator<EditPart> iter = parts.keySet().iterator();
+        // while (iter.hasNext()) {
+        // EditPart part = iter.next();
+        // try {
+        // parts.get(part).undo();
+        // } catch (NullPointerException e0) {
+        // // obscure exception when edit part is removed
+        // }
+        // }
+        // instance.map.remove(editor);
+        // }
     }
 
     /**
      * Reset all highlighting effects on the given editor.
      * 
-     * @param editor the editor
+     * @param editor
+     *            the editor
      */
     public static void reset(final IWorkbenchPart editor) {
-        if (!instance.isLocked) {
-            lockedReset(editor);
-        }
+        // if (!instance.isLocked) {
+        // lockedReset(editor);
+        // }
     }
 
     /**
      * Reset the highlighting on the given edit part.
      * 
-     * @param editor the editor
-     * @param editPart the edit part
+     * @param editor
+     *            the editor
+     * @param editPart
+     *            the edit part
      */
-    public static void lockedReset(final IWorkbenchPart editor, final EditPart editPart) {
-        HashMap<EditPart, ShapeHighlightEffect> parts = instance.map.get(editor);
-
-        if (parts != null && parts.containsKey(editPart)) {
-            ShapeHighlightEffect effect = parts.remove(editPart);
-            try {
-                effect.undo();
-            } catch (NullPointerException e0) {
-                // obscure exception when edit part is removed
-            }
-        }
+    public static void lockedReset(final IWorkbenchPart editor,
+            final EditPart editPart) {
+        // HashMap<EditPart, ShapeHighlightEffect> parts = instance.map
+        // .get(editor);
+        //
+        // if (parts != null && parts.containsKey(editPart)) {
+        // ShapeHighlightEffect effect = parts.remove(editPart);
+        // try {
+        // effect.undo();
+        // } catch (NullPointerException e0) {
+        // // obscure exception when edit part is removed
+        // }
+        // }
     }
 
     /**
      * Reset the highlighting on the given edit part.
      * 
-     * @param editor the editor
-     * @param editPart the edit part
+     * @param editor
+     *            the editor
+     * @param editPart
+     *            the edit part
      */
-    public static void reset(final IWorkbenchPart editor, final EditPart editPart) {
-        if (!instance.isLocked) {
-            reset(editor, editPart);
-        }
+    public static void reset(final IWorkbenchPart editor,
+            final EditPart editPart) {
+        // if (!instance.isLocked) {
+        // reset(editor, editPart);
+        // }
     }
 
     /**
      * Highlight the given edit part.
      * 
-     * @param part the part
-     * @param foreground the foreground color
-     * @param background the background color
+     * @param part
+     *            the part
+     * @param foreground
+     *            the foreground color
+     * @param background
+     *            the background color
      * @return the effect
      */
-    private static ShapeHighlightEffect highlightPart(final EditPart part, final Color foreground,
-            final Color background) {
-        ShapeHighlightEffect newEffect = new ShapeHighlightEffect();
-        newEffect.setColors(foreground, background);
-        newEffect.setTarget(part);
-        newEffect.execute();
-        return newEffect;
-    }
+    // private static ShapeHighlightEffect highlightPart(final EditPart part,
+    // final Color foreground, final Color background) {
+    // ShapeHighlightEffect newEffect = new ShapeHighlightEffect();
+    // newEffect.setColors(foreground, background);
+    // newEffect.setTarget(part);
+    // newEffect.execute();
+    // return newEffect;
+    // }
 }
