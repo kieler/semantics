6408f1fcce97d744130013243caeb6154ff4acda
Martin Mueller <mmu@informatik.uni-kiel.de>
2010-10-23 22:35:25 +0000

    fixed ModelChangeTrigger, adapted synccharts autoautolayout

--- plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/trigger/ModelChangeTrigger.java
+++ plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/trigger/ModelChangeTrigger.java
@@ -1,3 +1,16 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.core.model.trigger;
 
 import org.eclipse.emf.common.command.Command;
@@ -8,6 +21,7 @@ import org.eclipse.emf.transaction.ResourceSetListener;
 import org.eclipse.emf.transaction.RollbackException;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
 import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPartListener;
 import org.eclipse.ui.IWorkbenchPart;
 
@@ -15,56 +29,92 @@ import de.cau.cs.kieler.core.kivi.AbstractTrigger;
 import de.cau.cs.kieler.core.kivi.AbstractTriggerState;
 import de.cau.cs.kieler.core.kivi.ITrigger;
 import de.cau.cs.kieler.core.ui.util.CombinedWorkbenchListener;
+import de.cau.cs.kieler.core.ui.util.EditorUtils;
+import de.cau.cs.kieler.core.ui.util.MonitoredOperation;
 
 /**
- * A view management Trigger, that registered as a ResourceSetChangeListener onto any active
- * Diagram editor. It provides TriggerStates about any model changes. There are two different nested
- * states available, one for the semantic model, one for the GMF notation model.
+ * A view management Trigger that registered as a ResourceSetChangeListener onto any active Diagram
+ * editor. It provides TriggerStates about any model changes. There are two different nested states
+ * available, one for the semantic model, one for the GMF notation model.
+ * 
  * @author haf
- *
+ * 
  */
 public class ModelChangeTrigger extends AbstractTrigger implements IPartListener,
-    ResourceSetListener {
+        ResourceSetListener {
 
     private NotificationFilter notationFilter = (NotificationFilter
-        .createNotifierTypeFilter(NotationPackage.eINSTANCE.getView())
-        .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE.getStyle()))
-        .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE
-            .getLayoutConstraint()))
-        .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE.getAnchor()))
-        .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE.getBendpoints()))
-        .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE.getGuide())));
+            .createNotifierTypeFilter(NotationPackage.eINSTANCE.getView())
+            .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE.getStyle()))
+            .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE
+                    .getLayoutConstraint()))
+            .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE.getAnchor()))
+            .or(NotificationFilter.createNotifierTypeFilter(NotationPackage.eINSTANCE
+                    .getBendpoints())).or(NotificationFilter
+            .createNotifierTypeFilter(NotationPackage.eINSTANCE.getGuide())));
+
+    private DiagramEditor currentEditor;
 
     @Override
     public void register() {
         CombinedWorkbenchListener.addPartListener(this);
+        final ResourceSetListener that = this;
+        // register with the active editor
+        // else the initially open editor will not send events until the editor changes
+        MonitoredOperation.runInUI(new Runnable() {
+            public void run() {
+                IEditorPart part = EditorUtils.getLastActiveEditor();
+                if (part instanceof DiagramEditor) {
+                    currentEditor = (DiagramEditor) part;
+                    currentEditor.getEditingDomain().addResourceSetListener(that);
+                }
+            }
+        }, false);
     }
 
     @Override
     public void unregister() {
         CombinedWorkbenchListener.removePartListener(this);
-    }
-
-    public void partActivated(IWorkbenchPart part) {
-        if (part instanceof DiagramEditor) {
-            ((DiagramEditor) part).getEditingDomain().addResourceSetListener(this);
+        if (currentEditor != null) {
+            currentEditor.getEditingDomain().removeResourceSetListener(this);
         }
-
     }
 
-    public void partDeactivated(IWorkbenchPart part) {
+    /**
+     * {@inheritDoc}
+     */
+    public void partActivated(final IWorkbenchPart part) {
+        if (part instanceof DiagramEditor) {
+            currentEditor = (DiagramEditor) part;
+            currentEditor.getEditingDomain().addResourceSetListener(this);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void partDeactivated(final IWorkbenchPart part) {
         if (part instanceof DiagramEditor) {
             ((DiagramEditor) part).getEditingDomain().removeResourceSetListener(this);
         }
     }
 
-    public void partBroughtToTop(IWorkbenchPart part) {
+    /**
+     * {@inheritDoc}
+     */
+    public void partBroughtToTop(final IWorkbenchPart part) {
     }
 
-    public void partClosed(IWorkbenchPart part) {
+    /**
+     * {@inheritDoc}
+     */
+    public void partClosed(final IWorkbenchPart part) {
     }
 
-    public void partOpened(IWorkbenchPart part) {
+    /**
+     * {@inheritDoc}
+     */
+    public void partOpened(final IWorkbenchPart part) {
     }
 
     /**
@@ -74,35 +124,49 @@ public class ModelChangeTrigger extends AbstractTrigger implements IPartListener
         return null;
     }
 
-    public Command transactionAboutToCommit(ResourceSetChangeEvent event) throws RollbackException {
+    /**
+     * {@inheritDoc}
+     */
+    public Command transactionAboutToCommit(final ResourceSetChangeEvent event)
+            throws RollbackException {
         return null;
     }
 
-    public void resourceSetChanged(ResourceSetChangeEvent event) {
-
+    /**
+     * {@inheritDoc}
+     */
+    public void resourceSetChanged(final ResourceSetChangeEvent event) {
         boolean triggeredNotation = false;
         boolean triggeredSemantic = false;
-
         for (Notification notification : event.getNotifications()) {
             if (!triggeredNotation && notationFilter.matches(notification)) {
-                trigger(new ModelDiagramChangeState(event));
+                trigger(new DiagramChangeState(event, currentEditor));
                 triggeredNotation = true;
             }
             if (!triggeredSemantic && notationFilter.negated().matches(notification)) {
-                trigger(new ModelChangeState(event));
+                trigger(new ModelChangeState(event, currentEditor));
                 triggeredSemantic = true;
             }
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean isAggregatePrecommitListener() {
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean isPrecommitOnly() {
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean isPostcommitOnly() {
         return true;
     }
@@ -112,48 +176,109 @@ public class ModelChangeTrigger extends AbstractTrigger implements IPartListener
      * 
      * @author haf
      */
-    static public class ModelChangeState extends AbstractTriggerState {
-
-        ResourceSetChangeEvent event = null;
-
+    public static class ModelChangeState extends ChangeState {
+        /**
+         * Default constructor.
+         */
         public ModelChangeState() {
+            super(null, null);
         }
 
-        public ModelChangeState(ResourceSetChangeEvent event) {
-            this.event = event;
-        }
-
-        public Class<? extends ITrigger> getTriggerClass() {
-            return ModelChangeTrigger.class;
-        }
-
-        public ResourceSetChangeEvent getChange() {
-            return event;
+        /**
+         * Create a new change state with the given event.
+         * 
+         * @param changeEvent
+         *            the event that caused the change
+         * @param editor
+         *            the diagram editor where the change happened
+         */
+        public ModelChangeState(final ResourceSetChangeEvent changeEvent, final DiagramEditor editor) {
+            super(changeEvent, editor);
         }
     }
 
     /**
-     * A state about changes of the diagram model (currently only the GMF notation model)
+     * A state about changes of the diagram model (currently only the GMF notation model).
      * 
      * @author haf
      */
-    static public class ModelDiagramChangeState extends AbstractTriggerState {
-
-        ResourceSetChangeEvent event = null;
-
-        public ModelDiagramChangeState() {
+    public static class DiagramChangeState extends ChangeState {
+        /**
+         * Default constructor.
+         */
+        public DiagramChangeState() {
+            super(null, null);
         }
 
-        public ModelDiagramChangeState(ResourceSetChangeEvent event) {
-            this.event = event;
+        /**
+         * Create a new change state with the given event.
+         * 
+         * @param changeEvent
+         *            the event that caused the change
+         * @param editor
+         *            the diagram editor where the change happened
+         */
+        public DiagramChangeState(final ResourceSetChangeEvent changeEvent,
+                final DiagramEditor editor) {
+            super(changeEvent, editor);
+        }
+    }
+
+    /**
+     * A state about changes of the a model.
+     * 
+     * @author haf
+     */
+    private abstract static class ChangeState extends AbstractTriggerState {
+
+        private ResourceSetChangeEvent event = null;
+
+        private DiagramEditor diagramEditor = null;
+
+        /**
+         * Create a new change state with the given event.
+         * 
+         * @param changeEvent
+         *            the event that caused the change
+         * @param editor
+         *            the diagram editor where the change happened
+         */
+        public ChangeState(final ResourceSetChangeEvent changeEvent, final DiagramEditor editor) {
+            event = changeEvent;
+            diagramEditor = editor;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         public Class<? extends ITrigger> getTriggerClass() {
             return ModelChangeTrigger.class;
         }
 
+        /**
+         * Get the event that caused the change.
+         * 
+         * @return the event
+         */
         public ResourceSetChangeEvent getChange() {
-            return event;
+            if (event != null) {
+                return event;
+            } else {
+                return null; // FIXME sane default value
+            }
+        }
+
+        /**
+         * Get the editor where the change happened.
+         * 
+         * @return the diagram editor
+         */
+        public DiagramEditor getDiagramEditor() {
+            if (diagramEditor != null) {
+                return diagramEditor;
+            } else {
+                return null; // FIXME sane default value
+            }
         }
     }
 
--- plugins/de.cau.cs.kieler.synccharts.kivi/src/de/cau/cs/kieler/synccharts/kivi/LayoutAfterModelChangedCombination.java
+++ plugins/de.cau.cs.kieler.synccharts.kivi/src/de/cau/cs/kieler/synccharts/kivi/LayoutAfterModelChangedCombination.java
@@ -13,25 +13,85 @@
  */
 package de.cau.cs.kieler.synccharts.kivi;
 
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.NotificationFilter;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+
+import de.cau.cs.kieler.core.expressions.ExpressionsPackage;
 import de.cau.cs.kieler.core.kivi.AbstractCombination;
 import de.cau.cs.kieler.kiml.ui.layout.LayoutEffect;
-import de.cau.cs.kieler.synccharts.kivi.ModelChangedTrigger.ModelChangedState;
+import de.cau.cs.kieler.synccharts.SyncchartsPackage;
+import de.cau.cs.kieler.core.model.trigger.ModelChangeTrigger.ModelChangeState;
+import de.cau.cs.kieler.core.model.trigger.ModelChangeTrigger.DiagramChangeState;
 
 /**
  * Applies automatic layout after the model has been changed.
  * 
  * @author mmu
- *
+ * 
  */
 public class LayoutAfterModelChangedCombination extends AbstractCombination {
+
+    private static NotificationFilter modelFilter = NotificationFilter
+            .createFeatureFilter(SyncchartsPackage.eINSTANCE.getScope_Label())
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getAction_Label()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_SuspensionTrigger()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_BodyText()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_InnerActions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_ExitActions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_EntryActions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getState_Regions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getState_OutgoingTransitions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getState_IncomingTransitions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getRegion_States()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_Signals()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_Variables()))
+            .or(NotificationFilter.createFeatureFilter(ExpressionsPackage.eINSTANCE
+                    .getValuedObject_Name()));
+    private NotificationFilter diagramFilter = NotificationFilter
+            .createFeatureFilter(NotationPackage.eINSTANCE.getDrawerStyle_Collapsed());
 
     /**
      * Apply automatic layout every time the model changed state is updated.
      * 
-     * @param state label changed state
+     * @param modelState
+     *            model changed
+     * @param diagramState
+     *            diagram changed
      */
-    public void execute(final ModelChangedState state) {
+    public void execute(final ModelChangeState modelState, final DiagramChangeState diagramState) {
         dontUndo();
-        schedule(new LayoutEffect(state.getDiagramEditor(), state.getEObject()));
+        if (modelState.getSequenceNumber() > diagramState.getSequenceNumber()) {
+            // model changed
+            for (Notification notification : modelState.getChange().getNotifications()) {
+                if (modelFilter.matches(notification)
+                        && notification.getNotifier() instanceof EObject) {
+                    schedule(new LayoutEffect(modelState.getDiagramEditor(),
+                            (EObject) notification.getNotifier()));
+                }
+            }
+        } else {
+            // diagram changed
+            for (Notification notification : diagramState.getChange().getNotifications()) {
+                if (diagramFilter.matches(notification)
+                        && notification.getNotifier() instanceof EObject) {
+                    schedule(new LayoutEffect(diagramState.getDiagramEditor(),
+                            (EObject) notification.getNotifier()));
+                }
+            }
+        }
     }
 }
