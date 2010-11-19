f7a00d5c1a5ff77525802188dcda5ec9ef5f993e
Ulf Rueegg <uru@informatik.uni-kiel.de>
2010-11-19 09:51:42 +0000

    fixed possible class cast exception

--- plugins/de.cau.cs.kieler.synccharts.text.bridge/src/de/cau/cs/kieler/synccharts/text/actions/bridge/listeners/SerializerListener.java
+++ plugins/de.cau.cs.kieler.synccharts.text.bridge/src/de/cau/cs/kieler/synccharts/text/actions/bridge/listeners/SerializerListener.java
@@ -6,14 +6,15 @@ import org.eclipse.emf.transaction.NotificationFilter;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 
 import de.cau.cs.kieler.synccharts.Action;
+import de.cau.cs.kieler.synccharts.State;
 import de.cau.cs.kieler.synccharts.SyncchartsPackage;
 import de.cau.cs.kieler.synccharts.listener.FireOnceTriggerListener;
 import de.cau.cs.kieler.synccharts.text.actions.bridge.ActionLabelProcessorWrapper;
 import de.cau.cs.kieler.synccharts.text.actions.bridge.LabelParserBridgePlugin;
 
 /**
- * Will trigger a re-serialization of an Action label when one of its underlying
- * features has changed.
+ * Will trigger a re-serialization of an Action label when one of its underlying features has
+ * changed.
  * 
  * @author haf
  * 
@@ -23,26 +24,34 @@ public class SerializerListener extends FireOnceTriggerListener {
     private ActionLabelProcessorWrapper actionLabelProcessor = new ActionLabelProcessorWrapper();
 
     public SerializerListener() {
-        super(NotificationFilter.createFeatureFilter(
-                SyncchartsPackage.eINSTANCE.getAction_Delay()).or(
-                NotificationFilter
-                        .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                                .getAction_Effects())).or(
-                NotificationFilter
-                        .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                                .getAction_IsImmediate())).or(
-                NotificationFilter
-                        .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                                .getAction_Trigger())));
+        super(NotificationFilter
+                .createFeatureFilter(SyncchartsPackage.eINSTANCE.getAction_Delay())
+                .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                        .getAction_Effects()))
+                .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                        .getAction_IsImmediate()))
+                .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                        .getAction_Trigger()))
+                .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                        .getState_OutgoingTransitions())));
     }
 
     @Override
-    protected Command trigger(TransactionalEditingDomain domain,
-            Notification notification) {
-        Action action = (Action) notification.getNotifier();
+    protected Command trigger(TransactionalEditingDomain domain, Notification notification) {
+        Action action = null;
+        if (notification.getNotifier() instanceof State
+                && notification.getEventType() == Notification.ADD) {
+            System.out.println("Resource Set Changed: " + notification);
+            action = (Action) notification.getNewValue();
+        } else if (notification.getEventType() == Notification.ADD) {
+            action = (Action) notification.getNotifier();
+        }
+
         if (LabelParserBridgePlugin.getDefault().doAutomaticSerialization()) {
-            return actionLabelProcessor.getProcessActionCommand(action,
+            Command cmd = actionLabelProcessor.getProcessActionCommand(action,
                     ActionLabelProcessorWrapper.SERIALIZE);
+            // cmd.execute();
+            return cmd;
         }
         return null;
     }
