d0c3c9b67558d2e46d3170434acea8772aaf2a8a
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2011-02-08 14:38:48 +0000

    - Added: The Xtend transformation utilities can now plug-in another workflow component into the workflow, right before the transformed model is saved. - Fixed: Now all commenting annotations are imported correctly, along with attachment to entities. (#1432)

--- plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/util/XtendTransformationUtil.java
+++ plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/util/XtendTransformationUtil.java
@@ -14,7 +14,6 @@
 package de.cau.cs.kieler.core.model.util;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -47,12 +46,143 @@ import de.cau.cs.kieler.core.ui.util.MonitoredOperation;
  * Utitlity class to conveniently execute Xtend transformations.
  * 
  * @author haf
+ * @author cds
  * @kieler.rating 2010-06-04 proposed yellow proposed by haf
  */
 @SuppressWarnings("restriction")
 public final class XtendTransformationUtil {
 
     private XtendTransformationUtil() {
+    }
+
+    /**
+     * This transformation uses the Xtend transformation language.
+     * 
+     * @param monitor
+     *            if true a progress bar is displayed
+     * @param xtendFile
+     *            filename that holds the xtend functions
+     * @param startFunction
+     *            initial transformation function
+     * @param inputModelURI
+     *            EMF URI for input model (may be a local resource URI)
+     * @param outputModelURI
+     *            EMF URI for output model
+     * @param workflowHook
+     *            This component is hooked into the workflow just before
+     *            the transformed model is written to a file. Can be used
+     *            to do some last transformation work.
+     * @param involvedMetamodels
+     *            EPackages of metamodels that need to be known to the
+     *            transformation
+     * 
+     * @return the Status about success and errors and warnings
+     */
+    public static XtendStatus model2ModelTransform(final KielerProgressMonitor monitor,
+            final String xtendFile, final String startFunction, final URI inputModelURI,
+            final URI outputModelURI, final TransformationWorkflowHook workflowHook,
+            final EPackage... involvedMetamodels) {
+        
+        monitor.begin("Model2Model transformation", 2);
+
+        // Workflow
+        Workflow workflow = new Workflow();
+        WorkflowContext wfx = new WorkflowContextDefaultImpl();
+
+        // COMPONENT 1: EMF reader
+        // Loads the source model.
+        Reader emfReader = new Reader();
+        emfReader.setUri(inputModelURI.toString());
+        emfReader.setModelSlot("inputmodel");
+        
+        // Ptolemy models may contain strange XML elements that are not
+        // specified in XSD
+        emfReader.getResourceSet().getLoadOptions().put(XMIResource.OPTION_RECORD_UNKNOWN_FEATURE,
+                true);
+        
+        // Add parser option to avoid searching for DTDs online. This would require an
+        // online connection to execute the transformation
+        HashMap<String, Boolean> parserFeatures = new HashMap<String, Boolean>();
+        parserFeatures.put("http://xml.org/sax/features/validation", 
+                Boolean.FALSE);
+        parserFeatures.put("http://apache.org/xml/features/nonvalidating/load-dtd-grammar",
+                Boolean.FALSE); 
+        parserFeatures.put("http://apache.org/xml/features/nonvalidating/load-external-dtd",
+                Boolean.FALSE);
+        emfReader.getResourceSet().getLoadOptions().put(XMLResource.OPTION_PARSER_FEATURES,
+                parserFeatures);
+        
+        // COMPONENT 2: Issue Reporter
+        // Allows to pass information, warnings or errors back to the application
+        ExtIssueReporter issueReporter = new ExtIssueReporter();
+        Issues issues = new org.eclipse.emf.mwe.core.issues.IssuesImpl();
+        M2MProgressMonitor m2mMonitor = new M2MProgressMonitor(monitor, 2);
+        
+        // COMPONENT 3: Xtend transformation
+        // Does the actual transformation.
+        XtendComponent xtendComponent = new XtendComponent();
+        xtendComponent.setInvoke(xtendFile + "::" + startFunction + "(inputmodel)");
+        xtendComponent.setOutputSlot("outputmodel");
+
+        // Add the Metamodels
+        for (EPackage ePackage : involvedMetamodels) {
+            xtendComponent.addMetaModel(new EmfMetaModel(ePackage));
+        }
+        
+        // COMPONENT 4: Workflow Hook
+        // May do some last-minute transformation work.
+        if (workflowHook != null) {
+            workflowHook.setReader(emfReader);
+            workflowHook.setInputModelSlot("inputmodel");
+            workflowHook.setOutputModelSlot("outputmodel");
+        }
+        
+        // COMPONENT 5: EMF Writer
+        // Saves the transformed model.
+        Writer emfWriter = new Writer();
+        emfWriter.setUri(outputModelURI.toString());
+        emfWriter.setModelSlot("outputmodel");
+        emfWriter.setResourceSet(new ResourceSetImpl());
+        
+        // Set up the workflow's components
+        workflow.addComponent(emfReader);
+        workflow.addComponent(issueReporter);
+        workflow.addComponent(xtendComponent);
+        if (workflowHook != null) {
+            workflow.addComponent(workflowHook);
+        }
+        workflow.addComponent(emfWriter);
+        
+        // Run the workflow
+        Exception e = null;
+        try {
+            workflow.invoke(wfx, m2mMonitor, issues);
+        } catch (WorkflowInterruptedException we) {
+            if (we.getMessage().contains("UnknownHostException")) {
+                e = new KielerException("Failed loading Ptolemy file. "
+                        + "Could not resolve the Ptolemy DTD. Unfortunately the parser"
+                        + " currently requires an Internet connection.", we);
+            } else {
+                e = we;
+            }
+        } catch (Exception myE) {
+            e = myE;
+        }
+        
+        // Obtain the unknown features of the input file
+        Map<EObject, AnyType> unknownFeatures = new HashMap<EObject, AnyType>();
+        for (Resource resource : emfReader.getResourceSet().getResources()) {
+            if (resource instanceof XMLResource) {
+                unknownFeatures.putAll(((XMLResource) resource).getEObjectToExtensionMap());
+            }
+        }
+        
+        // Prepare a status object that tells the caller about how the transformation went
+        XtendStatus status = new XtendStatus(issues, xtendComponent.getLogMessage(),
+                unknownFeatures, e);
+        monitor.done();
+        
+        return status;
     }
 
     /**
@@ -77,88 +207,9 @@ public final class XtendTransformationUtil {
     public static XtendStatus model2ModelTransform(final KielerProgressMonitor monitor,
             final String xtendFile, final String startFunction, final URI inputModelURI,
             final URI outputModelURI, final EPackage... involvedMetamodels) {
-        monitor.begin("Model2Model transformation", 2);
-
-        // Workflow
-        Workflow workflow = new Workflow();
-
-        // EMF reader
-        Reader emfReader = new Reader();
-        emfReader.setUri(inputModelURI.toString());
-        emfReader.setModelSlot("inputmodel");
-        // ptolemy models may contain strange XML elements that are not
-        // specified in XSD
-        emfReader.getResourceSet().getLoadOptions().put(XMIResource.OPTION_RECORD_UNKNOWN_FEATURE,
-                true);
-        // add parser option to avoid searching for DTDs online. This would require an
-        // online connection to execute the transformation
-        HashMap<String, Boolean> parserFeatures = new HashMap<String, Boolean>();
-        parserFeatures.put("http://xml.org/sax/features/validation", 
-                Boolean.FALSE);
-        parserFeatures.put("http://apache.org/xml/features/nonvalidating/load-dtd-grammar",
-                Boolean.FALSE); 
-        parserFeatures.put("http://apache.org/xml/features/nonvalidating/load-external-dtd",
-                Boolean.FALSE);
-        emfReader.getResourceSet().getLoadOptions().put(XMLResource.OPTION_PARSER_FEATURES,
-                parserFeatures);
         
-        // EMF Writer for target model
-        Writer emfWriter = new Writer();
-        emfWriter.setUri(outputModelURI.toString());
-        emfWriter.setModelSlot("outputmodel");
-        emfWriter.setResourceSet(new ResourceSetImpl());
-
-        // XtendComponent
-        XtendComponent xtendComponent = new XtendComponent();
-        xtendComponent.setInvoke(xtendFile + "::" + startFunction + "(inputmodel)");
-        xtendComponent.setOutputSlot("outputmodel");
-
-        // add the Metamodels
-        for (EPackage ePackage : involvedMetamodels) {
-            xtendComponent.addMetaModel(new EmfMetaModel(ePackage));
-        }
-
-        // workflow
-        WorkflowContext wfx = new WorkflowContextDefaultImpl();
-        // register Issues component that allows to pass informations,
-        // warnings or errors
-        // back to the application
-        ExtIssueReporter issueReporter = new ExtIssueReporter();
-        Issues issues = new org.eclipse.emf.mwe.core.issues.IssuesImpl();
-        M2MProgressMonitor m2mMonitor = new M2MProgressMonitor(monitor, 2);
-
-        workflow.addComponent(emfReader);
-        workflow.addComponent(issueReporter);
-        workflow.addComponent(xtendComponent);
-        workflow.addComponent(emfWriter);
-        // workflow.invoke(wfx, (ProgressMonitor)monitor.subTask(80),
-        // issues);
-        Exception e = null;
-        try {
-            workflow.invoke(wfx, m2mMonitor, issues);
-        } catch (WorkflowInterruptedException we) {
-            if (we.getMessage().contains("UnknownHostException")) {
-                e = new KielerException("Failed loading Ptolemy file. "
-                        + "Could not resolve the Ptolemy DTD. Unfortunately the parser"
-                        + " currently requires an Internet connection.", we);
-            } else {
-                e = we;
-            }
-        } catch (Exception myE) {
-            e = myE;
-        }
-        // obtain the unknown features of the input file
-        Map<EObject, AnyType> unknownFeatures = new HashMap<EObject, AnyType>();
-        for (Resource resource : emfReader.getResourceSet().getResources()) {
-            if (resource instanceof XMLResource) {
-                unknownFeatures.putAll(((XMLResource) resource).getEObjectToExtensionMap());
-            }
-        }
-
-        XtendStatus status = new XtendStatus(issues, xtendComponent.getLogMessage(),
-                unknownFeatures, e);
-        monitor.done();
-        return status;
+        return model2ModelTransform(monitor, xtendFile, startFunction, inputModelURI,
+                outputModelURI, null, involvedMetamodels);
     }
 
     /**
