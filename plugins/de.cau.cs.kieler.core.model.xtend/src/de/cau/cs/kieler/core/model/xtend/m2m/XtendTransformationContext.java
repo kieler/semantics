36d0f52d19d55a7678b768d70586a7faf3277b73
Ulf Rueegg <uru@informatik.uni-kiel.de>
2011-02-18 16:16:08 +0000

    replaced PlatformUI... with MonitoredOperation...

--- plugins/de.cau.cs.kieler.core.model.xtend/src/de/cau/cs/kieler/core/model/xtend/m2m/XtendTransformationContext.java
+++ plugins/de.cau.cs.kieler.core.model.xtend/src/de/cau/cs/kieler/core/model/xtend/m2m/XtendTransformationContext.java
@@ -17,13 +17,13 @@ import java.util.Map;
 
 import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.xtend.XtendFacade;
 import org.eclipse.xtend.expression.Variable;
 
 import de.cau.cs.kieler.core.model.m2m.ITransformationContext;
 import de.cau.cs.kieler.core.model.m2m.TransformationDescriptor;
 import de.cau.cs.kieler.core.model.xtend.util.XtendTransformationUtil;
+import de.cau.cs.kieler.core.ui.util.MonitoredOperation;
 
 /**
  * {@link ITransformationContext} for the execution of Xtend transformations.
@@ -81,7 +81,7 @@ public class XtendTransformationContext implements ITransformationContext {
 
         if (editingDomain != null) {
             // FIXME workaround to avoid deadlock with FireOnceTriggerListener
-            PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+            MonitoredOperation.runInUI(new Runnable() {
 
                 public void run() {
                     XtendTransformationCommand command = new XtendTransformationCommand(
@@ -94,7 +94,7 @@ public class XtendTransformationContext implements ITransformationContext {
                     }
 
                 }
-            });
+            }, true);
         } else {
             Object o = xtendFacade.call(descriptor.getTransformationName(),
                     descriptor.getParameters());
