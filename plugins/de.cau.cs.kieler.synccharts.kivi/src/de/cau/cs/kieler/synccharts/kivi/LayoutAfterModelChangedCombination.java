e830197c7e3f66cefbf1a129c236631cffae4320
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2011-02-28 14:57:45 +0000

    Extracted the layout after collapsing/expanding nodes into a separate combination (not synccharts specific)

--- plugins/de.cau.cs.kieler.synccharts.kivi/src/de/cau/cs/kieler/synccharts/kivi/LayoutAfterModelChangedCombination.java
+++ plugins/de.cau.cs.kieler.synccharts.kivi/src/de/cau/cs/kieler/synccharts/kivi/LayoutAfterModelChangedCombination.java
@@ -26,95 +26,63 @@ import de.cau.cs.kieler.kiml.ui.layout.LayoutEffect;
 import de.cau.cs.kieler.synccharts.SyncchartsPackage;
 
 /**
- * Applies automatic layout after the model has been changed.
+ * Applies automatic layout after the model has been changed. Listens to 
+ * all model attributes that are relevant for the layout, e.g. all String labels
+ * of the model and the list of signals, etc.
  * 
- * @deprecated (haf) this combination might trigger layout very often, i.e. at every
- *             small model change. This calls layout even during a more complex model
- *             change that involves multiple small changes. Running layout during model
- *             changes sometimes result ConcurrentModificationExceptions.
+ * @deprecated (haf) this combination might trigger layout very often, i.e. at every small model
+ *             change. This calls layout even during a more complex model change that involves
+ *             multiple small changes. Running layout during model changes sometimes result
+ *             ConcurrentModificationExceptions.
  * 
- * @author mmu
+ * @author mmu, haf
  * 
  */
 public class LayoutAfterModelChangedCombination extends AbstractCombination {
 
     private static NotificationFilter modelFilter = NotificationFilter
             .createFeatureFilter(SyncchartsPackage.eINSTANCE.getScope_Label())
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getAction_Label()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_SuspensionTrigger()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_BodyText()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_InnerActions()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_ExitActions()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_EntryActions()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getState_Regions()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getState_OutgoingTransitions()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getState_IncomingTransitions()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getRegion_States()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_Signals()))
-            .or(NotificationFilter
-                    .createFeatureFilter(SyncchartsPackage.eINSTANCE
-                            .getScope_Variables()))
-            .or(NotificationFilter
-                    .createFeatureFilter(KExpressionsPackage.eINSTANCE
-                            .getValuedObject_Name()));
-    private NotificationFilter diagramFilter = NotificationFilter
-            .createFeatureFilter(NotationPackage.eINSTANCE
-                    .getDrawerStyle_Collapsed());
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getAction_Label()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_SuspensionTrigger()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_BodyText()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_InnerActions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_ExitActions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_EntryActions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getState_Regions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getState_OutgoingTransitions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getState_IncomingTransitions()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getRegion_States()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_Signals()))
+            .or(NotificationFilter.createFeatureFilter(SyncchartsPackage.eINSTANCE
+                    .getScope_Variables()))
+            .or(NotificationFilter.createFeatureFilter(KExpressionsPackage.eINSTANCE
+                    .getValuedObject_Name()));
 
     /**
      * Apply automatic layout every time the model changed state is updated.
      * 
      * @param modelState
      *            model changed
-     * @param diagramState
-     *            diagram changed
      */
-    public void execute(final ModelChangeState modelState,
-            final DiagramChangeState diagramState) {
+    public void execute(final ModelChangeState modelState) {
         dontUndo();
-        if (modelState.getSequenceNumber() > diagramState.getSequenceNumber()) {
-            // model changed
-            for (Notification notification : modelState.getChange()
-                    .getNotifications()) {
-                if (modelFilter.matches(notification)
-                        && notification.getNotifier() instanceof EObject) {
-                    schedule(new LayoutEffect(modelState.getWorkbenchPart(),
-                            (EObject) notification.getNotifier(), true, false,
-                            true));
-                }
-            }
-        } else {
-            // diagram changed
-            for (Notification notification : diagramState.getChange()
-                    .getNotifications()) {
-                if (diagramFilter.matches(notification)
-                        && notification.getNotifier() instanceof EObject) {
-                    schedule(new LayoutEffect(diagramState.getWorkbenchPart(),
-                            (EObject) notification.getNotifier(), true, false,
-                            true));
-                }
+        for (Notification notification : modelState.getChange().getNotifications()) {
+            // call layout for every relevant model change. layout effects will be merged by
+            // KiVi to avoid too many effects and to guarantee that the right parent is layouted
+            if (modelFilter.matches(notification) && notification.getNotifier() instanceof EObject) {
+                schedule(new LayoutEffect(modelState.getWorkbenchPart(),
+                        (EObject) notification.getNotifier(), true, false, true));
             }
         }
     }
