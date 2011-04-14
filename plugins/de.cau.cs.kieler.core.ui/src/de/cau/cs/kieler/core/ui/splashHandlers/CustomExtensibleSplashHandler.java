eebf0a90c7c7215af2a9b72aed856604ddde4679
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2011-04-14 09:02:00 +0000

    - Fixed: The splash screen works again. It also shows the version number of KIELER.

--- plugins/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/splashHandlers/CustomExtensibleSplashHandler.java
+++ plugins/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/splashHandlers/CustomExtensibleSplashHandler.java
@@ -1,3 +1,16 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ *
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ *
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.core.ui.splashHandlers;
 
 import java.util.ArrayList;
@@ -58,7 +71,7 @@ public class CustomExtensibleSplashHandler extends BasicSplashHandler {
 
     private Composite fTextPanel;
     
-    Color labelColor;
+    private Color labelColor;
 
     /**
      * 
@@ -312,35 +325,44 @@ public class CustomExtensibleSplashHandler extends BasicSplashHandler {
      */
     private void processSplashElementIcon(
             final IConfigurationElement configurationElement) {
+        
         // Get attribute icon
         String iconImageFilePath = configurationElement
                 .getAttribute(F_ELEMENT_ICON);
+        
         // Abort if an icon attribute was not specified
         if ((iconImageFilePath == null) || (iconImageFilePath.length() == 0)) {
             return;
         }
+        
         // Create a corresponding image descriptor
         ImageDescriptor descriptor = AbstractUIPlugin
                 .imageDescriptorFromPlugin(
                         configurationElement.getNamespaceIdentifier(),
                         iconImageFilePath);
+        
         // Abort if no corresponding image was found
         if (descriptor == null) {
             return;
         }
+        
         // Create the image
         Image image = descriptor.createImage();
+        
         // Abort if image creation failed
         if (image == null) {
             return;
         }
+        
         // Abort if the image does not have dimensions of 50x50
         if ((image.getBounds().width != F_IMAGE_WIDTH)
                 || (image.getBounds().height != F_IMAGE_HEIGHT)) {
+            
             // Dipose of the image
             image.dispose();
             return;
         }
+        
         // Store the image and tooltip
         fImageList.add(image);
     }
