dd2587086d955d4a7249b10efbbb4aa4319f35b7
Christian Motika <cmot@informatik.uni-kiel.de>
2011-05-17 09:52:31 +0000

    recover esterel

---
+++ plugins/de.cau.cs.kieler.esterel.cec/src/de/cau/cs/kieler/esterel/cec/Activator.java
@@ -0,0 +1,59 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.esterel.cec;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author ctr
+ */
+public class Activator extends Plugin {
+
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "de.cau.cs.kieler.esterel.cec";
+
+    // The shared instance
+    private static Activator plugin;
+
+    /**
+     * The constructor.
+     */
+    public Activator() {
+    }
+
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+}
