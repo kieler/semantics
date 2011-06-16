dfb26d53f8a413c62d54fa7225ef2d3e7a394aaa
Christoph Krueger <ckru@informatik.uni-kiel.de>
2011-06-16 09:32:34 +0000

    implemented kivi panning effect

---
+++ plugins/de.cau.cs.kieler.core.model.gmf/src/de/cau/cs/kieler/core/model/gmf/effects/PanningEffect.java
@@ -0,0 +1,133 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+
+package de.cau.cs.kieler.core.model.gmf.effects;
+
+import org.eclipse.draw2d.FigureCanvas;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
+
+import de.cau.cs.kieler.core.kivi.AbstractEffect;
+import de.cau.cs.kieler.core.ui.util.MonitoredOperation;
+
+/**
+ * 
+ * This effect scrolls to the given eobject or coordinates.
+ * 
+ * @author ckru
+ * 
+ */
+public class PanningEffect extends AbstractEffect {
+
+    private EObject target = null;
+
+    private Point coordinates = null;
+
+    private FigureCanvas canvas = null;
+
+    private DiagramDocumentEditor editor = null;
+
+    private boolean animate = false;
+
+    /**
+     * Constructor of the PanningEffect with coordinates as scrolling target.
+     * 
+     * @param editor
+     *            the editor
+     * @param coordinates
+     *            the coordinates to be scrolled to
+     * @param animate
+     *            true if scrolling should be animated
+     */
+    public PanningEffect(final DiagramDocumentEditor editor, final Point coordinates,
+            final boolean animate) {
+        this.coordinates = coordinates;
+        this.editor = editor;
+        this.animate = animate;
+
+        if (editor.getDiagramGraphicalViewer() instanceof ScrollingGraphicalViewer) {
+            ScrollingGraphicalViewer scrollViewer = (ScrollingGraphicalViewer) editor
+                    .getDiagramGraphicalViewer();
+            if (scrollViewer.getControl() instanceof FigureCanvas) {
+                canvas = (FigureCanvas) scrollViewer.getControl();
+            }
+        }
+    }
+
+    /**
+     * Constructor of the PanningEffect with EObject as scrolling target.
+     * 
+     * @param editor
+     *            the editor
+     * @param target
+     *            the coordinates to be scrolled to
+     * @param animate
+     *            true if scrolling should be animated
+     */
+    public PanningEffect(final DiagramDocumentEditor editor, final EObject target,
+            final boolean animate) {
+        this.target = target;
+        this.editor = editor;
+        this.animate = animate;
+
+        if (editor.getDiagramGraphicalViewer() instanceof ScrollingGraphicalViewer) {
+            ScrollingGraphicalViewer scrollViewer = (ScrollingGraphicalViewer) editor
+                    .getDiagramGraphicalViewer();
+            if (scrollViewer.getControl() instanceof FigureCanvas) {
+                canvas = (FigureCanvas) scrollViewer.getControl();
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void execute() {
+        Point coords = null;
+        if (target != null) {
+            EditPart part = editor.getDiagramEditPart().findEditPart(editor.getDiagramEditPart(),
+                    target);
+            if (part instanceof GraphicalEditPart) {
+                GraphicalEditPart graphicalPart = (GraphicalEditPart) part;
+                coords = graphicalPart.getFigure().getBounds().getLocation();
+                graphicalPart.getFigure().translateToAbsolute(coords);
+            }
+        } else if (coordinates != null) {
+            coords = coordinates;
+        }
+        final Point finalCoords = coords;
+        if (finalCoords != null) {
+            if (animate) {
+                MonitoredOperation.runInUI(new Runnable() {
+                    public void run() {
+                        canvas.scrollSmoothTo(finalCoords.x, finalCoords.y);
+                    }
+                }, false);
+            } else {
+                MonitoredOperation.runInUI(new Runnable() {
+                    public void run() {
+                        canvas.scrollTo(finalCoords.x, finalCoords.y);
+                    }
+                }, false);
+
+            }
+
+        }
+    }
+
+}
