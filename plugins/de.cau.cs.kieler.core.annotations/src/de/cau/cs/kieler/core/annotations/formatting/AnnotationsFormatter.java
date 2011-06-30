785b62cbd8df9296dafbc4dcf1d9b4c07a121881
Christian Schneider <chsch@informatik.uni-kiel.de>
2011-06-30 09:51:23 +0000

    Formatting configuration of annotations improved

--- plugins/de.cau.cs.kieler.core.annotations/src/de/cau/cs/kieler/core/annotations/formatting/AnnotationsFormatter.java
+++ plugins/de.cau.cs.kieler.core.annotations/src/de/cau/cs/kieler/core/annotations/formatting/AnnotationsFormatter.java
@@ -3,7 +3,7 @@
  * 
  * http://www.informatik.uni-kiel.de/rtsys/kieler/
  * 
- * Copyright 2010 by
+ * Copyright 2011 by
  * + Christian-Albrechts-University of Kiel
  *   + Department of Computer Science
  *     + Real-Time and Embedded Systems Group
@@ -26,53 +26,63 @@ import de.cau.cs.kieler.core.annotations.services.AnnotationsGrammarAccess;
  * @author chsch
  */
 public class AnnotationsFormatter extends AbstractDeclarativeFormatter {
-	
     /**
-     * Delegates to customConfigureFormatting.
+     * The interface method called by the runtime, delegating to
+     * {@link AnnotationsFormatter#customConfigureFormatting(FormattingConfig, AnnotationsGrammarAccess)}
+     * .
+     * 
+     * @param c the provided {@link FormattingConfig} accepting the provided instructions. 
      */
-	@Override
-	protected void configureFormatting(FormattingConfig c) {
-		customConfigureFormatting(c, (AnnotationsGrammarAccess) getGrammarAccess());
-	}
-	
-	/**
-	 * Method contains actual formatting instructions while GrammarAccess class
-	 * maybe parameterized allowing the reuse within ExpressionsFormatter. 
-	 * @param c FormattingConfig provided by caller
-	 * @param f GrammarAccess provided by caller
-	 */
-	protected void customConfigureFormatting(FormattingConfig c, AnnotationsGrammarAccess f) {
-//            c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
-//            c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
-//            c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
-            
-            c.setNoSpace().before(f.getTypedKeyStringValueAnnotationAccess().getLeftSquareBracketKeyword_2());
-            c.setNoSpace().after(f.getTypedKeyStringValueAnnotationAccess().getLeftSquareBracketKeyword_2());
-	    c.setNoSpace().before(f.getTypedKeyStringValueAnnotationAccess().getRightSquareBracketKeyword_4());
-	    
-            c.setLinewrap().after(f.getImportAnnotationRule());
+    @Override
+    protected void configureFormatting(final FormattingConfig c) {
+        customConfigureFormatting(c, (AnnotationsGrammarAccess) getGrammarAccess());
+    }
 
-            c.setLinewrap().after(f.getCOMMENT_ANNOTATIONRule());
-            c.setLinewrap().after(f.getTagAnnotationRule());
-            c.setLinewrap().after(f.getKeyStringValueAnnotationRule());
-            c.setLinewrap().after(f.getKeyBooleanValueAnnotationRule());
-            c.setLinewrap().after(f.getKeyIntValueAnnotationRule());
-            c.setLinewrap().after(f.getKeyFloatValueAnnotationRule());
-            c.setLinewrap().after(f.getTypedKeyStringValueAnnotationRule());
-            
-            c.setLinewrap().after(f.getCommentAnnotationAccess().getValueCOMMENT_ANNOTATIONTerminalRuleCall_0());
-            c.setLinewrap().after(f.getTagAnnotationAccess().getNameExtendedIDParserRuleCall_1_0());
-            c.setLinewrap().after(f.getKeyStringValueAnnotationAccess().getValueEStringParserRuleCall_2_0());
-            c.setLinewrap().after(f.getKeyBooleanValueAnnotationAccess().getValueBooleanTerminalRuleCall_2_0());
-            c.setLinewrap().after(f.getKeyIntValueAnnotationAccess().getValueINTTerminalRuleCall_2_0());
-            c.setLinewrap().after(f.getKeyFloatValueAnnotationAccess().getValueFloatTerminalRuleCall_2_0());
-            
-            for (Keyword at : f.findKeywords("@")) {            	
-            	c.setNoSpace().after(at);
-            }
-            
-            for (Pair<Keyword, Keyword> pair : f.findKeywordPairs("(", ")")) {
-                c.setIndentation(pair.getFirst(), pair.getSecond());
-            }
-	}
+    /**
+     * Method contains actual formatting instructions while GrammarAccess class maybe parameterized
+     * allowing the reuse within ExpressionsFormatter.
+     * 
+     * @param c
+     *            FormattingConfig provided by caller
+     * @param f
+     *            GrammarAccess provided by caller
+     */
+    protected void customConfigureFormatting(final FormattingConfig c,
+            final AnnotationsGrammarAccess f) {
+        c.setNoSpace().before(
+                f.getTypedKeyStringValueAnnotationAccess().getLeftSquareBracketKeyword_2());
+        c.setNoSpace().after(
+                f.getTypedKeyStringValueAnnotationAccess().getLeftSquareBracketKeyword_2());
+        c.setNoSpace().before(
+                f.getTypedKeyStringValueAnnotationAccess().getRightSquareBracketKeyword_4());
+
+        c.setLinewrap().after(f.getImportAnnotationRule());
+
+        c.setLinewrap().after(f.getCOMMENT_ANNOTATIONRule());
+        c.setLinewrap().after(f.getTagAnnotationRule());
+        c.setLinewrap().after(f.getKeyStringValueAnnotationRule());
+        c.setLinewrap().after(f.getKeyBooleanValueAnnotationRule());
+        c.setLinewrap().after(f.getKeyIntValueAnnotationRule());
+        c.setLinewrap().after(f.getKeyFloatValueAnnotationRule());
+        c.setLinewrap().after(f.getTypedKeyStringValueAnnotationRule());
+
+        c.setLinewrap().after(
+                f.getCommentAnnotationAccess().getValueCOMMENT_ANNOTATIONTerminalRuleCall_0());
+        c.setLinewrap().after(f.getTagAnnotationAccess().getNameExtendedIDParserRuleCall_1_0());
+        c.setLinewrap().after(
+                f.getKeyStringValueAnnotationAccess().getValueEStringParserRuleCall_2_0());
+        c.setLinewrap().after(
+                f.getKeyBooleanValueAnnotationAccess().getValueBooleanTerminalRuleCall_2_0());
+        c.setLinewrap().after(f.getKeyIntValueAnnotationAccess().getValueINTTerminalRuleCall_2_0());
+        c.setLinewrap().after(
+                f.getKeyFloatValueAnnotationAccess().getValueFloatTerminalRuleCall_2_0());
+
+        for (Keyword at : f.findKeywords("@")) {
+            c.setNoSpace().after(at);
+        }
+
+        for (Pair<Keyword, Keyword> pair : f.findKeywordPairs("(", ")")) {
+            c.setIndentation(pair.getFirst(), pair.getSecond());
+        }
+    }
 }
