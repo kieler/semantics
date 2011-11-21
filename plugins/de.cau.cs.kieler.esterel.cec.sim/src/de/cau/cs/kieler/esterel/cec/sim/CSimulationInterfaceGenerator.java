7115cd742f072fcf855bccaa2f670c44274d1c53
Christian Motika <cmot@informatik.uni-kiel.de>
2011-11-21 21:47:32 +0000

    Organized inputs

--- plugins/de.cau.cs.kieler.esterel.cec.sim/src/de/cau/cs/kieler/esterel/cec/sim/CSimulationInterfaceGenerator.java
+++ plugins/de.cau.cs.kieler.esterel.cec.sim/src/de/cau/cs/kieler/esterel/cec/sim/CSimulationInterfaceGenerator.java
@@ -14,47 +14,22 @@
 package de.cau.cs.kieler.esterel.cec.sim;
 
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.core.internal.resources.File;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.XtextDocument;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
-import de.cau.cs.kieler.core.annotations.AnnotationsPackage;
-import de.cau.cs.kieler.core.kexpressions.KExpressionsPackage;
+import com.google.inject.Guice;
+
 import de.cau.cs.kieler.esterel.cec.sim.xtend.Esterel2CSimulationInterface;
 import de.cau.cs.kieler.kies.esterel.Program;
-import de.cau.cs.kieler.kies.esterel.EsterelPackage;
-
-// Needed for @Inject tags for modularization (e.g., the Helper.xtend file)
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
 
 /**
  * @author cmot
