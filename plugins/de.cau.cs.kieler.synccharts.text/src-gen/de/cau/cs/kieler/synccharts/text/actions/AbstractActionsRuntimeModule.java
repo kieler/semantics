f3cd56a032f2ca684765789ef68e2f18e21099a9
Christian Motika <cmot@informatik.uni-kiel.de>
2011-11-24 17:33:13 +0000

    Fix: Generation problems w/ Xtext 2.1

--- plugins/de.cau.cs.kieler.synccharts.text/src-gen/de/cau/cs/kieler/synccharts/text/actions/AbstractActionsRuntimeModule.java
+++ plugins/de.cau.cs.kieler.synccharts.text/src-gen/de/cau/cs/kieler/synccharts/text/actions/AbstractActionsRuntimeModule.java
@@ -39,9 +39,19 @@ public abstract class AbstractActionsRuntimeModule extends DefaultRuntimeModule 
 		return de.cau.cs.kieler.synccharts.text.actions.services.ActionsGrammarAccess.class;
 	}
 
-	// contributed by org.eclipse.xtext.generator.parseTreeConstructor.ParseTreeConstructorFragment
-	public Class<? extends org.eclipse.xtext.parsetree.reconstr.IParseTreeConstructor> bindIParseTreeConstructor() {
-		return de.cau.cs.kieler.synccharts.text.actions.parseTreeConstruction.ActionsParsetreeConstructor.class;
+	// contributed by org.eclipse.xtext.generator.serializer.SerializerFragment
+	public Class<? extends org.eclipse.xtext.serializer.sequencer.ISemanticSequencer> bindISemanticSequencer() {
+		return de.cau.cs.kieler.synccharts.text.actions.serializer.ActionsSemanticSequencer.class;
+	}
+
+	// contributed by org.eclipse.xtext.generator.serializer.SerializerFragment
+	public Class<? extends org.eclipse.xtext.serializer.sequencer.ISyntacticSequencer> bindISyntacticSequencer() {
+		return de.cau.cs.kieler.synccharts.text.actions.serializer.ActionsSyntacticSequencer.class;
+	}
+
+	// contributed by org.eclipse.xtext.generator.serializer.SerializerFragment
+	public Class<? extends org.eclipse.xtext.serializer.ISerializer> bindISerializer() {
+		return org.eclipse.xtext.serializer.impl.Serializer.class;
 	}
 
 	// contributed by org.eclipse.xtext.generator.parser.antlr.XtextAntlrGeneratorFragment
---
+++ plugins/de.cau.cs.kieler.synccharts.text/src/de/cau/cs/kieler/synccharts/text/actions/serializer/ActionsSyntacticSequencer.java
@@ -0,0 +1,4 @@
+package de.cau.cs.kieler.synccharts.text.actions.serializer;
+
+public class ActionsSyntacticSequencer extends AbstractActionsSyntacticSequencer {
+}
