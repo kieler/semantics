6d48d5aebf55b06e9806e7e29979ce719cd59c27
Christian Motika <cmot@informatik.uni-kiel.de>
2011-12-20 13:53:43 +0100

    fixed kev

--- plugins/de.cau.cs.kieler.kev/src/de/cau/cs/kieler/kev/extension/dataobserver/Elevator.java
+++ plugins/de.cau.cs.kieler.kev/src/de/cau/cs/kieler/kev/extension/dataobserver/Elevator.java
@@ -19,7 +19,7 @@ import org.json.JSONObject;
 
 import de.cau.cs.kieler.sim.kiem.IJSONObjectDataComponent;
 import de.cau.cs.kieler.sim.kiem.JSONObjectDataComponent;
-import de.cau.cs.kieler.sim.kiem.JSONSignalValues;
+import de.cau.cs.kieler.sim.signals.JSONSignalValues;
 import de.cau.cs.kieler.sim.kiem.KiemExecutionException;
 import de.cau.cs.kieler.sim.kiem.KiemInitializationException;
 
