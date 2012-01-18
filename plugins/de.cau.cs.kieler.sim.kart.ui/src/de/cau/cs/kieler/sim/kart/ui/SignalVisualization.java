f274f72ae0539056d5c17f9e852422699598530d
Sebastian Schäfer <ssc@informatik.uni-kiel.de>
2012-01-18 16:40:01 +0100

    fixes to extension point definition, added empty implementations for both extension points

---
+++ plugins/de.cau.cs.kieler.sim.kart.ui/src/de/cau/cs/kieler/sim/kart/ui/SignalVisualization.java
@@ -0,0 +1,34 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.sim.kart.ui;
+
+import org.eclipse.swt.graphics.RGB;
+
+import de.cau.cs.kieler.sim.kart.ISignalVisualization;
+
+/**
+ * @author ssc
+ *
+ */
+public class SignalVisualization implements ISignalVisualization {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void visualize(String signalName, RGB color, int step) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
