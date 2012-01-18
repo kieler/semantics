cd4c0349ae9048d20315302dff700b19e088ed2e
Sebastian Schäfer <ssc@informatik.uni-kiel.de>
2012-01-18 16:24:16 +0100

    - added: extension point for visualizations

---
+++ plugins/de.cau.cs.kieler.sim.kart/src/de/cau/cs/kieler/sim/kart/IStateVisualization.java
@@ -0,0 +1,27 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.sim.kart;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+
+/**
+ * @author ssc
+ *
+ */
+public interface IStateVisualization {
+    public void visualize(List<EObject> isStates, List<EObject> shallStates, DiagramEditor editor);
+}
