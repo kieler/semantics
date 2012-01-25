ec5bb5a54d631b2a2029379566888f2e8468deb2
Christian Motika <cmot@informatik.uni-kiel.de>
2012-01-25 18:38:08 +0100

    Repaired KIES icons

--- plugins/de.cau.cs.kieler.kies/src/de/cau/cs/kieler/kies/Activator.java
+++ plugins/de.cau.cs.kieler.kies/src/de/cau/cs/kieler/kies/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "de.cau.cs.kieler.kies.transformation";
+    public static final String PLUGIN_ID = "de.cau.cs.kieler.kies";
 
     // The shared instance
     private static Activator plugin;
