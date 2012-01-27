b2b12d735fc3e3aa5066f02b7f9a7fabd27bc989
Christian Motika <cmot@informatik.uni-kiel.de>
2012-01-27 15:17:21 +0100

    errorStatement support for Esterel simulation

--- plugins/de.cau.cs.kieler.esterel.cec.sim.kivi/src/de/cau/cs/kieler/esterel/cec/sim/kivi/DataComponent.java
+++ plugins/de.cau.cs.kieler.esterel.cec.sim.kivi/src/de/cau/cs/kieler/esterel/cec/sim/kivi/DataComponent.java
@@ -42,26 +42,34 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 
 	/** The Esterel program. */
 	private Program esterelProgram;
-	
+
 	/** The active (and selected) statements, needed to undo. */
 	private LinkedList<EObject> activeStatements = new LinkedList<EObject>();
-	
+
+	/** The error statements, needed to undo. */
+	private LinkedList<EObject> errorStatements = new LinkedList<EObject>();
+
 	/** The recover style range map to recover original style. */
-	private Hashtable<Integer, StyleRange>  recoverStyleRangeMap = new Hashtable<Integer, StyleRange>();
-	
+	private Hashtable<Integer, StyleRange> recoverStyleRangeMap = new Hashtable<Integer, StyleRange>();
+
 	/** The highlight background color. */
-	RGB highlightBackgroundColor =  new RGB(255,180, 180); // light red
-	
-    /** The highlight flag is used internally. */
-    boolean highlight = false;
+	RGB highlightBackgroundColor = new RGB(255, 180, 180); // light red
+
+	/** The error background color. */
+	RGB errorBackgroundColor = new RGB(255, 180, 255); // light magenta
+
+	/**
+	 * The highlight background color is used internally and set NULL for
+	 * original.
+	 */
+	RGB specificBackgroundColor = null;
 
 	// -----------------------------------------------------------------------------
-	
+
 	/**
 	 * Instantiates a new data component.
 	 */
 	public DataComponent() {
-		// TODO Auto-generated constructor stub
 	}
 
 	// -----------------------------------------------------------------------------
@@ -76,15 +84,6 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 	@Override
 	public void initialize() throws KiemInitializationException {
 		this.esterelEditor = this.getEsterelEditor();
-
-//		// Disable/Lock the editing of the editor
-//		Display.getDefault().asyncExec(new Runnable() {
-//			public void run() {
-//				esterelEditor.getInternalSourceViewer().setEditable(false);
-//			}
-//				
-//		});
-		
 		this.esterelProgram = this.getEsterelProgram(this.esterelEditor);
 		refreshEObjectMap();
 		this.semanticResource = this.getEsterelSemanticResource(esterelProgram);
@@ -103,7 +102,16 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 		// Highlight the active statements
 		for (EObject statement : activeStatements) {
 			try {
-				setXtextSelection(statement, false);
+				// null == original background
+				setXtextSelection(statement, null);
+			} catch (KiemInitializationException e) {
+				// Hide any errors
+			}
+		}
+		for (EObject statement : errorStatements) {
+			try {
+				// null == original backgroudn
+				setXtextSelection(statement, null);
 			} catch (KiemInitializationException e) {
 				// Hide any errors
 			}
@@ -121,7 +129,6 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 	 */
 	@Override
 	public boolean isProducer() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -136,7 +143,6 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 	 */
 	@Override
 	public boolean isObserver() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
@@ -153,15 +159,15 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 	String getEncodedEMFId(EObject eObject) {
 		if (eObject.eResource() != null) {
 			String uri = eObject.eResource().getURIFragment(eObject);
-//			uri = uri.replaceAll("/", "x");
-//			uri = uri.replaceAll("@", "");
-//			// FIXME: Why does replaceAll not work for "."?!
-//			while (uri.indexOf(".") > 0) {
-//				String test = uri.substring(0, uri.indexOf("."));
-//				String test2 = uri
-//						.substring(uri.indexOf(".") + 1, uri.length());
-//				uri = test + test2;
-//			}
+			// uri = uri.replaceAll("/", "x");
+			// uri = uri.replaceAll("@", "");
+			// // FIXME: Why does replaceAll not work for "."?!
+			// while (uri.indexOf(".") > 0) {
+			// String test = uri.substring(0, uri.indexOf("."));
+			// String test2 = uri
+			// .substring(uri.indexOf(".") + 1, uri.length());
+			// uri = test + test2;
+			// }
 			uri = (uri.hashCode() + "").replace("-", "M");
 			return uri;
 		}
@@ -184,12 +190,10 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 	EObject getEObject(String eObjectID) {
 		if ((eObjectID == null) || eObjectID.equals("")) {
 			return null;
-		}
-		else if (eObjectMap.containsKey(eObjectID)) {
+		} else if (eObjectMap.containsKey(eObjectID)) {
 			// only do this if editor input has not changed
 			return eObjectMap.get(eObjectID);
-		} 
-		else {
+		} else {
 			// Refresh the map and try again
 			refreshEObjectMap();
 			if (eObjectMap.containsKey(eObjectID)) {
@@ -239,26 +243,17 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 	}
 
 	// -----------------------------------------------------------------------------
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * de.cau.cs.kieler.sim.kiem.ui.datacomponent.JSONObjectSimulationDataComponent
-	 * #doStep(org.json.JSONObject)
-	 */
-	@Override
-	public JSONObject doStep(JSONObject jSONObject)
+	
+	LinkedList<EObject> getStatements(JSONObject jSONObject, String signalName)
 			throws KiemExecutionException {
-		
-		String statementName = this.getProperties()[1].getValue();
+
 		LinkedList<EObject> newActiveStatements = new LinkedList<EObject>();
 
-		if (jSONObject.has(statementName)) {
+		if (jSONObject.has(signalName)) {
 			// Now extract the statements separated by a colon
 			try {
 				String activeStatementsString = jSONObject
-						.getString(statementName);
+						.getString(signalName);
 
 				String[] activeStatementsArray = activeStatementsString
 						.split(",");
@@ -278,24 +273,53 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 						false, false, true, e);
 			}
 		}
+		return newActiveStatements;
+	}
+
+	// -----------------------------------------------------------------------------
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.cau.cs.kieler.sim.kiem.ui.datacomponent.JSONObjectSimulationDataComponent
+	 * #doStep(org.json.JSONObject)
+	 */
+	@Override
+	public JSONObject doStep(JSONObject jSONObject)
+			throws KiemExecutionException {
+		String statementName = this.getProperties()[1].getValue();
+		String errorStatementName = this.getProperties()[2].getValue();
+
 		
+		LinkedList<EObject> newActiveStatements = getStatements(jSONObject, statementName);
+		LinkedList<EObject> newErrorStatements =  getStatements(jSONObject, errorStatementName);
+		LinkedList<EObject> newActiveErrorStatements = new LinkedList<EObject>();
+		newActiveErrorStatements.addAll(newActiveStatements);
+		newActiveErrorStatements.addAll(newErrorStatements);
+
+		// Silent repeat
 		try {
 			if (failSilentRecoveryFromUserEditorChange(newActiveStatements)) {
 				// silently repeat
 				return doStep(jSONObject);
 			}
 		} catch (KiemInitializationException e) {
-			new KiemExecutionException("fail silent recovery from user editor change failed.", false, e);
+			new KiemExecutionException(
+					"fail silent recovery from user editor change failed.",
+					false, e);
 		}
-		
 
 		// Undo Highlighting
 		// Highlight the active statements
-		for (EObject statement : activeStatements) {
-			// remove highlighting only if not again to highlight 
-			if (!newActiveStatements.contains(statement)) {
+		LinkedList<EObject> oldActiveErrorStatements = new LinkedList<EObject>();
+		oldActiveErrorStatements.addAll(activeStatements);
+		oldActiveErrorStatements.addAll(errorStatements);
+		for (EObject statement : oldActiveErrorStatements) {
+			// remove highlighting only if not again to highlight
+			if (!newActiveErrorStatements.contains(statement)) {
 				try {
-					setXtextSelection(statement, false);
+					setXtextSelection(statement, null);
 				} catch (KiemInitializationException e) {
 					throw new KiemExecutionException(
 							"No active Esterel editor for statement visualization.",
@@ -303,17 +327,30 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 				}
 			}
 		}
-		
-		LinkedList<EObject> oldActiveStatements = activeStatements;
+
 		// New active statements
+		LinkedList<EObject> oldActiveStatements = activeStatements;
+		LinkedList<EObject> oldErrorStatements = errorStatements;
 		activeStatements = newActiveStatements;
-		
+		errorStatements = newErrorStatements;
+
 		// Highlight the active statements
-		for (EObject statement : activeStatements) {
+		for (EObject statement : newActiveErrorStatements) {
 			// highlight only if not highlighted before
-			if (!oldActiveStatements.contains(statement)) {
+			if (!oldActiveErrorStatements.contains(statement)
+				|| 
+				oldActiveStatements.contains(statement) && newErrorStatements.contains(statement)
+				||
+				oldErrorStatements.contains(statement) && newActiveStatements.contains(statement)) 
+				{
 				try {
-					setXtextSelection(statement, true);
+					// give preference to error coloring
+					if (errorStatements.contains(statement)) {
+						setXtextSelection(statement, this.errorBackgroundColor);
+					}
+					else {
+						setXtextSelection(statement, this.highlightBackgroundColor);
+					}
 				} catch (KiemInitializationException e) {
 					throw new KiemExecutionException(
 							"No active Esterel editor for statement visualization.",
@@ -326,39 +363,48 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 		return null;
 	}
 
+	// -----------------------------------------------------------------------------
+
 	private Resource getEsterelSemanticResource(EObject semanticElement) {
 		return semanticElement.eResource();
 	}
 
 	ICompositeNode xtextNode;
 	boolean selectionDone;
-	
+
 	// -----------------------------------------------------------------------------
-	
+
 	/**
 	 * Sets the xtext selection to have a specific background color.
-	 *
-	 * @param semanticElement the semantic element
-	 * @param highlight the highlight
-	 * @throws KiemInitializationException the kiem initialization exception
+	 * 
+	 * @param semanticElement
+	 *            the semantic element
+	 * @param highlight
+	 *            the highlight
+	 * @throws KiemInitializationException
+	 *             the kiem initialization exception
 	 */
-	private void setXtextSelection(EObject semanticElement, boolean highlight) throws KiemInitializationException {
-			this.highlight = highlight;
-			setXtextSelection(semanticElement);
+	private void setXtextSelection(EObject semanticElement,
+			RGB specificBackgroundColor) throws KiemInitializationException {
+		this.specificBackgroundColor = specificBackgroundColor;
+		setXtextSelection(semanticElement);
 	}
 
 	// -----------------------------------------------------------------------------
-	
+
 	/**
-	 * Fail silent recovery from user editor change. If the user edits text in the 
-	 * editor during visualization, the highlighting is invalidated but the next time
-	 * he clicks on step (and has focused the right editor) the refreshed editor input
-	 * is used for visualization.
-	 *
-	 * @throws KiemInitializationException the kiem initialization exception
+	 * Fail silent recovery from user editor change. If the user edits text in
+	 * the editor during visualization, the highlighting is invalidated but the
+	 * next time he clicks on step (and has focused the right editor) the
+	 * refreshed editor input is used for visualization.
+	 * 
+	 * @throws KiemInitializationException
+	 *             the kiem initialization exception
 	 */
-	private boolean failSilentRecoveryFromUserEditorChange(LinkedList<EObject> activeStatements) throws KiemInitializationException {
-		
+	private boolean failSilentRecoveryFromUserEditorChange(
+			LinkedList<EObject> activeStatements)
+			throws KiemInitializationException {
+
 		for (EObject statement : activeStatements) {
 			String semanticElementFragment = semanticResource
 					.getURIFragment(statement);
@@ -368,18 +414,19 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 				return true;
 			}
 		}
-		// NO recovery action 
+		// NO recovery action
 		return false;
 	}
-	
-	
+
 	// -----------------------------------------------------------------------------
 
 	/**
 	 * Sets the xtext selection to have the last used background color.
-	 *
-	 * @param semanticElement the new xtext selection
-	 * @throws KiemInitializationException the kiem initialization exception
+	 * 
+	 * @param semanticElement
+	 *            the new xtext selection
+	 * @throws KiemInitializationException
+	 *             the kiem initialization exception
 	 */
 	private void setXtextSelection(EObject semanticElement)
 			throws KiemInitializationException {
@@ -393,8 +440,7 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 		EObject semanticElementInDocument = xtextResource
 				.getEObject(semanticElementFragment);
 		xtextNode = NodeModelUtils.findActualNodeFor(semanticElementInDocument);
-		
-		
+
 		if (xtextNode != null) {
 			selectionDone = false;
 			Display.getDefault().asyncExec(new Runnable() {
@@ -405,37 +451,47 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 					// getOffset() and getLength() are trimming whitespaces
 					int offset = xtextNode.getOffset();
 
-
-					// Find the next leaf node element (the actual Esterel Statement)
+					// Find the next leaf node element (the actual Esterel
+					// Statement)
 					// and get its legth
-					int length = NodeModelUtils.findLeafNodeAtOffset(xtextNode, offset).getLength();
-					
+					int length = NodeModelUtils.findLeafNodeAtOffset(xtextNode,
+							offset).getLength();
+
 					xtextEditor.getInternalSourceViewer().setRangeIndication(
 							offset, length, true);
-					xtextEditor.getInternalSourceViewer().revealRange(
-							offset, length);
+					xtextEditor.getInternalSourceViewer().revealRange(offset,
+							length);
 
-					
 					StyleRange styleRange = null;
-					if (highlight) {
+					// if any background color is set, then use this
+					if (specificBackgroundColor != null) {
 						// Save the current style before
-						StyleRange backupStyleRange = xtextEditor.getInternalSourceViewer().getTextWidget().getStyleRangeAtOffset(offset);
+						StyleRange backupStyleRange = xtextEditor
+								.getInternalSourceViewer().getTextWidget()
+								.getStyleRangeAtOffset(offset);
 						recoverStyleRangeMap.put(offset, backupStyleRange);
-						Color highlightColor = new Color(Display.getCurrent(), highlightBackgroundColor);
-						styleRange = new StyleRange(offset, length, backupStyleRange.foreground, highlightColor);
+						Color highlightColor = new Color(Display.getCurrent(),
+								specificBackgroundColor);
+						styleRange = new StyleRange(offset, length,
+								backupStyleRange.foreground, highlightColor);
 					} else {
 						// Recover the old style
-						StyleRange recoverStyleRange = recoverStyleRangeMap.get(offset);
+						StyleRange recoverStyleRange = recoverStyleRangeMap
+								.get(offset);
 						if (recoverStyleRange != null) {
-							styleRange = new StyleRange(offset, length, recoverStyleRange.foreground, recoverStyleRange.background, recoverStyleRange.fontStyle);
+							styleRange = new StyleRange(offset, length,
+									recoverStyleRange.foreground,
+									recoverStyleRange.background,
+									recoverStyleRange.fontStyle);
 							recoverStyleRangeMap.remove(offset);
 						}
 					}
-					
+
 					if (styleRange != null) {
-						xtextEditor.getInternalSourceViewer().getTextWidget().setStyleRange(styleRange);
+						xtextEditor.getInternalSourceViewer().getTextWidget()
+								.setStyleRange(styleRange);
 					}
-					
+
 					selectionDone = true;
 				}
 			});
@@ -449,19 +505,21 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 		}
 	}
 
-	
 	// -------------------------------------------------------------------------
-	
-	/* (non-Javadoc)
-	 * @see de.cau.cs.kieler.sim.kiem.internal.AbstractDataComponent#isHistoryObserver()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.cau.cs.kieler.sim.kiem.internal.AbstractDataComponent#isHistoryObserver
+	 * ()
 	 */
 	@Override
-    public boolean isHistoryObserver() {
-        return true;
-    }
-		
-	// -----------------------------------------------------------------------------
+	public boolean isHistoryObserver() {
+		return true;
+	}
 
+	// -----------------------------------------------------------------------------
 
 	/**
 	 * Gets the Esterel editor.
@@ -493,9 +551,11 @@ public class DataComponent extends JSONObjectSimulationDataComponent {
 
 	@Override
 	public KiemProperty[] doProvideProperties() {
-		final int nProperties = 1;
+		final int nProperties = 2;
 		KiemProperty[] properties = new KiemProperty[nProperties];
 		properties[0] = new KiemProperty("Statement Name", "statement");
+		properties[1] = new KiemProperty("Error Statement Name",
+				"errorStatement");
 
 		return properties;
 	}
