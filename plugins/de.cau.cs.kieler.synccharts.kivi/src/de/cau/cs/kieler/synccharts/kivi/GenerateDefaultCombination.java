14ab6e20c7a4bf6ae9adabea8654a54c00a35fec
ckru <christoph.krueger@campusbusinessbox.de>
2012-02-03 10:12:25 +0100

    enhanced synccharts default combination

--- plugins/de.cau.cs.kieler.synccharts.kivi/src/de/cau/cs/kieler/synccharts/kivi/GenerateDefaultCombination.java
+++ plugins/de.cau.cs.kieler.synccharts.kivi/src/de/cau/cs/kieler/synccharts/kivi/GenerateDefaultCombination.java
@@ -23,8 +23,15 @@ import java.net.URL;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.NotificationFilter;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListener;
+import org.eclipse.emf.transaction.RollbackException;
+import org.eclipse.gmf.runtime.diagram.core.DiagramEditingDomainFactory;
 
 import de.cau.cs.kieler.core.kexpressions.Signal;
 import de.cau.cs.kieler.core.kivi.AbstractCombination;
@@ -43,71 +50,75 @@ import de.cau.cs.kieler.synccharts.diagram.part.SyncchartsDiagramEditor;
  * @author ckru
  *
  */
-public class GenerateDefaultCombination extends AbstractCombination {
+public class GenerateDefaultCombination extends AbstractCombination implements ResourceSetListener {
 
+    String absolutePath = null;
+    
+    public GenerateDefaultCombination() {
+        InputStream inStream;
+        StringBuffer contentBuffer = new StringBuffer();
+        URL url = Activator.getDefault().getBundle()
+                .getEntry("/transformations/feature.ext");
+        try {
+            inStream = url.openStream();
+
+            while (inStream.available() > 0) {
+                contentBuffer.append((char) inStream.read());
+            }
+            IPath path = ResourcesPlugin.getPlugin().getStateLocation();
+            // Transformation file name:
+
+            path = path.append("Synccharts");
+            // Add extension:
+            path = path.addFileExtension(".ext");
+
+            File file = new File(path.toOSString());
+            if (file != null) {
+                FileOutputStream out = null;
+                try {
+                    out = new FileOutputStream(file);
+                    if (out != null) {
+                        if (!file.exists()) {
+                            if (!file.createNewFile()) {
+                                // error
+                            }
+                        }
+
+                        out.write(contentBuffer.toString().getBytes());
+                        out.flush();
+                        out.close();
+                    }
+                } catch (FileNotFoundException fne) {
+                    fne.printStackTrace();
+                } catch (SecurityException sece) {
+                    sece.printStackTrace();
+                } finally {
+                    if (out != null) {
+                        out.close();
+                    }
+                }
+                // Set delete on exit flag, so the files will be cleaned
+                // when exiting
+                // eclipse
+                file.deleteOnExit();
+                absolutePath = file.getAbsolutePath();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    
     public void execute(final DiagramState diagram) {
         if (diagram.getDiagramPart() instanceof SyncchartsDiagramEditor) {
             SyncchartsDiagramEditor sde = (SyncchartsDiagramEditor) diagram.getDiagramPart();
             EObject object = diagram.getSemanticModel();
             Region r = (Region) object;
+            sde.getEditingDomain().addResourceSetListener(this);
             EList<State> states = r.getStates();
             EList<Signal> signals = r.getSignals();
-            String absolutePath = null;
-            if (states.isEmpty() && signals.isEmpty()) {
-
-                InputStream inStream;
-                StringBuffer contentBuffer = new StringBuffer();
-                URL url = Activator.getDefault().getBundle()
-                        .getEntry("/transformations/feature.ext");
-                try {
-                    inStream = url.openStream();
-
-                    while (inStream.available() > 0) {
-                        contentBuffer.append((char) inStream.read());
-                    }
-                    IPath path = ResourcesPlugin.getPlugin().getStateLocation();
-                    // Transformation file name:
-
-                    path = path.append(sde.getContributorId());
-                    // Add extension:
-                    path = path.addFileExtension(".ext");
-
-                    File file = new File(path.toOSString());
-                    if (file != null) {
-                        FileOutputStream out = null;
-                        try {
-                            out = new FileOutputStream(file);
-                            if (out != null) {
-                                if (!file.exists()) {
-                                    if (!file.createNewFile()) {
-                                        // error
-                                    }
-                                }
-
-                                out.write(contentBuffer.toString().getBytes());
-                                out.flush();
-                                out.close();
-                            }
-                        } catch (FileNotFoundException fne) {
-                            fne.printStackTrace();
-                        } catch (SecurityException sece) {
-                            sece.printStackTrace();
-                        } finally {
-                            if (out != null) {
-                                out.close();
-                            }
-                        }
-                        // Set delete on exit flag, so the files will be cleaned
-                        // when exiting
-                        // eclipse
-                        file.deleteOnExit();
-                        absolutePath = file.getAbsolutePath();
-                    }
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
+            if (states.isEmpty() && signals.isEmpty()) {            
                 if (absolutePath != null) {
                     Object[] mapping = { r };
                     TransformationDescriptor descriptor = new TransformationDescriptor(
@@ -122,6 +133,64 @@ public class GenerateDefaultCombination extends AbstractCombination {
                 }
             }
         }
+    }
+
+    public NotificationFilter getFilter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Command transactionAboutToCommit(ResourceSetChangeEvent event) throws RollbackException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void resourceSetChanged(ResourceSetChangeEvent event) {
+        for (Notification n : event.getNotifications()) {
+            if ((n.getEventType() == Notification.REMOVE) && (n.getNotifier() instanceof Region)) {
+                Region r = this.getRootRegion((Region) n.getNotifier());
+                EList<State> states = r.getStates();
+                EList<Signal> signals = r.getSignals();
+                if (states.isEmpty() && signals.isEmpty()) {
+                if (absolutePath != null) {
+                    Object[] mapping = { r };
+                    TransformationDescriptor descriptor = new TransformationDescriptor(
+                            "createDefault", mapping);
+                    String[] packages = { "de.cau.cs.kieler.synccharts.SyncchartsPackage" };
+
+                    XtendTransformationContext context = new XtendTransformationContext(
+                            absolutePath, packages, null, event.getEditingDomain());
+                    XtendTransformationEffect effect = new XtendTransformationEffect(context,
+                            descriptor);
+                    effect.schedule();
+                }
+                }
+            }
+        }
+        
+    }
+
+    private Region getRootRegion(Region r) {
+        if (r.getParentState() == null) {
+            return r;
+        } else {
+            return this.getRootRegion(r.getParentState().getParentRegion());
+        }
+    }
+    
+    public boolean isAggregatePrecommitListener() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isPrecommitOnly() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean isPostcommitOnly() {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
