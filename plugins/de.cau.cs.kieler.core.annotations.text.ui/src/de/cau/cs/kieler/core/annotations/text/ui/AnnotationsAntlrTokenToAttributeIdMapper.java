80733f1c6d92a1d54715c45916351ac1afec5712
Christian Schneider <chsch@informatik.uni-kiel.de>
2012-05-10 17:12:02 +0200

    polish of annotation.text.ui's code providing the highlighting and the proposals

--- plugins/de.cau.cs.kieler.core.annotations.text.ui/src/de/cau/cs/kieler/core/annotations/text/ui/AnnotationsAntlrTokenToAttributeIdMapper.java
+++ plugins/de.cau.cs.kieler.core.annotations.text.ui/src/de/cau/cs/kieler/core/annotations/text/ui/AnnotationsAntlrTokenToAttributeIdMapper.java
@@ -1,22 +1,38 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.core.annotations.text.ui;
 
-import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultAntlrTokenToAttributeIdMapper;
 
 /**
- * Custom {@link AbstractAntlrTokenToAttributeIdMapper} contributing highlighting to Xtext editors
- * encompassing Annotations. It defines custom mappings of keywords and terminals to highlighting
- * configurations.
+ * Custom {@link org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper}
+ * contributing highlighting to Xtext editors encompassing Annotations. It defines custom mappings
+ * of keywords and terminals to highlighting configurations.
  * 
  * @author chsch
  */
 public class AnnotationsAntlrTokenToAttributeIdMapper extends DefaultAntlrTokenToAttributeIdMapper {
 
-    protected String calculateId(String tokenName, int tokenType) {
+    @Override
+    protected String calculateId(final String tokenName, final int tokenType) {
         if ("RULE_COMMENT_ANNOTATION".equals(tokenName)) {
             return AnnotationsHighlightingConfiguration.COMMENT_ANNOTATION;
         }
         if ("'@'".equals(tokenName)) {
+            return AnnotationsHighlightingConfiguration.ANNOTATION_KEY;
+        }
+        if ("RULE_ANNOTATION_KEY".equals(tokenName)) {
             return AnnotationsHighlightingConfiguration.ANNOTATION_KEY;
         }
 
