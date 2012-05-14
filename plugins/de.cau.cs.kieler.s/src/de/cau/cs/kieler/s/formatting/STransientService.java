b77abcfcbdcacfb3dc6aadf0e5acb7ab6fec6c95
Christian Motika <cmot@informatik.uni-kiel.de>
2012-05-14 12:36:56 +0200

    do not serialized isInput isOutput for ISignals!

--- plugins/de.cau.cs.kieler.s/src/de/cau/cs/kieler/s/formatting/STransientService.java
+++ plugins/de.cau.cs.kieler.s/src/de/cau/cs/kieler/s/formatting/STransientService.java
@@ -4,11 +4,22 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.parsetree.reconstr.impl.DefaultTransientValueService;
 
+import de.cau.cs.kieler.core.kexpressions.KExpressionsPackage;
+
 public class STransientService extends DefaultTransientValueService {
 
     public boolean isTransient(EObject owner, EStructuralFeature feature, int index) {
-        boolean returnValue = (!owner.eIsSet(feature));
-        return returnValue;
+    	
+    	// chsch & cmot: if element to serialize is an ISignal and processing input or output features then
+    	// do not serialize isInput or isOutput flags (because they are not part of ISignal)
+    	if (KExpressionsPackage.eINSTANCE.getISignal().isInstance(owner)
+    			&& (KExpressionsPackage.eINSTANCE.getSignal_IsInput().equals(feature)
+    			      || KExpressionsPackage.eINSTANCE.getSignal_IsOutput().equals(feature))
+    			) {
+    		return true;
+    	}
+    	
+        return !owner.eIsSet(feature);
     }    
     
 }
