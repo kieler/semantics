481f1886e44614a59154555de56e3c46799e57b4
Christian Motika <cmot@informatik.uni-kiel.de>
2012-05-23 12:09:04 +0200

    comments

--- plugins/de.cau.cs.kieler.synccharts.codegen.s/src/de/cau/cs/kieler/synccharts/codegen/s/CodegenSPlugin.java
+++ plugins/de.cau.cs.kieler.synccharts.codegen.s/src/de/cau/cs/kieler/synccharts/codegen/s/CodegenSPlugin.java
@@ -17,39 +17,47 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author cmot
  */
 public class CodegenSPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
+	/** The plug-in ID. */
 	public static final String PLUGIN_ID = "de.cau.cs.kieler.synccharts.codegen.s"; //$NON-NLS-1$
 
-	// The shared instance
+	/** The shared instance. */
 	private static CodegenSPlugin plugin;
 	
+    // -------------------------------------------------------------------------
+
 	/**
 	 * The constructor
 	 */
 	public CodegenSPlugin() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
+    // -------------------------------------------------------------------------
+
+    /**
+     * The constructor of the table plug-in.
+     */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+    // -------------------------------------------------------------------------
+
+	/**
+     * The constructor of the table plug-in.
+     */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
+
+	// -------------------------------------------------------------------------
 
 	/**
 	 * Returns the shared instance
@@ -60,4 +68,5 @@ public class CodegenSPlugin extends AbstractUIPlugin {
 		return plugin;
 	}
 
+    // -------------------------------------------------------------------------
 }
