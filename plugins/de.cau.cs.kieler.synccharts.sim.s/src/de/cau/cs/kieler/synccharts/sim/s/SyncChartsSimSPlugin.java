d07b08557556d0965809c8da3117ea2e41ffd30d
Christian Motika <cmot@informatik.uni-kiel.de>
2012-05-23 12:11:57 +0200

    comments and pom file

--- plugins/de.cau.cs.kieler.synccharts.sim.s/src/de/cau/cs/kieler/synccharts/sim/s/SyncChartsSimSPlugin.java
+++ plugins/de.cau.cs.kieler.synccharts.sim.s/src/de/cau/cs/kieler/synccharts/sim/s/SyncChartsSimSPlugin.java
@@ -1,42 +1,63 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.synccharts.sim.s;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author cmot
  */
 public class SyncChartsSimSPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
+	/** The plug-in ID. */
 	public static final String PLUGIN_ID = "de.cau.cs.kieler.synccharts.sim.s"; //$NON-NLS-1$
 
-	// The shared instance
+	/** The shared instance. */
 	private static SyncChartsSimSPlugin plugin;
 	
+    // -------------------------------------------------------------------------
+
 	/**
 	 * The constructor
 	 */
 	public SyncChartsSimSPlugin() {
 	}
+	
+    // -------------------------------------------------------------------------
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
+    /**
+     * {@inheritDoc}
+     */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
+
+    // -------------------------------------------------------------------------
 
 	/**
 	 * Returns the shared instance
@@ -47,4 +68,5 @@ public class SyncChartsSimSPlugin extends AbstractUIPlugin {
 		return plugin;
 	}
 
+    // -------------------------------------------------------------------------
 }
