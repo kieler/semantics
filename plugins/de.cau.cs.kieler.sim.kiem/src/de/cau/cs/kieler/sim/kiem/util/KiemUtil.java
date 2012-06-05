3da22eeebc6849547742bed41c5d372373d72f3b
Christian Motika <cmot@informatik.uni-kiel.de>
2012-06-05 17:38:02 +0200

    file path conversion

--- plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/util/KiemUtil.java
+++ plugins/de.cau.cs.kieler.sim.kiem/src/de/cau/cs/kieler/sim/kiem/util/KiemUtil.java
@@ -14,11 +14,15 @@
 
 package de.cau.cs.kieler.sim.kiem.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
 
@@ -38,14 +42,13 @@ public final class KiemUtil {
 
     /**
      * Open a bundle or workspace file and return an InputStream.
-     * 
-     * @param fileAbsolute
-     *            the file absolute
+     *
+     * @param fileAbsolute the file absolute
+     * @param pluginID the plugin id
      * @return the input stream
-     * @throws IOException
-     *             Signals that an I/O exception has occurred.
+     * @throws IOException Signals that an I/O exception has occurred.
      */
-    static InputStream openBundleOrWorkspaceFile(final URL fileAbsolute, final String pluginID)
+    public static InputStream openBundleOrWorkspaceFile(final URL fileAbsolute, final String pluginID)
             throws IOException {
         // if bundle entry then just to string
         if (fileAbsolute.toString().contains("bundleentry")) {
@@ -65,4 +68,71 @@ public final class KiemUtil {
 
     // -------------------------------------------------------------------------
 
+    /**
+     * Gets the absolute file path.
+     * 
+     * @param url
+     *            the url
+     * @return the absolute file path
+     */
+    public static String getAbsoluteFilePath(final URL url) {
+        // if bundle entry then just to string
+        if (url.toString().contains("bundleentry")) {
+            return url.toString();
+        }
+        IPath urlPath = new Path(url.getFile());
+        return getAbsoluteFilePath(urlPath);
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Gets the absolute file path.
+     * 
+     * @param ifile
+     *            the file
+     * @return the absolute file path
+     */
+    // To resolve references to locations outside the workspace
+    // org.eclipse.core.internal.resources.Resource seems to be needed.
+    // Is there a better alternative? -> "restriction"
+    @SuppressWarnings({ "restriction" })
+   public static String getAbsoluteFilePath(final IFile ifile) {
+        IPath fullPath = ifile.getLocation();
+        // If we have spaces, try it like this...
+        if (fullPath == null && ifile instanceof org.eclipse.core.internal.resources.Resource) {
+            org.eclipse.core.internal.resources.Resource resource = 
+                    (org.eclipse.core.internal.resources.Resource) ifile;
+            fullPath = resource.getLocalManager().locationFor(resource);
+        }
+        return (getAbsoluteFilePath(fullPath));
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Gets the absolute file path.
+     * 
+     * @param ipath
+     *            the ipath
+     * @return the absolute file path
+     */
+    public static String getAbsoluteFilePath(final IPath ipath) {
+        // if bundle entry then just to string
+        if (ipath.toString().contains("bundleentry")) {
+            return ipath.toString();
+        }
+        // Ensure it is absolute
+        ipath.makeAbsolute();
+        java.io.File javaFile = new File(ipath.toString().replaceAll("%20", " "));
+        if (javaFile.exists()) {
+            String fileString = javaFile.getAbsolutePath();
+            return fileString;
+        }
+        // Something went wrong, we could not resolve the file location
+        return null;
+    }
+
+    // -------------------------------------------------------------------------
+    
 }
