430dc3b06056a2aed47e66f2f7d81adef7ef99d6
Christian Motika <cmot@informatik.uni-kiel.de>
2012-06-28 15:53:39 +0200

    checkstyle warning

--- plugins/de.cau.cs.kieler.esterel.cec/src/de/cau/cs/kieler/esterel/cec/CEC.java
+++ plugins/de.cau.cs.kieler.esterel.cec/src/de/cau/cs/kieler/esterel/cec/CEC.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.eclipse.core.runtime.Path;
