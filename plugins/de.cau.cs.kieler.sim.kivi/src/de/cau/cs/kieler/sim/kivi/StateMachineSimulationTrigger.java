85cfe5adb06ae83da22120e91586fecb9ae1624c
Christian Motika <cmot@informatik.uni-kiel.de>
2012-07-10 12:37:06 +0200

    removed Checkstyle warnings

--- plugins/de.cau.cs.kieler.sim.kivi/src/de/cau/cs/kieler/sim/kivi/StateMachineSimulationTrigger.java
+++ plugins/de.cau.cs.kieler.sim.kivi/src/de/cau/cs/kieler/sim/kivi/StateMachineSimulationTrigger.java
@@ -14,9 +14,7 @@
 package de.cau.cs.kieler.sim.kivi;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
@@ -26,18 +24,22 @@ import de.cau.cs.kieler.core.kivi.AbstractTriggerState;
 import de.cau.cs.kieler.core.kivi.ITrigger;
 
 /**
- * A trigger notifying the view management about the internal model states and
- * simulation state during simulation.
+ * A trigger notifying the view management about the internal model states and simulation state
+ * during simulation.
  * 
- * @author haf
+ * @author haf, cmot
  * 
  */
 public class StateMachineSimulationTrigger extends AbstractTrigger {
 
+    /** The instance. */
     private static StateMachineSimulationTrigger instance;
 
+    /** The last state. */
     private StateMachineSimulationState lastState;
-    
+
+    // ------------------------------------------------------------------------
+
     /**
      * Convenience method to get the current instance of this trigger.
      * 
@@ -47,76 +49,137 @@ public class StateMachineSimulationTrigger extends AbstractTrigger {
         return instance;
     }
 
+    // ------------------------------------------------------------------------
+
     /**
      * Give map of active states to the view management.
      * 
-     * @param activeStates
-     *            map of active states
      * @param editor
      *            the diagram editor
      */
     public void commitStep(final DiagramEditor editor) {
-        if(lastState != null){
+        if (lastState != null) {
             trigger(lastState);
         }
-        
+
     }
-    
-    public StateMachineSimulationState getCurrentSimulationState(final DiagramEditor editor,boolean createNew){
-        if(createNew){
-            lastState = new StateMachineSimulationState(true,editor);
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Gets the current simulation state.
+     * 
+     * @param editor
+     *            the editor
+     * @param createNew
+     *            the create new
+     * @return the current simulation state
+     */
+    public StateMachineSimulationState getCurrentSimulationState(final DiagramEditor editor,
+            final boolean createNew) {
+        if (createNew) {
+            lastState = new StateMachineSimulationState(true, editor);
         }
         return lastState;
     }
-    
-    public void stopSimulationState(final DiagramEditor editor){
-        trigger(new StateMachineSimulationState(false,editor));
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Stop simulation state.
+     * 
+     * @param editor
+     *            the editor
+     */
+    public void stopSimulationState(final DiagramEditor editor) {
+        trigger(new StateMachineSimulationState(false, editor));
     }
-    
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void register() {
         instance = this;
     }
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void unregister() {
         instance = null;
     }
 
+    // ------------------------------------------------------------------------
+
     /**
-     * A TriggerState for a KIEM simulation of a State Machine, e.g. a SyncChart. It
-     * contains categorized lists of objects that are semantically relevant in a 
-     * simulation step. These are for example the active states, transitions that have been
-     * taken in that step, signals that are present and values of signals and variables.
+     * A TriggerState for a KIEM simulation of a State Machine, e.g. a SyncChart. It contains
+     * categorized lists of objects that are semantically relevant in a simulation step. These are
+     * for example the active states, transitions that have been taken in that step, signals that
+     * are present and values of signals and variables.
      */
     public static final class StateMachineSimulationState extends AbstractTriggerState {
 
-        List<EObject> activeStates;
-        List<EObject> takenTransitions;
-        List<EObject> presentSignals;
-        List<Object>  signalValues;
-        List<Object>  variableValues;
-        
+        /** The active states. */
+        private List<EObject> activeStates;
+
+// cmot: yet unused member variables        
+//        /** The taken transitions. */
+//        private List<EObject> takenTransitions;
+//
+//        /** The present signals. */
+//        private List<EObject> presentSignals;
+//
+//        /** The signal values. */
+//        private List<Object> signalValues;
+//
+//        /** The variable values. */
+//        private List<Object> variableValues;
+
+        /** The is simulating. */
         private boolean isSimulating = false;
 
+        /** The diagram editor. */
         private DiagramEditor diagramEditor;
 
-        public StateMachineSimulationState(boolean isSimulating,DiagramEditor editor) {
+        // --------------------------------------------------------------------
+
+        /**
+         * Instantiates a new state machine simulation state.
+         * 
+         * @param isSimulating
+         *            the is simulating
+         * @param editor
+         *            the editor
+         */
+        public StateMachineSimulationState(final boolean isSimulating, final DiagramEditor editor) {
             this.isSimulating = isSimulating;
             this.diagramEditor = editor;
         }
 
-        public List<EObject> getActiveStates(int stepsAgo){
-            if(activeStates == null){
+        // --------------------------------------------------------------------
+
+        /**
+         * Gets the active states.
+         * 
+         * @param stepsAgo
+         *            the steps ago
+         * @return the active states
+         */
+        public List<EObject> getActiveStates(final int stepsAgo) {
+            if (activeStates == null) {
                 activeStates = new ArrayList<EObject>();
             }
             return activeStates;
         }
-        
-        
-        
-        
-        
+
+        // --------------------------------------------------------------------
+
         /**
          * Default Constructor.
          */
@@ -124,12 +187,16 @@ public class StateMachineSimulationTrigger extends AbstractTrigger {
 
         }
 
+        // --------------------------------------------------------------------
+
         /**
          * {@inheritDoc}
          */
         public Class<? extends ITrigger> getTriggerClass() {
             return StateMachineSimulationTrigger.class;
         }
+
+        // --------------------------------------------------------------------
 
         /**
          * Get the list of active states.
@@ -139,7 +206,9 @@ public class StateMachineSimulationTrigger extends AbstractTrigger {
         public boolean isSimulating() {
             return isSimulating;
         }
-        
+
+        // --------------------------------------------------------------------
+
         /**
          * Get the diagram editor where the simulation is running.
          * 
@@ -150,6 +219,8 @@ public class StateMachineSimulationTrigger extends AbstractTrigger {
             return diagramEditor;
         }
 
+        // --------------------------------------------------------------------
     }
 
+    // ---------------------------------------------------------------------------
 }
