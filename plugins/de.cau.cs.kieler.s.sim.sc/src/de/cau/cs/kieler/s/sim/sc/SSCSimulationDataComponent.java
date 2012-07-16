b7c3fa3215724099e7b506867063d98b7e44c771
Christian Motika <cmot@informatik.uni-kiel.de>
2012-07-16 21:56:29 +0200

    minimal buffer size

--- plugins/de.cau.cs.kieler.s.sim.sc/src/de/cau/cs/kieler/s/sim/sc/SSCSimulationDataComponent.java
+++ plugins/de.cau.cs.kieler.s.sim.sc/src/de/cau/cs/kieler/s/sim/sc/SSCSimulationDataComponent.java
@@ -13,11 +13,7 @@
  */
 package de.cau.cs.kieler.s.sim.sc;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Map;
@@ -52,7 +48,6 @@ import de.cau.cs.kieler.sim.kiem.properties.KiemPropertyTypeFile;
 import de.cau.cs.kieler.sim.kiem.ui.datacomponent.JSONObjectSimulationDataComponent;
 import de.cau.cs.kieler.sim.kiem.util.KiemUtil;
 import de.cau.cs.kieler.sim.signals.JSONSignalValues;
-import de.cau.cs.kieler.core.kexpressions.ValueType;
 
 /**
  * The SimulationDataComponent for simulating S code with and without visualization.
@@ -75,12 +70,15 @@ public class SSCSimulationDataComponent extends JSONObjectSimulationDataComponen
     private static final int NUMBER_OF_TASKS = 10;
 
     private static final int KIEM_PROPERTY_FULLDEBUGMODE = 3;
-    
+
     /** The estimated maximum size used for a pure signal. */
     private static final int PURE_SIGNAL_BUFFER_CONSTANT = 21;
 
     /** The estimated maximum size used for a valued signal. */
     private static final int VALUED_SIGNAL_BUFFER_CONSTANT = 100;
+    
+    /** The Constant MINIMAL_BUFFER_SIZE. */
+    private static final double MINIMAL_BUFFER_SIZE = 2048;
 
     // -------------------------------------------------------------------------
 
@@ -104,32 +102,32 @@ public class SSCSimulationDataComponent extends JSONObjectSimulationDataComponen
      * {@inheritDoc}
      */
     public void initialize() throws KiemInitializationException {
-        
-//        String executable = "C:\\Users\\delphino\\AppData\\Local\\Temp\\SC.exe";
-//        Process executionProcess;
-//        try {
-//            executionProcess = Runtime.getRuntime().exec(executable);
-//            
-//            PrintWriter out = new PrintWriter(new OutputStreamWriter(
-//                    executionProcess.getOutputStream()));
-//            BufferedReader in = new BufferedReader(new InputStreamReader(
-//                    executionProcess.getInputStream()));
-//            BufferedReader err = new BufferedReader(new InputStreamReader(
-//                    executionProcess.getErrorStream()));
-//            
-//            
-//            for (int tick = 0; tick < 10; tick++) {
-//                out.print("\n");
-//                out.flush();
-//                String line = in.readLine();
-//                System.out.println(line);
-//            }
-//            
-//        } catch (IOException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-        
+
+        // String executable = "C:\\Users\\delphino\\AppData\\Local\\Temp\\SC.exe";
+        // Process executionProcess;
+        // try {
+        // executionProcess = Runtime.getRuntime().exec(executable);
+        //
+        // PrintWriter out = new PrintWriter(new OutputStreamWriter(
+        // executionProcess.getOutputStream()));
+        // BufferedReader in = new BufferedReader(new InputStreamReader(
+        // executionProcess.getInputStream()));
+        // BufferedReader err = new BufferedReader(new InputStreamReader(
+        // executionProcess.getErrorStream()));
+        //
+        //
+        // for (int tick = 0; tick < 10; tick++) {
+        // out.print("\n");
+        // out.flush();
+        // String line = in.readLine();
+        // System.out.println(line);
+        // }
+        //
+        // } catch (IOException e) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+
     }
 
     // -------------------------------------------------------------------------
@@ -160,7 +158,7 @@ public class SSCSimulationDataComponent extends JSONObjectSimulationDataComponen
             }
 
             String receivedMessage = scExecution.getExecutionInterfaceFromSC().readLine();
-            
+
             System.out.println(receivedMessage);
 
             if (receivedMessage != null) {
@@ -174,9 +172,9 @@ public class SSCSimulationDataComponent extends JSONObjectSimulationDataComponen
                         boolean sSignalIsPresent = JSONSignalValues.isPresent(sSignalOutput
                                 .getJSONObject(sSignalOutputName));
 
-//                        returnObj.accumulate(sSignalOutputName, sSignalOutput
-//                                .getJSONObject(sSignalOutputName));
-                        
+                        // returnObj.accumulate(sSignalOutputName, sSignalOutput
+                        // .getJSONObject(sSignalOutputName));
+
                         // Test if the output variable is an auxiliary signal
                         // that is only there to mark the current S statement
                         // in full_simulation mode of the simulator.
@@ -380,19 +378,21 @@ public class SSCSimulationDataComponent extends JSONObjectSimulationDataComponen
 
             // Set a random output folder for the compiled files
             String outputFolder = KiemUtil.generateRandomTempOutputFolder();
-            
+
             // Calculate buffersize for SC program
             int bufferSizeInt = 0;
             for (Signal signal : transformedProgram.getSignals()) {
                 if (signal.getType() == ValueType.PURE) {
                     bufferSizeInt += signal.getName().length() + PURE_SIGNAL_BUFFER_CONSTANT;
-                }
-                else {
+                } else {
                     bufferSizeInt += signal.getName().length() + VALUED_SIGNAL_BUFFER_CONSTANT;
                 }
             }
-            double log = Math.ceil(Math.log(bufferSizeInt)/Math.log(2));
-            double bufferSizeDouble = Math.ceil(Math.pow(2, log));
+            double log = Math.ceil(Math.log(bufferSizeInt) / Math.log(2));
+            double bufferSizeDouble = Math.ceil(Math.pow(2, log + 1));
+            if (bufferSizeDouble < MINIMAL_BUFFER_SIZE) {
+                bufferSizeDouble = MINIMAL_BUFFER_SIZE;
+            }
             String bufferSize = bufferSizeDouble + "";
             bufferSize = bufferSize.substring(0, bufferSize.lastIndexOf('.'));
 
