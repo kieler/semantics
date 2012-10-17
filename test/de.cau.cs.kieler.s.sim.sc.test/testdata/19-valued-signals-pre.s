2e300f4401ffdf72eaa7f922b978fb504065b515
Christian Motika <cmot@informatik.uni-kiel.de>
2012-10-18 00:02:31 +0200

    enhanced test

--- test/de.cau.cs.kieler.s.sim.sc.test/testdata/19-valued-signals-pre.s
+++ test/de.cau.cs.kieler.s.sim.sc.test/testdata/19-valued-signals-pre.s
@@ -6,5 +6,9 @@ output signal b : combine integer with * ;
 state ( L_root_surface ) {
 	emit ( a ( 5 ) ) ;
 	pause();
+	emit ( a ( 4 ) ) ;
+	emit ( b(pre(?a)));
+	pause();
+	emit ( a ( 3 ) ) ;
 	emit ( b(pre(?a)));
 }
