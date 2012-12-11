3611f364c58b210386d08718a24a889b95581dc7
wah <wah@informatik.uni-kiel.de>
2012-12-11 23:42:16 +0100

    fixed transition error

--- plugins/de.cau.cs.kieler.yakindu.sccharts.model.scchartstext/src/de/cau/cs/kieler/yakindu/sccharts/model/scchartstext/scchartstext/SCChartsExpRuntimeModule.java
+++ plugins/de.cau.cs.kieler.yakindu.sccharts.model.scchartstext/src/de/cau/cs/kieler/yakindu/sccharts/model/scchartstext/scchartstext/SCChartsExpRuntimeModule.java
@@ -3,9 +3,63 @@
  */
 package de.cau.cs.kieler.yakindu.sccharts.model.scchartstext.scchartstext;
 
+import org.eclipse.xtext.linking.ILinker;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.scoping.IScopeProvider;
+import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+import org.yakindu.base.types.ITypeSystemAccess;
+import org.yakindu.base.types.impl.BaseTypeSystemAccessImpl;
+import org.yakindu.sct.model.sgraph.resource.SCTLinker;
+import org.yakindu.sct.model.stext.conversion.StextValueConverterService;
+import org.yakindu.sct.model.stext.naming.StextNameProvider;
+import org.yakindu.sct.model.stext.scoping.NamespaceLocalScopeResolver;
+import org.yakindu.sct.model.stext.scoping.STextGlobalScopeProvider;
+import org.yakindu.sct.model.stext.validation.ITypeInferrer;
+import org.yakindu.sct.model.stext.validation.TypeInferrer;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
-public class SCChartsExpRuntimeModule extends de.cau.cs.kieler.yakindu.sccharts.model.scchartstext.scchartstext.AbstractSCChartsExpRuntimeModule {
+public class SCChartsExpRuntimeModule extends AbstractSCChartsExpRuntimeModule {
 
+	public Class<? extends org.eclipse.xtext.scoping.IGlobalScopeProvider> bindIGlobalScopeProvider() {
+		super.bindIGlobalScopeProvider();
+		return STextGlobalScopeProvider.class;
+	}
+	
+	@Override
+	public Class<? extends ILinker> bindILinker() {
+		return SCTLinker.class;
+	}
+
+	@Override
+	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
+		return StextNameProvider.class;
+	}
+
+	@Override
+	public Class<? extends org.eclipse.xtext.conversion.IValueConverterService> bindIValueConverterService() {
+		return StextValueConverterService.class;
+	}
+	
+	public Class<? extends ITypeSystemAccess> bindITypeSystemAccess(){
+		return BaseTypeSystemAccessImpl.class;
+	}
+	
+	public Class<? extends ITypeInferrer> bindITypeInferrer(){
+		return TypeInferrer.class;
+	}
+	
+	
+	// contributed by
+	// org.eclipse.xtext.generator.scoping.AbstractScopingFragment
+	public void configureIScopeProviderDelegate(Binder binder) {
+		binder.bind(IScopeProvider.class)
+				.annotatedWith(
+						Names.named(AbstractDeclarativeScopeProvider.NAMED_DELEGATE))
+				.to(NamespaceLocalScopeResolver.class);
+	}
 }
--- plugins/de.cau.cs.kieler.yakindu.sccharts.model.scchartstext/src/de/cau/cs/kieler/yakindu/sccharts/model/scchartstext/scchartstext/validation/SCChartsExpJavaValidator.java
+++ plugins/de.cau.cs.kieler.yakindu.sccharts.model.scchartstext/src/de/cau/cs/kieler/yakindu/sccharts/model/scchartstext/scchartstext/validation/SCChartsExpJavaValidator.java
@@ -1,13 +1,67 @@
 package de.cau.cs.kieler.yakindu.sccharts.model.scchartstext.scchartstext.validation;
+
+import java.util.ArrayList;
+
+import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.validation.CheckType;
+import org.yakindu.sct.model.sgraph.Declaration;
+import org.yakindu.sct.model.stext.validation.STextJavaValidator;
+
+import de.cau.cs.kieler.yakindu.sccharts.model.scchartstext.scchartstext.sCChartsExp.SCChartsExpPackage;
+import de.cau.cs.kieler.yakindu.sccharts.model.scchartstext.scchartstext.sCChartsExp.VariableDefinition;
+import de.cau.cs.kieler.yakindu.sccharts.model.scchartstext.scchartstext.util.SccUtils;
  
 
-public class SCChartsExpJavaValidator extends AbstractSCChartsExpJavaValidator {
+public class SCChartsExpJavaValidator extends STextJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+	
+	/*
+	 *  Check variable definitions
+	 */
+	@Check(CheckType.FAST)
+	public void checkVariableDefinition(VariableDefinition variable) {
+		// load all declarations made in ancestor States
+		// the parameter is variable.eContainer().eContainer() to skip current
+		// state declarations
+		ArrayList<Declaration> declarations = SccUtils
+				.getAncestorDeclarations(variable.eContainer().eContainer());
+		for (Declaration declaration : declarations) {
+			if (declaration != null) {
+				if (declaration.getName().equals(variable.getName())) {
+					error("Variable '" + variable.getName()
+							+ "' already exists in an ancestor State.",
+							SCChartsExpPackage.Literals.VARIABLE_DEFINITION__DIRECTION);
+					return;
+				}
+			}
+		}
+		// load all declarations made in the current sate
+		declarations = SccUtils.getCurrentStateDeclarations(variable
+				.eContainer());
+		// The variable isEscapeError is initially set to true to escape errors.
+		// If a declaration is found in the list with same name, isEscapeError
+		// is set to true. If another declaration with the same name is found,
+		// an error is displayed.
+		boolean isEscapeError = true;
+		for (Declaration declaration : declarations) {
+			if (declaration != null) {
+				if (declaration.getName().equals(variable.getName())) {
+					if (isEscapeError) {
+						isEscapeError = false;
+					} else {
+						error("Variable '" + variable.getName()
+								+ "' exists more than ones in the same State.",
+								SCChartsExpPackage.Literals.VARIABLE_DEFINITION__DIRECTION);
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	// @Check(CheckType.FAST)
+	// public void checkEventDefinition(ReactionTrigger reactionTrigger){
+	// error("ReactionTrigger",null);
+	// }
 
 }
