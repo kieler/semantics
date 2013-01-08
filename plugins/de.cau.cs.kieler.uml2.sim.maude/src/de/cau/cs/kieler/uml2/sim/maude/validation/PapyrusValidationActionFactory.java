0875df7526fa62e4de635206c302266eda8ccea1
Jens Schoenborn <jensschoenborn@gmx.de>
2013-01-08 13:18:46 +0100

    fixed for name changes in papyrus

--- plugins/de.cau.cs.kieler.uml2.sim.maude/src/de/cau/cs/kieler/uml2/sim/maude/validation/PapyrusValidationActionFactory.java
+++ plugins/de.cau.cs.kieler.uml2.sim.maude/src/de/cau/cs/kieler/uml2/sim/maude/validation/PapyrusValidationActionFactory.java
@@ -3,7 +3,8 @@ package de.cau.cs.kieler.uml2.sim.maude.validation;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.action.Action;
-import org.eclipse.papyrus.diagram.statemachine.part.ValidateAction;
+//import org.eclipse.papyrus.uml.diagram.statemachine.part.ValidateAction;
+
 import org.eclipse.papyrus.editor.PapyrusMultiDiagramEditor;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
@@ -11,6 +12,9 @@ import org.eclipse.uml2.uml.UMLPackage;
 
 import de.cau.cs.kieler.core.model.validation.AbstractValidationActionFactory;
 
+/**
+ * {@inheritDoc}
+ */
 public class PapyrusValidationActionFactory extends
         AbstractValidationActionFactory {
 
