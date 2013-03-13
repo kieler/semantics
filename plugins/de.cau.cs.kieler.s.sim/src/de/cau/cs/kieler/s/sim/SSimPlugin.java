e588ca0d30998ee3a31a803a26a9b4c7bfabaefb
Christian Motika <cmot@informatik.uni-kiel.de>
2013-03-13 13:50:47 +0100

    generalize S simulation for SJ

---
+++ plugins/de.cau.cs.kieler.s.sim/src/de/cau/cs/kieler/s/sim/SSimPlugin.java
@@ -0,0 +1,79 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.s.sim;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author cmot 
+ * @kieler.design 2012-10-08 proposed cmot
+ * @kieler.rating 2012-10-08 yellow KI-28
+ */
+public class SSimPlugin extends AbstractUIPlugin {
+
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "de.cau.cs.kieler.s.sim.sc"; //$NON-NLS-1$
+
+    /**
+     * The Constant AUXILIARY_VARIABLE_TAG. This TAG is also defined in S2Simulation.xtend. Both
+     * TAGs must match
+     */
+    public static final String AUXILIARY_VARIABLE_TAG = "oSoAUXILIARYo";
+
+    /** The shared instance. */
+    private static SSimPlugin plugin;
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * The constructor.
+     */
+    public SSimPlugin() {
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    public void stop(final BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the shared instance
+     */
+    public static SSimPlugin getDefault() {
+        return plugin;
+    }
+
+}
