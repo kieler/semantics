f424379f90d6579fc5b359beae94dd2a6e5aca64
Christian Motika <cmot@informatik.uni-kiel.de>
2013-04-08 14:35:36 +0200

    Sharing (reimplemented) LIGHTWEIGHT Synchronous Java project

---
+++ plugins/de.cau.cs.kieler.sjl/src/de/cau/cs/kieler/sjl/SJExecution.java
@@ -0,0 +1,202 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.sjl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import de.cau.cs.kieler.sim.kiem.util.AbstractExecution;
+import de.cau.cs.kieler.sim.kiem.util.KiemUtil;
+
+import org.eclipse.jdt.core.compiler.batch.BatchCompiler;
+
+/**
+ * This class is intended to compile and execute SJ code.
+ * 
+ * @author cmot
+ * @kieler.design 2012-10-08 proposed cmot
+ * @kieler.rating 2012-10-08 proposed yellow cmot
+ */
+public class SJExecution extends AbstractExecution {
+
+    /** The Constant COMPILER_DEFAULT. */
+    private static final String COMPILER_DEFAULT = "java";
+
+    /** The Constant SJL_PATH to the SJLProgram.class for compilation. */
+    private static final String SJL_PATH = "src/de/cau/cs/kieler/sjl";
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * Instantiates a new sC execution.
+     * 
+     * @param outputPath
+     *            the output path
+     * @param cycleCountOrTime
+     *            the cycle count or time
+     */
+    public SJExecution(final String outputPath, final boolean cycleCountOrTime) {
+        super(outputPath, cycleCountOrTime);
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String setCompilerDefault() {
+        return COMPILER_DEFAULT;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String buildCompileCommandLine(final List<String> filePaths) {
+        // No command line necessary
+        return null;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    public String generateExecutableName(final List<String> filePaths, final String modelName) {
+        // No executable name necessary
+        return null;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String filesPreProcessing(final String filePath) {
+        // No pre processing
+        return filePath;
+    }
+
+    // -------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String buildExecutionCommandLine() {
+        // No command line necessary
+        return null;
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void compile(final List<String> filePaths, final String modelName) throws IOException,
+            InterruptedException {
+
+        // building path to bundle
+        Bundle bundle = Platform.getBundle(SJPlugin.PLUGIN_ID);
+
+        URL url = null;
+        try {
+            url = FileLocator.toFileURL(FileLocator.find(bundle, new Path(SJL_PATH), null));
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+        // if (url == null) {
+        // return;
+        // }
+        String bundleLocation = url.getFile();
+
+        // Windows vs. Linux: Exchange possibly wrong slash/backslash
+        bundleLocation = bundleLocation.replaceAll("[/\\\\]+", "\\" + File.separator);
+        if (bundleLocation.startsWith("\\")) {
+            bundleLocation = bundleLocation.substring(1);
+        }
+        for (String filePath : filePaths) {
+            filePath = filePath.replaceAll("[/\\\\]+", "\\" + File.separator);
+            if (filePath.startsWith("\\")) {
+                filePath = filePath.substring(1);
+            }
+        }
+
+//        for (String filePath : filePaths) {
+//            BatchCompiler.compile(
+//                    "-classpath " + bundleLocation + " " + filePath,
+//                    new PrintWriter(System.out),
+//                    new PrintWriter(System.err),
+//                    null);
+//        }
+
+        
+        BatchCompiler.compile(
+                "-classpath rt.jar -classpath " + bundleLocation + " -classpath d:/test/ d:/test/advanced.java",
+                new PrintWriter(System.out),
+                new PrintWriter(System.err),
+                null);
+        
+        
+//        String compile = this.getCompiler();
+//
+//        StringBuffer compileBuf = new StringBuffer();
+//        for (String filePath : filePaths) {
+//            compileBuf.append(" " + filePath);
+//        }
+//
+//        compile += compileBuf.toString();
+//
+//        compile += " -jar -classpath " + bundleLocation + "SJLProgram.class " + bundleLocation
+//                + "cJSON.c " + " -I " + bundleLocation + " " + "-o " + this.getOutputPath()
+//                + getExecutableName()
+//                // -m32 = 32 bit compatibility mode to prevent compiler errors on
+//                // 64bit machines/architectures.
+//                // + " -lm -D_SC_NOTRACE  -D_SC_USE_PRE -D_SC_NOASSEMBLER";
+//                + " -lm  -D_SC_USE_PRE -D_SC_NOASSEMBLER";
+//
+//        super.
+        return;
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void startExecution() throws IOException {
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void stopExecution(final boolean tryToDelete) {
+        // reset successful started flag
+        setStarted(false);
+
+    }
+
+    // -------------------------------------------------------------------------
+}
---
+++ plugins/de.cau.cs.kieler.sjl/src/de/cau/cs/kieler/sjl/SJLProgram.java
@@ -0,0 +1,255 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2013 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.sjl;
+
+import java.util.ArrayList;
+
+/**
+ * SJLProgram.
+ * 
+ * @author cmot
+ * 
+ */
+abstract class SJLProgram<State extends Enum<?>> {
+    
+    // A flag indicating debug output should be printed
+    static final boolean DEBUG = false; 
+    
+    // Active means NOT-paused for a tick
+    private PriorityQueue<Thread> activeThreads;
+    
+    // Alive means NOT-aborted and NOT-termed 
+    private PriorityQueue<Thread> aliveThreads;
+    
+    // The current thread that is executed
+    Thread currentThread;
+
+    // -------------------------------------------------------------------------
+
+    public void debug(String action, Thread thread) {
+        debug(action, thread, null);
+    }
+    
+    public void debug(String action, Thread thread, State forkedOrResumedState) {
+        if (DEBUG) {
+            if (forkedOrResumedState == null) {
+                System.out.println(action + " " + thread.state + " (" +aliveThreads.getPrio(thread) + ")" );
+            }
+            else {
+                System.out.println(action + " " + thread.state + " (" +aliveThreads.getPrio(thread) + ")" + " ->" + forkedOrResumedState);
+            }
+        }
+    }
+    
+    // -------------------------------------------------------------------------
+    
+    public SJLProgram(State startState, int startPrio, int maxPrio) {
+        // Create new active and alive priority queues
+        aliveThreads  = new PriorityQueue<Thread>(maxPrio);
+        // Create the main thread
+        Thread startThread = new Thread(startState, null);
+        // Add the main thread as an alive thread
+        aliveThreads.insert(startPrio, startThread);
+        // Set the start thread as the current thread
+        currentThread = startThread;
+        // Note that the active threads will be cloned from the alive threads
+        // in the doTick() method
+    }
+
+    // -------------------------------------------------------------------------
+    
+    public boolean doTick() {
+        // Clone the alive threads means making all alive threads active
+        activeThreads = aliveThreads.clone();
+        // Run the tick() method
+        tick();
+        // Return whether the program terminated 
+        return isTerminated();
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void tick() {
+        // The default implementation does nothing
+    }
+
+    // -------------------------------------------------------------------------
+
+    public boolean isTickDone() {
+        // Return whether there are no more active threads 
+        if (activeThreads.size == 0) {
+            if (DEBUG) {
+                System.out.println("\n");
+                for (Object object: aliveThreads.elements) {
+                    if (object != null) {
+                        Thread thread = (Thread) object;
+                        debug("Children of", thread);
+                        for (Thread child: thread.childs) {
+                            debug("   -", child);
+                        }
+                    }
+                }
+                System.out.println("\n");
+            }
+        }
+        return (activeThreads.size == 0);
+    }
+
+    // -------------------------------------------------------------------------
+
+    public boolean isTerminated() {
+        // Return whether there are no more active threads 
+        return (aliveThreads.size == 0);
+    }
+
+    // -------------------------------------------------------------------------
+
+    public State state() {
+        // Update the current thread
+        currentThread = activeThreads.getFirst();
+        // Find the new currentState
+        debug("Updating state", currentThread);
+        return currentThread.state;
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void fork(State forkedState, int prio) {
+         debug("Forking", currentThread, forkedState);
+         // Create a new thread
+         Thread forkedThread = new Thread(forkedState, currentThread);
+         // Add child-dependency to current one
+         currentThread.childs.add(forkedThread);
+         // Add new thread as an alive & active one
+         aliveThreads.insert(prio, forkedThread);
+         activeThreads.insert(prio, forkedThread);
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void termB() {
+        debug("Terminating", currentThread);
+        // Set the termed flag for the current thread
+        currentThread.termed = true;
+        // Remove the thread from the active and the alive ones
+        activeThreads.remove(currentThread);
+        aliveThreads.remove(currentThread);
+        // Remove the thread from its parents childs list
+        if (currentThread.parent != null) {
+            currentThread.parent.childs.remove(currentThread);
+        }
+        // Note that at this point there may be descendant child threads
+        // that are still active and alive!
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void abort() {
+        debug("Aborting children of", currentThread);
+        // Abort all descendants (all children and children of children...)
+        for (Thread childThread: currentThread.childs) {
+            // Recursively abort children
+            abort(childThread);
+        }
+        // Remove children
+        currentThread.childs.clear();
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void gotoB(State resumeState) {
+        debug("Goto of", currentThread, resumeState);
+        // Update the state label
+        currentThread.state = resumeState;
+    }
+
+    // -------------------------------------------------------------------------
+
+    public void pauseB(State resumeState) {
+        // Update the state label
+        gotoB(resumeState);
+        // Remove the thread from the active ones (it is paused now)
+        activeThreads.remove(currentThread);
+        debug("Pausing", currentThread);
+    }
+
+    // -------------------------------------------------------------------------
+    
+    public void prioB(int prio) {
+        // Reorder elements
+        activeThreads.update(prio, currentThread);
+        aliveThreads.update(prio, currentThread);
+    }
+
+    // -------------------------------------------------------------------------
+
+    public boolean join() {
+        // Go thru all (direct) children and check whether they have termed
+        for (Thread child : currentThread.childs) {
+            if (!child.termed) {
+                debug("Join failed of", currentThread);
+                // If any child not termed yet, return false
+                return false;
+            }
+        }
+        debug("Join sucessfull of", currentThread);
+        // Return true if all childs termed
+        return true;
+    }
+
+    // -------------------------------------------------------------------------
+    // -- CONVENIENT METHODS
+    // -------------------------------------------------------------------------
+
+    public void transB(State stateLabel) {
+        abort();
+        gotoB(stateLabel);
+    }
+
+    // -------------------------------------------------------------------------
+    // -- INTERNAL METHODS
+    // -------------------------------------------------------------------------
+
+    private void abort(Thread thread) {
+        debug("Aborting", thread);
+        // Remove the thread from active and alive ones
+        activeThreads.remove(thread);
+        aliveThreads.remove(thread);
+        // Abort all descendants (all children and children of children...)
+        for (Thread childThread: thread.childs) {
+            // Recursively abort children
+            abort(childThread);
+        }
+        // Remove children
+        thread.childs.clear();
+    }
+    
+    // -------------------------------------------------------------------------
+
+    private class Thread {
+        public boolean termed;
+        public State state;
+        public ArrayList<Thread> childs = new ArrayList<Thread>();
+        public Thread parent;
+        
+        public Thread(State state, Thread parent) {
+            this.state = state; 
+            this.parent = parent;
+            this.termed = false;
+        }
+    }
+    
+    // -------------------------------------------------------------------------
+    
+}
