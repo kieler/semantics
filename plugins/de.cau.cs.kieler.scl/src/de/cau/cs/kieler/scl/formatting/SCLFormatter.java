9e0ae18b1522dcd2f1ceaba7b2739f31679c4108
Steven Smyth <ssm@informatik.uni-kiel.de>
2013-04-24 21:49:04 +0200

    updated scl formatter

--- plugins/de.cau.cs.kieler.scl/src/de/cau/cs/kieler/scl/formatting/SCLFormatter.java
+++ plugins/de.cau.cs.kieler.scl/src/de/cau/cs/kieler/scl/formatting/SCLFormatter.java
@@ -41,6 +41,10 @@ public class SCLFormatter extends AbstractDeclarativeFormatter {
 //            c.setNoSpace().before(f.getInstructionListAccess().getSemicolonKeyword_1_0_0_0_1());
 //            c.setNoSpace().before(f.getInstructionListAccess().getSemicolonKeyword_0_1_0_0_0_1());
 //            c.setNoSpace().before(f.getInstructionListAccess().getSemicolonKeyword_1_1_2_0_0_0_1());
+            c.setNoSpace().before(f.getProgramAccess().getSemicolonKeyword_4_0_0_0_1());
+            c.setNoSpace().before(f.getConditionalAccess().getSemicolonKeyword_3_0_0_0_1());
+            c.setNoSpace().before(f.getInstructionScopeAccess().getSemicolonKeyword_3_0_0_0_1());
+            c.setNoSpace().before(f.getThreadAccess().getSemicolonKeyword_1_0_0_0_1());
             
             c.setLinewrap(1).before(f.getInstructionRule());
             c.setLinewrap(1).before(f.getAnnotationRule());
