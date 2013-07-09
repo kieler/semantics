fd555a41e7509aedee599e8aaa3de73d9f02af93
Christian Motika <cmot@informatik.uni-kiel.de>
2013-07-09 12:25:26 +0200

    REMOVED Yakindu SyncCharts Editor

--- plugins/de.cau.cs.kieler.yakindu.synccharts.model.stext/src/de/cau/cs/kieler/yakindu/synccharts/model/stext/validation/SyncChartstextJavaValidator.java
+++
@@ -1,8 +0,0 @@
-package de.cau.cs.kieler.yakindu.synccharts.model.stext.validation;
-
-import de.cau.cs.kieler.yakindu.model.stext.validation.SynctextJavaValidator;
- 
-
-public class SyncChartstextJavaValidator extends SynctextJavaValidator {
-
-}
