f2e600485882dd36854774377d194e76ee68a458
Steven Smyth <ssm@informatik.uni-kiel.de>
2013-07-12 20:09:11 +0200

    scharts.scl: added enum for scl optimizations

---
+++ plugins/de.cau.cs.kieler.yakindu.sccharts.scl/src/de/cau/cs/kieler/yakindu/sccharts/scl/xtend/SCLOptimization.java
@@ -0,0 +1,11 @@
+package de.cau.cs.kieler.yakindu.sccharts.scl.xtend;
+
+public enum SCLOptimization {
+    NONE,
+    GOTO,
+    LABEL,
+    SELFLOOP,
+    STATEPOSITION,
+    DUPLICATEDTRANSITIONS,
+    WTOTRANSITIONS
+}
