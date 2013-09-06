353f95275eede7c0e63dedb4bb18dbb281e9d4b7
Christian Motika <cmot@informatik.uni-kiel.de>
2013-09-06 15:37:32 +0200

    scg and scgdep plugin consolidated

--- plugins/de.cau.cs.kieler.scg/src/de/cau/cs/kieler/scgdep/impl/ScgdepFactoryImpl.java
+++ plugins/de.cau.cs.kieler.scg/src/de/cau/cs/kieler/scgdep/impl/ScgdepFactoryImpl.java
@@ -38,7 +38,7 @@ public class ScgdepFactoryImpl extends EFactoryImpl implements ScgdepFactory {
      */
     public static ScgdepFactory init() {
         try {
-            ScgdepFactory theScgdepFactory = (ScgdepFactory)EPackage.Registry.INSTANCE.getEFactory(ScgdepPackage.eNS_URI);
+            ScgdepFactory theScgdepFactory = (ScgdepFactory)EPackage.Registry.INSTANCE.getEFactory("http://kieler.cs.cau.de/scgdep/0.1.0"); 
             if (theScgdepFactory != null) {
                 return theScgdepFactory;
             }
