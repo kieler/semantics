10a203c132f81e8c0758e08e7151e4fabca941bc
Christian Motika <cmot@informatik.uni-kiel.de>
2013-10-17 01:38:06 +0200

    ssdfsdf

---
+++ plugins/de.cau.cs.kieler.sccharts.s/src/de/cau/cs/kieler/sccharts/s/DependencyState.java
@@ -0,0 +1,62 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2013 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.sccharts.s;
+
+import de.cau.cs.kieler.sccharts.State;
+
+/**
+ * This class holds a dependency representation of an SCCharts State. 
+ * 
+ * @author cmot
+ * @kieler.design 2013-10-10 proposed 
+ * @kieler.rating 2013-10-10 proposed yellow
+ *
+ */
+public class DependencyState {
+    
+    private State state;
+    private boolean isJoin;
+    private int priority;
+    
+    public DependencyState() {
+    }
+    
+    public DependencyState(State state) {
+        this.state = state;
+    }
+    
+    public State getState() {
+        return state;
+    }
+    
+    public void setState(State state) {
+        this.state = state;
+    }
+    
+    public void setIsJoin(boolean isJoin) {
+        this.isJoin = isJoin;
+    }
+
+    public boolean getIsJoin() {
+       return this.isJoin;
+    }
+    
+    public int getPriority() {
+        return priority;
+    }
+    
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
