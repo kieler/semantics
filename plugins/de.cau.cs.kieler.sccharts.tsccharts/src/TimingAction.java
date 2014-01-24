85805e60f41fdb2de1c7f671852eb73a290a1b12
Insa Fuhrmann <ima@informatik.uni-kiel.de>
2014-01-24 12:05:49 +0100

    Part of the progress of switching from action to handler for timing analysis. Annotation and first transformation integrated.

--- plugins/de.cau.cs.kieler.sccharts.tsccharts/src/TimingAction.java
+++ plugins/de.cau.cs.kieler.sccharts.tsccharts/src/TimingAction.java
@@ -1,10 +1,44 @@
+/**
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ * 
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+
+import java.io.File;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.util.ResourceUtil;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
 import com.google.inject.Guice;
 
@@ -34,9 +68,66 @@ public class TimingAction implements IAction {
     public TimingAction() {
         Guice.createInjector().injectMembers(this);
     }
+    
+    private IUnitOfWork<?, XtextResource> annotateRegions() {
+        return new IUnitOfWork.Void<XtextResource>() {
+
+            @Override
+            public void process(XtextResource resource) throws Exception {
+                if (resource.getContents().isEmpty()
+                        || (!(resource.getContents().get(0) instanceof Region))) {
+                    return;
+                }
+                
+                IFile file = ResourceUtil.getFile(resource);
+                String uri = file.getLocationURI().toString();
+                
+                
+                Resource r = //resource.getResourceSet().
+                        new ResourceSetImpl().
+                   createResource(URI.createURI(/* "file:/home/ima/runtime-SemanticsM/test/robot.scc")); */ uri.replace(".sct", "a.scc")));
+                
+                // boolean test = new File(r.getURI().toFileString()).delete();
+                // System.out.println(r.getURI());
+                
+                Region rootRegion = (Region) EcoreUtil.copy(resource.getContents().get(0));
+                
+                annotationProvider.setTimingDomainsSimple(rootRegion, 0);
+                
+                r.getContents().add(rootRegion);
+                
+                r.save(Collections.emptyMap());
+                
+                file.getProject().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+            }
+        
+        };
+    }
 
     @Override
-    public ActionResult execute(ActionContext context) {
+    public ActionResult execute(final ActionContext context) {
+        
+        Job job = new Job("TiminAnalysis") {
+            
+            @Override
+            protected IStatus run(IProgressMonitor monitor) {
+                IWorkbenchPart part = context.getViewContext().getSourceWorkbenchPart();
+                IXtextDocument document = null;
+                
+                if (part instanceof XtextEditor) {
+                    document = ((XtextEditor) part).getDocument();
+                } else {
+                    return Status.CANCEL_STATUS;
+                }
+                
+                document.readOnly(annotateRegions());
+
+                return Status.OK_STATUS;
+            }
+        };
+        
+        job.schedule();
+        
         // in deinem Falle eine Region/State
         Object semanticElement =
                 context.getViewContext().getSourceElement(context.getKGraphElement());
