df0510c10931d83657775a6800c51f56a98a022d
Steven Smyth <ssm@informatik.uni-kiel.de>
2014-09-12 22:40:27 +0200

    annotations/kexpressions: rebuild model code

--- plugins/de.cau.cs.kieler.core.annotations/src/de/cau/cs/kieler/core/annotations/StringAnnotation.java
+++ plugins/de.cau.cs.kieler.core.annotations/src/de/cau/cs/kieler/core/annotations/StringAnnotation.java
@@ -36,29 +36,29 @@ package de.cau.cs.kieler.core.annotations;
  */
 public interface StringAnnotation extends Annotation {
     /**
-     * Returns the value of the '<em><b>Value</b></em>' attribute.
-     * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Value</b></em>' attribute.
+	 * <!-- begin-user-doc -->
      * <p>
      * If the meaning of the '<em>Value</em>' attribute isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @return the value of the '<em>Value</em>' attribute.
-     * @see #setValue(String)
-     * @see de.cau.cs.kieler.core.annotations.AnnotationsPackage#getStringAnnotation_Value()
-     * @model
-     * @generated
-     */
+	 * @return the value of the '<em>Value</em>' attribute.
+	 * @see #setValue(String)
+	 * @see de.cau.cs.kieler.core.annotations.AnnotationsPackage#getStringAnnotation_Value()
+	 * @model
+	 * @generated
+	 */
     String getValue();
 
     /**
-     * Sets the value of the '{@link de.cau.cs.kieler.core.annotations.StringAnnotation#getValue <em>Value</em>}' attribute.
-     * <!-- begin-user-doc -->
+	 * Sets the value of the '{@link de.cau.cs.kieler.core.annotations.StringAnnotation#getValue <em>Value</em>}' attribute.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param value the new value of the '<em>Value</em>' attribute.
-     * @see #getValue()
-     * @generated
-     */
+	 * @param value the new value of the '<em>Value</em>' attribute.
+	 * @see #getValue()
+	 * @generated
+	 */
     void setValue(String value);
 
 } // StringAnnotation
--- plugins/de.cau.cs.kieler.core.kexpressions/src/de/cau/cs/kieler/core/kexpressions/impl/FunctionCallImpl.java
+++ plugins/de.cau.cs.kieler.core.kexpressions/src/de/cau/cs/kieler/core/kexpressions/impl/FunctionCallImpl.java
@@ -37,185 +37,185 @@ import org.eclipse.emf.ecore.util.InternalEList;
  */
 public class FunctionCallImpl extends ExpressionImpl implements FunctionCall {
     /**
-     * The default value of the '{@link #getFunctionName() <em>Function Name</em>}' attribute.
-     * <!-- begin-user-doc -->
+	 * The default value of the '{@link #getFunctionName() <em>Function Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getFunctionName()
-     * @generated
-     * @ordered
-     */
+	 * @see #getFunctionName()
+	 * @generated
+	 * @ordered
+	 */
     protected static final String FUNCTION_NAME_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getFunctionName() <em>Function Name</em>}' attribute.
-     * <!-- begin-user-doc -->
+	 * The cached value of the '{@link #getFunctionName() <em>Function Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getFunctionName()
-     * @generated
-     * @ordered
-     */
+	 * @see #getFunctionName()
+	 * @generated
+	 * @ordered
+	 */
     protected String functionName = FUNCTION_NAME_EDEFAULT;
 
     /**
-     * The cached value of the '{@link #getParameters() <em>Parameters</em>}' containment reference list.
-     * <!-- begin-user-doc -->
+	 * The cached value of the '{@link #getParameters() <em>Parameters</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @see #getParameters()
-     * @generated
-     * @ordered
-     */
+	 * @see #getParameters()
+	 * @generated
+	 * @ordered
+	 */
     protected EList<Parameter> parameters;
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     protected FunctionCallImpl() {
-        super();
-    }
+		super();
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @Override
     protected EClass eStaticClass() {
-        return KExpressionsPackage.Literals.FUNCTION_CALL;
-    }
+		return KExpressionsPackage.Literals.FUNCTION_CALL;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public String getFunctionName() {
-        return functionName;
-    }
+		return functionName;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public void setFunctionName(String newFunctionName) {
-        String oldFunctionName = functionName;
-        functionName = newFunctionName;
-        if (eNotificationRequired())
-            eNotify(new ENotificationImpl(this, Notification.SET, KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME, oldFunctionName, functionName));
-    }
+		String oldFunctionName = functionName;
+		functionName = newFunctionName;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME, oldFunctionName, functionName));
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     public EList<Parameter> getParameters() {
-        if (parameters == null) {
-            parameters = new EObjectContainmentEList<Parameter>(Parameter.class, this, KExpressionsPackage.FUNCTION_CALL__PARAMETERS);
-        }
-        return parameters;
-    }
+		if (parameters == null) {
+			parameters = new EObjectContainmentEList<Parameter>(Parameter.class, this, KExpressionsPackage.FUNCTION_CALL__PARAMETERS);
+		}
+		return parameters;
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @Override
     public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
-        switch (featureID) {
-            case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
-                return ((InternalEList<?>)getParameters()).basicRemove(otherEnd, msgs);
-        }
-        return super.eInverseRemove(otherEnd, featureID, msgs);
-    }
+		switch (featureID) {
+			case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
+				return ((InternalEList<?>)getParameters()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @Override
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
-        switch (featureID) {
-            case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
-                return getFunctionName();
-            case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
-                return getParameters();
-        }
-        return super.eGet(featureID, resolve, coreType);
-    }
+		switch (featureID) {
+			case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
+				return getFunctionName();
+			case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
+				return getParameters();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @SuppressWarnings("unchecked")
     @Override
     public void eSet(int featureID, Object newValue) {
-        switch (featureID) {
-            case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
-                setFunctionName((String)newValue);
-                return;
-            case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
-                getParameters().clear();
-                getParameters().addAll((Collection<? extends Parameter>)newValue);
-                return;
-        }
-        super.eSet(featureID, newValue);
-    }
+		switch (featureID) {
+			case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
+				setFunctionName((String)newValue);
+				return;
+			case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
+				getParameters().clear();
+				getParameters().addAll((Collection<? extends Parameter>)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @Override
     public void eUnset(int featureID) {
-        switch (featureID) {
-            case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
-                setFunctionName(FUNCTION_NAME_EDEFAULT);
-                return;
-            case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
-                getParameters().clear();
-                return;
-        }
-        super.eUnset(featureID);
-    }
+		switch (featureID) {
+			case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
+				setFunctionName(FUNCTION_NAME_EDEFAULT);
+				return;
+			case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
+				getParameters().clear();
+				return;
+		}
+		super.eUnset(featureID);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @Override
     public boolean eIsSet(int featureID) {
-        switch (featureID) {
-            case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
-                return FUNCTION_NAME_EDEFAULT == null ? functionName != null : !FUNCTION_NAME_EDEFAULT.equals(functionName);
-            case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
-                return parameters != null && !parameters.isEmpty();
-        }
-        return super.eIsSet(featureID);
-    }
+		switch (featureID) {
+			case KExpressionsPackage.FUNCTION_CALL__FUNCTION_NAME:
+				return FUNCTION_NAME_EDEFAULT == null ? functionName != null : !FUNCTION_NAME_EDEFAULT.equals(functionName);
+			case KExpressionsPackage.FUNCTION_CALL__PARAMETERS:
+				return parameters != null && !parameters.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
 
     /**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
     @Override
     public String toString() {
-        if (eIsProxy()) return super.toString();
+		if (eIsProxy()) return super.toString();
 
-        StringBuffer result = new StringBuffer(super.toString());
-        result.append(" (functionName: ");
-        result.append(functionName);
-        result.append(')');
-        return result.toString();
-    }
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (functionName: ");
+		result.append(functionName);
+		result.append(')');
+		return result.toString();
+	}
 
 } //FunctionCallImpl
