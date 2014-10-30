fab9fe7168669ce971f7962c76e2ba5c5268febe
Karsten Rathlev <krat@informatik.uni-kiel.de>
2014-10-30 14:00:28 +0100

    ...

--- plugins/de.cau.cs.kieler.scl.ui/src/de/cau/cs/kieler/scl/ui/SclHighlightingCalculator.java
+++ plugins/de.cau.cs.kieler.scl.ui/src/de/cau/cs/kieler/scl/ui/SclHighlightingCalculator.java
@@ -1,16 +1,12 @@
 package de.cau.cs.kieler.scl.ui;
 
-import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.EnumLiteralDeclaration;
 import org.eclipse.xtext.Keyword;
-import org.eclipse.xtext.RuleCall;
-import org.eclipse.xtext.TerminalRule;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
-import de.cau.cs.kieler.scl.scl.Assignment;
 import de.cau.cs.kieler.scl.scl.EmptyStatement;
 
 public class SclHighlightingCalculator implements ISemanticHighlightingCalculator {
@@ -36,7 +32,6 @@ public class SclHighlightingCalculator implements ISemanticHighlightingCalculato
             // highlighting for keywords
             else if (node.getGrammarElement() instanceof Keyword &&
                     !(IGNOREKEYWORD.contains(((Keyword) node.getGrammarElement()).getValue()))) {
-                    System.out.println("Keyword: " + (Keyword)node.getGrammarElement());
                     acceptor.addPosition(node.getOffset(), node.getLength(),
                             SclHighlightingConfiguration.KEYWORD);
                 }
