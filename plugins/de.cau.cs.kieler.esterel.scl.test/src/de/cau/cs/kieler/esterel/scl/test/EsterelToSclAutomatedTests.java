75eeaeda42a985710b1d20513daa5f11bcc43cfa
Karsten Rathlev <krat@informatik.uni-kiel.de>
2014-11-09 15:36:44 +0100

    regression tests started

---
+++ plugins/de.cau.cs.kieler.esterel.scl.test/src/de/cau/cs/kieler/esterel/scl/test/EsterelToSclAutomatedTests.java
@@ -0,0 +1,80 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ *
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ *
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.esterel.scl.test;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+
+import de.cau.cs.kieler.sim.kiem.test.KiemAutomatedJUnitTest;
+
+/**
+ * The test plugin for regression testing the Esterel to SCL transformation.
+ *
+ * @author krat
+ * 
+ */
+public class EsterelToSclAutomatedTests extends KiemAutomatedJUnitTest {
+
+    /**
+     * Instantiates a new simulation test.
+     *
+     * @param esoFile the ESO file
+     */
+    public EsterelToSclAutomatedTests(final IPath esoFile) {
+        super(esoFile);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected String getPluginId() {
+        return "de.cau.cs.kieler.esterel.scl.test";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected IPath getBundleTestPath() {
+        return new Path("testdata");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected String getModelFileExtension() {
+        return "scg";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected String getTemporaryWorkspaceFolderName() {
+        return "test-esterelToScg";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected String getExecutionFileName() {
+        return "sccharts_c_validation_headless.execution";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected IPath getExternalRelativeTestPath() {
+        return null;
+    }
+}
