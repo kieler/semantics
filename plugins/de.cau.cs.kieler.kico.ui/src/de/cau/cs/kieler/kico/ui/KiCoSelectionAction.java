98efbf2407c7e8d265b2b6e187a039b0e7e236b6
Christian Motika <cmot@informatik.uni-kiel.de>
2014-12-18 14:39:58 +0100

    kico.ui extension point: priorized alternatives

--- plugins/de.cau.cs.kieler.kico.ui/src/de/cau/cs/kieler/kico/ui/KiCoSelectionAction.java
+++ plugins/de.cau.cs.kieler.kico.ui/src/de/cau/cs/kieler/kico/ui/KiCoSelectionAction.java
@@ -97,7 +97,7 @@ public class KiCoSelectionAction extends KiCoKlighdAction implements IAction {
 
             KiCoSelectionView.addRequiredTransformationVisualization(activeEditorID);
 
-            System.out.println(Arrays.toString(KiCoSelectionView.getSelectedTransformations(
+            System.out.println(Arrays.toString(KiCoSelectionView.getSelectedAndDisabledTransformations(
                     KiCoSelectionView.getActiveEditorID()).toArray()));
 
             // notify listeners about currently active transformations
