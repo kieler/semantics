7425d5ab3a1de687ea5d2ac82c5892f3d9fff0e9
Karsten Rathlev <krat@informatik.uni-kiel.de>
2015-01-31 14:24:58 +0100

    plugin.xml updated with respect to conventions

--- plugins/de.cau.cs.kieler.esterel.scl/src/de/cau/cs/kieler/esterel/scl/EsterelToSclPlugin.java
+++ plugins/de.cau.cs.kieler.esterel.scl/src/de/cau/cs/kieler/esterel/scl/EsterelToSclPlugin.java
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class EsterelToSclPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "de.cau.cs.kieler.esterel.scl"; //$NON-NLS-1$
 
 	// The shared instance
-	private static Activator plugin;
+	private static EsterelToSclPlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public EsterelToSclPlugin() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static EsterelToSclPlugin getDefault() {
 		return plugin;
 	}
 
