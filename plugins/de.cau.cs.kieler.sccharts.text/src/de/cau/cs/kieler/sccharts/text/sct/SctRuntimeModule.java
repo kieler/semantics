7e780f2a727762dc170e3a98fafbf12688b49e44
Insa Fuhrmann <ima@informatik.uni-kiel.de>
2015-02-04 12:06:40 +0100

    Preliminary fix of eopposite vs. Xtext bug.

--- plugins/de.cau.cs.kieler.sccharts.text/src/de/cau/cs/kieler/sccharts/text/sct/SctRuntimeModule.java
+++ plugins/de.cau.cs.kieler.sccharts.text/src/de/cau/cs/kieler/sccharts/text/sct/SctRuntimeModule.java
@@ -18,6 +18,10 @@ package de.cau.cs.kieler.sccharts.text.sct;
 import de.cau.cs.kieler.sccharts.text.sct.scoping.SctQualifiedNameProvider;
 import de.cau.cs.kieler.sccharts.text.sct.scoping.SctScopeProvider;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.linking.ILinker;
+import org.eclipse.xtext.linking.impl.Linker;
+
 /**
  * @author chsch
  *
@@ -65,4 +69,47 @@ public class SctRuntimeModule extends
         return de.cau.cs.kieler.sccharts.text.sct.formatting.SctIndentionInformation.class;
     }
     
+//    /**
+//     * Method registers the non-lazy linking Linker since the default
+//     * {@link org.eclipse.xtext.linking.lazy.LazyLinker} doesn't work properly with EOpposite
+//     * references. (Produces error markers in editor.)
+//     * 
+//     * @return the {@link Linker} class
+//     */
+//    @Override
+//    public Class<? extends ILinker> bindILinker() {
+//        return SctLinker.class;
+//    }
+//
+//    /**
+//     * FIXME
+//     * Temporary fix for an issue where Transition#targetState is set to null again
+//     * _after_ it was successfully linked. 
+//     */
+//    private static class SctLinker extends Linker {
+//        
+//        protected boolean isClearAllReferencesRequired(Resource resource) {
+//                return false;
+//        }
+//        
+////      protected void ensureModelLinked(EObject model, final IDiagnosticProducer producer) {
+////              boolean clearAllReferencesRequired = isClearAllReferencesRequired(model.eResource());
+////              TreeIterator<EObject> iterator = getAllLinkableContents(model);
+////              
+////              // first clear all (possibly invalid) references 
+////              while(iterator.hasNext()) {
+////                      EObject next = iterator.next();
+////                      if (clearAllReferencesRequired) {
+////                              clearReferences(next);
+////                      }
+////              }
+////              
+////              // re-link
+////              iterator = getAllLinkableContents(model);
+////              while(iterator.hasNext()) {
+////                      EObject next = iterator.next();
+////                      ensureLinked(next, producer);
+////              }
+////      }
+//    }  
 }
