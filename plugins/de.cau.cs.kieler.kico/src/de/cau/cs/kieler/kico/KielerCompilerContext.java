ae3346b8001c48737365f773989a98ccc4f6fa5e
Christian Motika <cmot@informatik.uni-kiel.de>
2015-03-18 14:17:18 +0100

    Code clean up

--- plugins/de.cau.cs.kieler.kico/src/de/cau/cs/kieler/kico/KielerCompilerContext.java
+++ plugins/de.cau.cs.kieler.kico/src/de/cau/cs/kieler/kico/KielerCompilerContext.java
@@ -21,7 +21,6 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.xtext.EcoreUtil2;
 
 /**
  * This class implements the context of a KielerCompiler call.
--- plugins/de.cau.cs.kieler.kico/src/de/cau/cs/kieler/kico/TransformationDummyGraph.java
+++ plugins/de.cau.cs.kieler.kico/src/de/cau/cs/kieler/kico/TransformationDummyGraph.java
@@ -18,9 +18,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
-
-import com.google.inject.Inject;
 
 /**
  * This class represents a transformation dummy graph for fast processing transformation
