7d2d57149de1e85702b22249f70d5181ab493c61
Christian Motika <cmot@informatik.uni-kiel.de>
2015-07-01 12:55:35 +0200

    renaming plugin

--- plugins/de.cau.cs.kieler.esterel.sim.kivi/src/de/cau/cs/kieler/esterel/sim/kivi/EsterelCECSimKiviPlugin.java
+++ plugins/de.cau.cs.kieler.esterel.sim.kivi/src/de/cau/cs/kieler/esterel/sim/kivi/EsterelCECSimKiviPlugin.java
@@ -2,41 +2,41 @@
  * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
  *
  * http://www.informatik.uni-kiel.de/rtsys/kieler/
- * 
+ *
  * Copyright 2012 by
  * + Christian-Albrechts-University of Kiel
  *   + Department of Computer Science
  *     + Real-Time and Embedded Systems Group
- * 
+ *
  * This code is provided under the terms of the Eclipse Public License (EPL).
  * See the file epl-v10.html for the license text.
  */
-package de.cau.cs.kieler.esterel.cec.sim.kivi;
+package de.cau.cs.kieler.esterel.sim.kivi;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle.
- * 
+ *
  * @author cmot
  * @kieler.design 2012-10-08 proposed cmot
  * @kieler.rating 2012-10-08 proposed yellow
  */
-public class EsterelCECSimKiviPlugin extends AbstractUIPlugin {
+public class EsterelSimKiviPlugin extends AbstractUIPlugin {
 
     /** The Constant PLUGIN_ID. */
     public static final String PLUGIN_ID = "de.cau.cs.kieler.esterel.cec.sim.kivi"; //$NON-NLS-1$
 
     /** The plugin. */
-    private static EsterelCECSimKiviPlugin plugin;
+    private static EsterelSimKiviPlugin plugin;
 
     // -----------------------------------------------------------------------------
 
     /**
      * The constructor.
      */
-    public EsterelCECSimKiviPlugin() {
+    public EsterelSimKiviPlugin() {
     }
 
     // -----------------------------------------------------------------------------
@@ -63,10 +63,10 @@ public class EsterelCECSimKiviPlugin extends AbstractUIPlugin {
 
     /**
      * Returns the shared instance.
-     * 
+     *
      * @return the shared instance
      */
-    public static EsterelCECSimKiviPlugin getDefault() {
+    public static EsterelSimKiviPlugin getDefault() {
         return plugin;
     }
 
--- plugins/de.cau.cs.kieler.esterel.sim.kivi/src/de/cau/cs/kieler/esterel/sim/kivi/EsterelDataComponent.java
+++ plugins/de.cau.cs.kieler.esterel.sim.kivi/src/de/cau/cs/kieler/esterel/sim/kivi/EsterelDataComponent.java
@@ -2,25 +2,25 @@
  * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
  *
  * http://www.informatik.uni-kiel.de/rtsys/kieler/
- * 
+ *
  * Copyright 2012 by
  * + Christian-Albrechts-University of Kiel
  *   + Department of Computer Science
  *     + Real-Time and Embedded Systems Group
- * 
+ *
  * This code is provided under the terms of the Eclipse Public License (EPL).
  * See the file epl-v10.html for the license text.
  */
-package de.cau.cs.kieler.esterel.cec.sim.kivi;
+package de.cau.cs.kieler.esterel.sim.kivi;
 
 import org.eclipse.emf.ecore.EObject;
 
-import de.cau.cs.kieler.esterel.cec.sim.EsterelCECSimPlugin;
+import de.cau.cs.kieler.esterel.sim.EsterelSimPlugin;
 import de.cau.cs.kieler.sim.kiem.ui.datacomponent.kivi.StateErrorXtextVisualizationDataComponent;
 
 /**
  * The DataComponent for visualizing Esterel simulation.
- * 
+ *
  * @author cmot
  * @kieler.design 2012-10-08 proposed cmot
  * @kieler.rating 2012-10-08 proposed yellow
@@ -48,7 +48,7 @@ public class EsterelDataComponent extends StateErrorXtextVisualizationDataCompon
     }
 
     // -----------------------------------------------------------------------------
-    
+
     /**
      * {@inheritDoc}
      */
@@ -59,7 +59,7 @@ public class EsterelDataComponent extends StateErrorXtextVisualizationDataCompon
             return uri;
         }
         return null;
-    }    
+    }
 
     // -----------------------------------------------------------------------------
 }
