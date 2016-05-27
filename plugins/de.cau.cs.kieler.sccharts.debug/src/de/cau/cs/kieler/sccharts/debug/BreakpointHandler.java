c37686e00dc446f6a3fb1adcd6448ec642b1cc4c
Lena Grimm <stu115342@informatik.uni-kiel.de>
2016-05-27 14:23:25 +0200

    additions to breakpoint visualization

---
+++ plugins/de.cau.cs.kieler.sccharts.debug/src/de/cau/cs/kieler/sccharts/debug/BreakpointHandler.java
@@ -0,0 +1,37 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://rtsys.informatik.uni-kiel.de/kieler
+ * 
+ * Copyright ${year} by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ */
+package de.cau.cs.kieler.sccharts.debug;
+
+import java.util.Map;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * @author lgr
+ *
+ */
+public class BreakpointHandler extends AbstractHandler {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+}
