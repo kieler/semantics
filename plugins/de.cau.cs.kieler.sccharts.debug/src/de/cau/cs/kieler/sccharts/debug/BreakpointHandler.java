b25a5abe75641f930d723980b33e7a80c1e0409b
Lena Grimm <stu115342@informatik.uni-kiel.de>
2016-05-27 16:05:01 +0200

    fixed breakpoint visualization (still correct icon missing)

--- plugins/de.cau.cs.kieler.sccharts.debug/src/de/cau/cs/kieler/sccharts/debug/BreakpointHandler.java
+++ plugins/de.cau.cs.kieler.sccharts.debug/src/de/cau/cs/kieler/sccharts/debug/BreakpointHandler.java
@@ -12,26 +12,26 @@
  */
 package de.cau.cs.kieler.sccharts.debug;
 
-import java.util.Map;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * @author lgr
  *
  */
 public class BreakpointHandler extends AbstractHandler {
-
+    
+    
     /**
      * {@inheritDoc}
      */
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        // TODO Auto-generated method stub
         return null;
     }
-
-
 }
