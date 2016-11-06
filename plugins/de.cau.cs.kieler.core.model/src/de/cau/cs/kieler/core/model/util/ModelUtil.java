6253193e4ff43d4eefff21a6c70aab17cf2c4044
Andreas Stange <aas@informatik.uni-kiel.de>
2016-11-06 19:49:33 +0100

    KiemJUnitTest: fixed createLinkedWorkspaceFile

--- plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/util/ModelUtil.java
+++ plugins/de.cau.cs.kieler.core.model/src/de/cau/cs/kieler/core/model/util/ModelUtil.java
@@ -482,11 +482,7 @@ public final class ModelUtil {
         IPath absoluteBundlePath = new Path(absoluteBundlePathString);
         
         // Get workspace relative path from absolute path
-        String bundleLocation = bundle.getLocation();
-        String bundleName = bundle.getSymbolicName();//new Path(bundleLocation).removeTrailingSeparator().lastSegment();
-        System.out.println("Bundle Name: "+bundleName);
-        System.out.println("bundle location: "+bundleLocation);
-        System.out.println("absoluteBundlePathString: "+absoluteBundlePathString);
+        String bundleName = bundle.getSymbolicName();
         int afterBundleNameIndex = absoluteBundlePathString.indexOf(bundleName)+bundleName.length()+1;
         String relativeBundlePathString = absoluteBundlePathString.substring(afterBundleNameIndex);
         IPath relativeBundlePath = new Path(relativeBundlePathString);
