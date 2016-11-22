9015cd25f217d0deb0a4b2eca250a374bfbc75a7
Christian Motika <cmot@informatik.uni-kiel.de>
2016-11-22 02:14:04 +0100

    added icons dir

--- plugins/de.cau.cs.kieler.sccharts.sim.java/src/de/cau/cs/kieler/sccharts/sim/java/JavaExecution.java
+++ plugins/de.cau.cs.kieler.sccharts.sim.java/src/de/cau/cs/kieler/sccharts/sim/java/JavaExecution.java
@@ -46,7 +46,7 @@ public class JavaExecution extends AbstractExecution {
 
     /** The Constant SJL_PATH to the SJLProgram.class for compilation. */
     private static final String SJL_PATH_BIN = "bin";
-//    private static final String SJL_PATH_BIN = ".";
+    // private static final String SJL_PATH_BIN = ".";
 
     /** The model name used when compile() was called. */
     private String modelName;
@@ -142,55 +142,57 @@ public class JavaExecution extends AbstractExecution {
         Bundle bundle = Platform.getBundle(SCChartsSimJavaPlugin.PLUGIN_ID);
 
         String bundleLocation = ".";
-        
+
         try {
-        String relativeFilePath = "/bin";
-        //final Bundle bundle = Platform.getBundle(pluginID);
-        URL bundleFileUrl = bundle.getEntry(relativeFilePath);
-        URL absoluteBundleUrl = KiemUtil.getAbsoluteBundlePath(bundleFileUrl);
-        bundleLocation = KiemUtil.getAbsoluteFilePath(absoluteBundleUrl);
-        } catch (Exception e) {
-            e.printStackTrace();
-            // second try
-            String relativeFilePath = "/classes";
-            //final Bundle bundle = Platform.getBundle(pluginID);
+            String relativeFilePath = "/bin";
+            // final Bundle bundle = Platform.getBundle(pluginID);
             URL bundleFileUrl = bundle.getEntry(relativeFilePath);
             URL absoluteBundleUrl = KiemUtil.getAbsoluteBundlePath(bundleFileUrl);
             bundleLocation = KiemUtil.getAbsoluteFilePath(absoluteBundleUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                // second try
+                String relativeFilePath = "/classes";
+                // final Bundle bundle = Platform.getBundle(pluginID);
+                URL bundleFileUrl = bundle.getEntry(relativeFilePath);
+                URL absoluteBundleUrl = KiemUtil.getAbsoluteBundlePath(bundleFileUrl);
+                bundleLocation = KiemUtil.getAbsoluteFilePath(absoluteBundleUrl);
+            } catch (Exception ee) {
+                ee.printStackTrace();
+            }
         }
-        
-        
-        
-//        java.net.URL path = null;
-//        if (bundle != null) {
-//                File bundleFile = null;
-//                try {
-//                    bundleFile = FileLocator.getBundleFile(bundle);
-//                    //FileLocator.toFileURL()
-//                    //URL binFolderURL = FileLocator.resolve(bundle, new Path("bin"), null);
-//                    
-//                    
-//                    if (binFolderURL != null) {
-//                        path = FileLocator.toFileURL(binFolderURL);
-//                    } else {
-//                        path = new Path(bundleFile.getAbsolutePath());
-//                    }
-//                    if (!path.isAbsolute()) {
-//                        path = path.makeAbsolute();
-//                    }
-//                } catch (IOException e) {
-//                }
-//        }
-        
-       System.out.println("bundleLocationB:" + bundleLocation);
 
-//        URL url = null;
-//        try {
-//            url = FileLocator.toFileURL(FileLocator.find(bundle, new Path(SJL_PATH_BIN), null));
-//        } catch (Exception e2) {
-//            e2.printStackTrace();
-//        }
-//        String bundleLocation = url.getFile();
+        // java.net.URL path = null;
+        // if (bundle != null) {
+        // File bundleFile = null;
+        // try {
+        // bundleFile = FileLocator.getBundleFile(bundle);
+        // //FileLocator.toFileURL()
+        // //URL binFolderURL = FileLocator.resolve(bundle, new Path("bin"), null);
+        //
+        //
+        // if (binFolderURL != null) {
+        // path = FileLocator.toFileURL(binFolderURL);
+        // } else {
+        // path = new Path(bundleFile.getAbsolutePath());
+        // }
+        // if (!path.isAbsolute()) {
+        // path = path.makeAbsolute();
+        // }
+        // } catch (IOException e) {
+        // }
+        // }
+
+        System.out.println("bundleLocationB:" + bundleLocation);
+
+        // URL url = null;
+        // try {
+        // url = FileLocator.toFileURL(FileLocator.find(bundle, new Path(SJL_PATH_BIN), null));
+        // } catch (Exception e2) {
+        // e2.printStackTrace();
+        // }
+        // String bundleLocation = url.getFile();
 
         // Windows vs. Linux: Exchange possibly wrong slash/backslash
         bundleLocation = bundleLocation.replaceAll("[/\\\\]+", "\\" + File.separator);
@@ -218,16 +220,14 @@ public class JavaExecution extends AbstractExecution {
             if (filePath.lastIndexOf("\\") > 0) {
                 filePathRoot = filePath.substring(0, filePath.lastIndexOf("\\"));
             }
-            
-            
-            
-            
+
             ByteArrayOutputStream os = new ByteArrayOutputStream();
-            
-            BatchCompiler.compile("-verbose -classpath " + bundleLocation
-                    + " -source 1.5 -target 1.5 -classpath " + filePathRoot + " " + filePath,
+
+            BatchCompiler.compile(
+                    "-verbose -classpath " + bundleLocation + " -source 1.5 -target 1.5 -classpath "
+                            + filePathRoot + " " + filePath,
                     new PrintWriter(System.out), new PrintWriter(os), null);
-            
+
             // Test for error message
             String errorMessage = os.toString();
             boolean foundError = errorMessage.contains("ERROR in");
@@ -237,8 +237,8 @@ public class JavaExecution extends AbstractExecution {
         }
 
         // Instantiate the dynamic class loader
-        DynamicClassLoader dynamicClassLoader = new DynamicClassLoader(this.getClass()
-                .getClassLoader());
+        DynamicClassLoader dynamicClassLoader =
+                new DynamicClassLoader(this.getClass().getClassLoader());
 
         // Fill the class loader with all necessary file paths
         for (String filePath : filePaths) {
@@ -257,32 +257,32 @@ public class JavaExecution extends AbstractExecution {
         }
 
         String className1 = "test." + modelName;
-        //String className2 = "test." + modelName + "$State";
+        // String className2 = "test." + modelName + "$State";
 
         // Register classes to be loaded specifically by the dynamic class loader
         dynamicClassLoader.addClassFileByName(className1);
-        //dynamicClassLoader.addClassFileByName(className2);
+        // dynamicClassLoader.addClassFileByName(className2);
 
         // Build array of compiled files as return value (for possibly measuring file size)
         File[] returnFiles = new File[2];
         String classFileName1 = className1;
-        //String classFileName2 = className2;
+        // String classFileName2 = className2;
         if (className1.lastIndexOf(".") > 0) {
             classFileName1 = className1.substring(className1.lastIndexOf(".") + 1);
         }
-        //if (className2.lastIndexOf(".") > 0) {
-        //    classFileName2 = className2.substring(className2.lastIndexOf(".") + 1);
-        //}
+        // if (className2.lastIndexOf(".") > 0) {
+        // classFileName2 = className2.substring(className2.lastIndexOf(".") + 1);
+        // }
         classFileName1 += ".class";
-        //classFileName2 += ".class";
+        // classFileName2 += ".class";
         URL classFileURL1 = dynamicClassLoader.getResource(classFileName1);
-        //URL classFileURL2 = dynamicClassLoader.getResource(classFileName2);
+        // URL classFileURL2 = dynamicClassLoader.getResource(classFileName2);
         if (classFileURL1 != null) {
             returnFiles[0] = new File(classFileURL1.getFile());
         }
-        //if (classFileURL2 != null) {
-        //    returnFiles[1] = new File(classFileURL2.getFile());
-        //}
+        // if (classFileURL2 != null) {
+        // returnFiles[1] = new File(classFileURL2.getFile());
+        // }
 
         // Instantiate new class as SJProgramWithSignals
         Class<?> cls;
