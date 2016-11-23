6a15c3493b7bf022018091967fcfc7bf86a1c797
Christian Motika <cmot@informatik.uni-kiel.de>
2016-11-23 15:20:40 +0100

    Updated online compiler according to SYNCHRON meeting

--- plugins/de.cau.cs.kieler.sccharts.sim.java/src/de/cau/cs/kieler/sccharts/sim/java/SCChartsJavaDataComponent.java
+++ plugins/de.cau.cs.kieler.sccharts.sim.java/src/de/cau/cs/kieler/sccharts/sim/java/SCChartsJavaDataComponent.java
@@ -86,7 +86,7 @@ public class SCChartsJavaDataComponent extends JSONObjectSimulationDataComponent
     /** The dirty indicator is used to notice editor changes and set the dirty flag accordingly. */
     private int dirtyIndicator = 0;
 
-    private static final int KIEM_PROPERTY_MAX = 7;
+    private static final int KIEM_PROPERTY_MAX = 8;
 
     private static final int KIEM_PROPERTY_STATENAME = 0;
     private static final String KIEM_PROPERTY_NAME_STATENAME = "State Name";
@@ -132,6 +132,11 @@ public class SCChartsJavaDataComponent extends JSONObjectSimulationDataComponent
     /** The Constant KIEM_PROPERTY_DEFAULT_COMPILETRANSFORMATIONS. */
     private static final String KIEM_PROPERTY_DEFAULT_LOWLEVELTRANSFORMATIONS =
             "codegeneration, T_sccharts.scg, T_s.java";
+    
+    private static final int KIEM_PROPERTY_ALLVARS = 7;
+    private static final String KIEM_PROPERTY_NAME_ALLVARS = "Expose ALL Variables";
+    private static final boolean KIEM_PROPERTY_DEFAULT_ALLVARS = false;
+    
 
     /** The benchmark flag for generating cycle and file size signals. */
     private boolean benchmark = false;
@@ -203,6 +208,11 @@ public class SCChartsJavaDataComponent extends JSONObjectSimulationDataComponent
         properties[KIEM_PROPERTY_LOWLEVELTRANSFORMATIONS] =
                 new KiemProperty(KIEM_PROPERTY_NAME_LOWLEVELTRANSFORMATIONS,
                         KIEM_PROPERTY_DEFAULT_LOWLEVELTRANSFORMATIONS);
+        properties[KIEM_PROPERTY_ALLVARS] =
+                new KiemProperty(KIEM_PROPERTY_NAME_ALLVARS, KIEM_PROPERTY_DEFAULT_ALLVARS);
+        
+                
+        
         // properties[KIEM_PROPERTY_BENCHMARK] = new KiemProperty(KIEM_PROPERTY_NAME_BENCHMARK,
         // false);
         // properties[KIEM_PROPERTY_RUNTIMEDEBUGCONSOLE] = new KiemProperty(
@@ -214,6 +224,19 @@ public class SCChartsJavaDataComponent extends JSONObjectSimulationDataComponent
         // properties[KIEM_PROPERTY_RUNTIME] = new KiemProperty(KIEM_PROPERTY_NAME_RUNTIME,
         // new KiemPropertyTypeChoice(items), items[0]);
         return properties;
+    }
+    
+    
+    // -------------------------------------------------------------------------
+    
+    /**
+     * Checks if is expose all vars flag is turned on.
+     *
+     * @return true, if is expose all vars
+     */
+    public boolean isExposeAllVars() {
+        return this.getProperties()[KIEM_PROPERTY_ALLVARS
+                             + KIEM_PROPERTY_DIFF].getValueAsBoolean();
     }
 
     // -------------------------------------------------------------------------
@@ -899,7 +922,7 @@ public class SCChartsJavaDataComponent extends JSONObjectSimulationDataComponent
                     present = (order) > 0;
                 }
 
-                if ((!(simulatedModel instanceof State)) && fullDebugMode) {
+                if (isExposeAllVars() || ((!(simulatedModel instanceof State)) && fullDebugMode)) {
                     if (output.startsWith("g") || output.equals("_GO")) {
                         Object value;
                         try {
