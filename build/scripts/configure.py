b656f0e4a3862e4e3dcdd6782caf078b136ef48e
Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
2017-01-31 12:37:07 +0100

    build: Commented and improved release scripts

--- build/scripts/configure.py
+++ build/scripts/configure.py
@@ -10,7 +10,7 @@
 # See the file epl-v10.html for the license text.
 
 #
-# Release script to set version number in all plugins
+# Release script to set version number in all plugins and related files
 # Author: Alexander Schulz-Rosengarten <als@informatik.uni-kiel.de>
 #
 
@@ -37,7 +37,7 @@ def main(args):
     targetplatformFile = join(args.path, 'build/de.cau.cs.kieler.semantics.targetplatform/de.cau.cs.kieler.semantics.targetplatform.target')
     if not isfile(targetplatformFile):
         print 'Target platform does not exist: ' + targetplatformFile
-        sys.exit(-1)
+        pause(args)
     targetplatformXml = etree.parse(targetplatformFile, parser = etree.XMLParser(remove_comments=False))
     targetplatform = [elem.attrib['location'] for elem in targetplatformXml.findall('./locations/location/repository')]
 
@@ -46,53 +46,60 @@ def main(args):
     template = join(args.path, 'build/scripts/template/index.html')
     if not isfile(template):
         print 'Updatesite index.html template file does not exist: ' + updatesite
-        sys.exit(-1)
-    createUpdatesiteFacade(template, args)
+        pause(args)
+    else:
+        createUpdatesiteFacade(template, args)
 
     print '\n- Setting category description -'
     # check category file
     category = join(args.path, 'build/de.cau.cs.kieler.semantics.repository/category.xml')
     if not isfile(category):
         print 'category.xml does not exist: ' + category
-        sys.exit(-1)
-    setCategoryDesc(category, targetplatform, args)
+        pause(args)
+    else:
+        setCategoryDesc(category, targetplatform, args)
 
     print '\n- Processing Features -'
     # check features path
     features = join(args.path, 'features')
     if not isdir(features):
         print 'No features directory found in ' + args.path
-        sys.exit(-1)
-    setFeatureUpdatesite(features, args)
+        pause(args)
+    else:
+        setFeatureUpdatesite(features, args)
 
     print '\n- Setting product artifact name -'
     # check product pom file
     product = join(args.path, 'build/de.cau.cs.kieler.semantics.product.repository/pom.xml')
     if not isfile(product):
         print 'pom.xml does not exist: ' + product
-        sys.exit(-1)
-    setArtifactName(product, args)
+        pause(args)
+    else:
+        setArtifactName(product, args)
 
     print '\n- Setting product updatesite -'
     # check main pom file
     pom = join(args.path, 'build/pom.xml')
     if not isfile(pom):
         print 'pom.xml does not exist: ' + pom
-        sys.exit(-1)
-    setProductUpdateSite(pom, args)
+        pause(args)
+    else:
+        setProductUpdateSite(pom, args)
 
     print '\n- Setting product updatesites -'
     # check main pom file
     p2inf = join(args.path, 'build/de.cau.cs.kieler.semantics.product.repository/kieler.p2.inf')
     if not isfile(p2inf):
         print 'kieler.p2.inf does not exist: ' + pom
-        sys.exit(-1)
-    setProductUpdateSites(p2inf, targetplatform, args)
+        pause(args)
+    else:
+        setProductUpdateSites(p2inf, targetplatform, args)
 
     print '\n= Finished Configuring ='
 
 
 def createUpdatesiteFacade(template, args):
+    """Creates index.html for updatesite from template"""
     target = join(args.path, 'build/de.cau.cs.kieler.semantics.repository/facade/index.html')
     with open(template, 'r') as inFile:
         with open(target, 'w') as outFile:
@@ -108,7 +115,8 @@ def createUpdatesiteFacade(template, args):
 
 
 def setCategoryDesc(category, targetplatform, args):
-    xml = etree.parse(category, parser = etree.XMLParser(remove_comments=False, remove_blank_text=True))
+    """Set name and url of the updatesite in the updatesite and update all repository references associating other updatesites with the updatesite"""
+    xml = etree.parse(category, parser = etree.XMLParser(remove_comments=False, remove_blank_text=True)) # remove blanks to allow formatting of new entries
     root = xml.getroot()
 
     description = xml.find('./description')
@@ -136,12 +144,11 @@ def setCategoryDesc(category, targetplatform, args):
         root.append(refElem)
         print 'Added references repository: %s' % ref
 
-    with open(category, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-        xml.write(f, encoding='UTF-8', pretty_print=True, xml_declaration=False)
-
+    writeXML(xml, category)
+         
 
 def setFeatureUpdatesite(features, args):
+    """Set name and url of the updatesite displayed in features"""
     for featureDir in sorted(os.listdir(features)):
         feature = join(features, featureDir, 'feature.xml')
         if isfile(feature):
@@ -157,14 +164,11 @@ def setFeatureUpdatesite(features, args):
                 print 'Set updatesite url in %s' % featureDir
             # no else because dependency feature has not url
 
-            with open(feature, 'w') as f:
-                f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-                # restore html entities
-                restoreHTMLentities(xml)
-                f.write(fixHTMLentities(etree.tostring(xml, encoding='UTF-8', pretty_print=True, xml_declaration=False)))
+            writeXML(xml, feature, htmlentities=True)
 
 
 def setArtifactName(product, args):
+    """Set filename pattern for product artifacts in product pom"""
     xml = etree.parse(product, parser = etree.XMLParser(remove_comments=False))
     archivename = xml.find('./p:build/p:plugins/p:plugin/p:configuration/p:products/p:product/p:archiveFileName', namespaces={'p':'http://maven.apache.org/POM/4.0.0'})
     if archivename is not None:
@@ -174,15 +178,14 @@ def setArtifactName(product, args):
             archivename.text = 'sccharts_rca_nightly_${buildQualifier}'
         print 'Artifact name pattern set to: %s' % archivename.text
         
-        with open(product, 'w') as f:
-            f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-            xml.write(f, encoding='UTF-8', pretty_print=True, xml_declaration=False)
+        writeXML(xml, product)
     else:
         print 'Cannot find archiveFileName element in %s' % product
         pause(args)
 
 
 def setProductUpdateSite(pom, args):
+    """Set source updatesite for product build"""
     xml = etree.parse(pom, parser = etree.XMLParser(remove_comments=False))
     updatesite = xml.find('./p:properties/p:kieler.semantics.updatesite', namespaces={'p':'http://maven.apache.org/POM/4.0.0'})
     if updatesite is not None:
@@ -200,14 +203,11 @@ def setProductUpdateSite(pom, args):
     if updatesite is not None:
         trimspace.text = u'&#xA0;(Sources)'
 
-    with open(pom, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-        # restore html entities
-        restoreHTMLentities(xml)
-        f.write(fixHTMLentities(etree.tostring(xml, encoding='UTF-8', pretty_print=True, xml_declaration=False)))
+    writeXML(xml, pom, htmlentities=True)
 
 
 def setProductUpdateSites(p2inf, targetplatform, args):
+    """Create p2.inf for product configuring all updatesites registered in the product"""
     updatesites = targetplatform
     if args.release:
         updatesites.append(releaseUpdatesite % args.release)
--- build/scripts/version.py
+++ build/scripts/version.py
@@ -19,8 +19,6 @@ import re
 import os
 import argparse
 import traceback
-import htmlentitydefs
-import xml.etree.ElementTree as serializer
 from lxml import etree
 from os.path import join, isdir, isfile, normpath, dirname, abspath
 from reuse import *
@@ -33,57 +31,65 @@ def main(args):
     plugins = join(args.path, 'plugins')
     if not isdir(plugins):
         print 'No plugins directory found in ' + args.path
-        sys.exit(-1)
-    setProjectVersion(plugins, args, [setMainfestVersion, setPomVersion])
+        pause(args)
+    else:
+        setProjectVersion(plugins, args, [setMainfestVersion, setPomVersion])
 
     print '\n- Processing Features -'
     # check features path
     features = join(args.path, 'features')
     if not isdir(features):
         print 'No features directory found in ' + args.path
-        sys.exit(-1)
-    setProjectVersion(features, args, [setFeatureVersion, setPomVersion])
+        pause(args)
+    else:
+        setProjectVersion(features, args, [setFeatureVersion, setPomVersion])
 
     print '\n- Updating p2 repository -'
     # check category file
     category = join(args.path, 'build/de.cau.cs.kieler.semantics.repository/category.xml')
     if not isfile(category):
         print 'category.xml does not exist: ' + category
-        sys.exit(-1)
-    updateCategory(features, category, args)
+        pause(args)
+    else:
+        updateCategory(features, category, args)
     # check pom file
     pom = join(args.path, 'build/de.cau.cs.kieler.semantics.repository/pom.xml')
     if not isfile(pom):
         print 'pom.xml does not exist: ' + pom
-        sys.exit(-1)
-    setPomVersion(join(args.path, 'build'), 'de.cau.cs.kieler.semantics.repository', args)
+        pause(args)
+    else:
+        setPomVersion(join(args.path, 'build'), 'de.cau.cs.kieler.semantics.repository', args)
 
     print '\n- Updating product plugin.xml -'
     # check product file
     plugin = join(args.path, 'plugins/de.cau.cs.kieler.core.product/plugin.xml')
     if not isfile(plugin):
         print 'plugin.xml does not exist: ' + plugin
-        sys.exit(-1)
-    updateProductPlugin(plugin, args)
+        pause(args)
+    else:
+        updateProductPlugin(plugin, args)
 
     print '\n- Updating product -'
     # check product file
     product = join(args.path, 'build/de.cau.cs.kieler.semantics.product.repository/kieler.product')
     if not isfile(product):
         print 'kieler.product does not exist: ' + product
-        sys.exit(-1)
-    updateProduct(product, args)
+        pause(args)
+    else:
+        updateProduct(product, args)
     # check pom file
     pom = join(args.path, 'build/de.cau.cs.kieler.semantics.product.repository/pom.xml')
     if not isfile(pom):
         print 'pom.xml does not exist: ' + pom
-        sys.exit(-1)
-    setPomVersion(join(args.path, 'build'), 'de.cau.cs.kieler.semantics.product.repository', args)
+        pause(args)
+    else:
+        setPomVersion(join(args.path, 'build'), 'de.cau.cs.kieler.semantics.product.repository', args)
 
     print '\n= Finished Version Update ='
 
 
 def setProjectVersion(directory, args, setter):
+    """Iterate all eclipse projects in directory an apply setter function"""
     for project in sorted(os.listdir(directory)):
         if project.startswith(PROJECT_PREFIX) and isdir(join(directory, project)) and isfile(join(directory, project, '.project')):
             try:
@@ -100,6 +106,7 @@ def setProjectVersion(directory, args, setter):
 
 
 def setMainfestVersion(directory, project, args):
+    """Set bundle version property in manifest"""
     manifest = join(directory, project, 'META-INF', 'MANIFEST.MF')
     if isfile(manifest):
         # read
@@ -117,69 +124,61 @@ def setMainfestVersion(directory, project, args):
 
 
 def setFeatureVersion(directory, project, args):
+    """Set version attribute in feature definition"""
     feature = join(directory, project, 'feature.xml')
     if isfile(feature):
         xml = etree.parse(feature, parser = etree.XMLParser(remove_comments=False, resolve_entities=False))
         xml.getroot().attrib['version'] = '%s.qualifier' % args.version
 
-        with open(feature, 'w') as f:
-            f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-            # restore html entities
-            restoreHTMLentities(xml)
-            f.write(fixHTMLentities(etree.tostring(xml, encoding='UTF-8', pretty_print=True, xml_declaration=False)))
+        writeXML(xml, feature, htmlentities=True)
     else:
         print '-- feature.xml file is missing!'
         pause(args)
 
 
 def setPomVersion(directory, project, args):
+    """Set version element in project pom"""
     pom = join(directory, project, 'pom.xml')
     if isfile(pom):
         xml = etree.parse(pom, parser = etree.XMLParser(remove_comments=False))
         xml.find('./{http://maven.apache.org/POM/4.0.0}version').text = '%s-SNAPSHOT' % args.version
-        with open(pom, 'w') as f:
-            f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-            xml.write(f, encoding='UTF-8', pretty_print=True, xml_declaration=False)
+
+        writeXML(xml, pom)
     else:
         print '-- pom.xml file is missing!'
         pause(args)
 
 
 def updateCategory(features, category, args):
+    """Fix all versions of features referenced by category.xml"""
     xml = etree.parse(category, parser = etree.XMLParser(remove_comments=False))
     for feature in xml.findall('feature'):
         feature.attrib['url'] = 'features/%s_%s.qualifier.jar' % (feature.attrib['id'], args.version)
         feature.attrib['version'] = '%s.qualifier' % args.version
         print "Updated version of %s feature" % feature.attrib['id']
     
-    with open(category, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-        xml.write(f, encoding='UTF-8', pretty_print=True, xml_declaration=False)
+    writeXML(xml, category)
 
 def updateProductPlugin(product, args):
+    """Set version property of product runtime plugin"""
     xml = etree.parse(product, parser = etree.XMLParser(remove_comments=False))
     version = xml.find("./extension/product/property[@name='version']")
     if version is not None:
         version.attrib['value'] = args.version
         print 'Set version in product plugin'
     else:
-        print 'Cannot find versio property element in %s' % version
+        print 'Cannot find version property element in %s' % version
         pause(args)
 
-    with open(product, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-        xml.write(f, encoding='UTF-8', pretty_print=True, xml_declaration=False)
+    writeXML(xml, product)
 
 def updateProduct(product, args):
+    """Set version in product definition"""
     xml = etree.parse(product, parser = etree.XMLParser(remove_comments=False))
     xml.getroot().attrib['version'] = '%s.qualifier' % args.version
-    print 'Set version in product'
+    print 'Set version in product definition'
 
-    with open(product, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n') # etree creates a wrong xml decl
-        # restore html entities
-        restoreHTMLentities(xml)
-        f.write(fixHTMLentities(etree.tostring(xml, encoding='UTF-8', pretty_print=True, xml_declaration=False)))
+    writeXML(xml, product, htmlentities=True)
 
 
 if __name__ == "__main__":
