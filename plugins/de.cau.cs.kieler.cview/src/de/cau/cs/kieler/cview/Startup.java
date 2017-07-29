59dc25fc183694de28bbbf136ff2b52d521ac920
Christian Motika <cmot@informatik.uni-kiel.de>
2017-07-29 22:08:06 +0200

    Fixed: startup important for selection listener

---
+++ plugins/de.cau.cs.kieler.cview/src/de/cau/cs/kieler/cview/Startup.java
@@ -0,0 +1,27 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://rtsys.informatik.uni-kiel.de/kieler
+ * 
+ * Copyright 2017 by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ */
+package de.cau.cs.kieler.cview;
+
+import org.eclipse.ui.IStartup;
+
+import com.google.inject.Guice;
+
+public class Startup implements IStartup {
+
+    @Override
+    public void earlyStartup() {
+        System.out.println("+++ STARTUP +++");
+        Guice.createInjector().getInstance(KLighDController.class);
+    }
+
+}
